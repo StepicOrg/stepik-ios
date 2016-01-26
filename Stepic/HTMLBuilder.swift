fcce83ec492ed08b4c601cc3a9188490dccaf4ec
Alexander Karpov <x1te.rus@gmail.com>
2016-01-26 20:13:47 +0300

    fixed web view header sizes

--- Stepic/HTMLBuilder.swift
+++ Stepic/HTMLBuilder.swift
@@ -15,10 +15,10 @@ class HTMLBuilder: NSObject {
     private var stepicStyleString : String {        
         var res : String = ""
         res += "<style>"
-        res += "\nbody{font-size: 10pt; font-family:Arial, Helvetica, sans-serif; line-height:1.6em;}"
-        res += "\nh1{font-size: 12pt; font-family:Arial, Helvetica, sans-serif; line-height:1.6em;}"
-        res += "\nh2{font-size: 14pt; font-family:Arial, Helvetica, sans-serif; line-height:1.6em;}"
-        res += "\nh3{font-size: 18pt; font-family:Arial, Helvetica, sans-serif; line-height:1.6em;}"
+        res += "\nbody{font-size: 12pt; font-family:Arial, Helvetica, sans-serif; line-height:1.6em;}"
+        res += "\nh1{font-size: 20pt; font-family:Arial, Helvetica, sans-serif; line-height:1.6em;}"
+        res += "\nh2{font-size: 17pt; font-family:Arial, Helvetica, sans-serif; line-height:1.6em;}"
+        res += "\nh3{font-size: 14pt; font-family:Arial, Helvetica, sans-serif; line-height:1.6em;}"
         res += "\nimg { max-width: 100%; }"
         
 //        res += "\np { white-space: pre-wrap; word-wrap: break-word; max-width: 100%; }"
