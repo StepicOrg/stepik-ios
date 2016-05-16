76deab1d8ec7621c66d7f645dd68ec51f0219f96
Alexander Karpov <x1te.rus@gmail.com>
2016-05-16 16:49:18 +0300

    Added NotificationDataExtractor

---
+++ Stepic/NotificationDataExtractor.swift
@@ -0,0 +1,41 @@
+//
+//  NotificationDataExtractor.swift
+//  Stepic
+//
+//  Created by Alexander Karpov on 16.05.16.
+//  Copyright © 2016 Alex Karpov. All rights reserved.
+//
+
+import Foundation
+
+/*
+ Extracts information from the given notification
+ */
+class NotificationDataExtractor {
+    var notification : Notification
+    
+    private var parser : HTMLParser
+    
+    init(notification : Notification) {
+        self.notification = notification
+        self.parser = HTMLParser(htmlText: notification.htmlText)
+    }
+    
+    
+    //gets course id if it is available for the given notification type
+    func getCourseId() -> Int? {
+        if notification.type != .Learn {
+            return nil
+        } else {
+            if let courseLink = parser.getLink(index: 0) {
+                if let courseIdStartIndex = courseLink.lastIndexOf("-") {
+                    let start = courseLink.startIndex.advancedBy(courseIdStartIndex + 1)
+                    let end = courseLink.startIndex.advancedBy(courseLink.characters.count - 1)
+                    let courseIdString = courseLink.substringWithRange(start ..< end ) 
+                    return Int(courseIdString)
+                }
+            }
+            return nil
+        }
+    }
+}
