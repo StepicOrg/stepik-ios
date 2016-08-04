ce2ccfb78b78c669dee058d889a06c3a1b384182
Alexander Karpov <akarpov.work@gmail.com>
2016-08-05 00:09:27 +0300

    Added reaction to non-authorized course opening

--- Stepic/DeepLinkRouter.swift
+++ Stepic/DeepLinkRouter.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class DeepLinkRouter {
     
-    static func routeFromDeepLink(link: NSURL) {
+    static func routeFromDeepLink(link: NSURL, completion: (UIViewController? -> Void)) {
         func getCourseID(string: String) -> Int? {
             var courseString = ""
             for character in string.characters.reverse() {
@@ -34,21 +34,63 @@ class DeepLinkRouter {
             if components[1].lowercaseString == "course" {
                 if let courseId = getCourseID(components[2]) {
                     if components.count == 3 {
-                        routeToCourseWithId(courseId)
+                        routeToCourseWithId(courseId, completion: completion)
                     }
                     if components.count == 4 && components[3].lowercaseString.containsString("syllabus") {
-                        routeToSyllabusWithId(courseId)
+                        routeToSyllabusWithId(courseId, completion: completion)
                     }
                 } 
             }
         }
     }
     
-    private static func routeToCourseWithId(courseId: Int) {
-        
+    private static func routeToCourseWithId(courseId: Int, completion: (UIViewController? -> Void)) {
+        if StepicAPI.shared.isAuthorized {
+            if let vc = ControllerHelper.instantiateViewController(identifier: "CoursePreviewViewController") as?  CoursePreviewViewController {
+                do {
+                    let courses = try Course.getCourses([courseId])
+                    if courses.count == 0 {
+                        ApiDataDownloader.sharedDownloader.getCoursesByIds([courseId], deleteCourses: [], refreshMode: .Delete, success: {
+                            loadedCourses in 
+                            if loadedCourses.count == 1 {
+                                UIThread.performUI {
+                                    vc.course = loadedCourses[0]
+                                    completion(vc)
+                                }
+                            } else {
+                                print("error while downloading course with id \(courseId) - no courses or more than 1 returned")
+                                completion(nil)
+                                return
+                            }
+                        }, failure: {
+                                error in
+                                print("error while downloading course with id \(courseId)")
+                                completion(nil) 
+                                return
+                        })
+                        return
+                    } 
+                    if courses.count == 1 {
+                        vc.course = courses[0]
+                        completion(vc)
+                        return
+                    }
+                    completion(nil)
+                    return
+                }
+                catch {
+                    print("something bad happened")
+                    completion(nil)
+                    return
+                }
+            }
+            
+            completion(nil)
+            return
+        }
     }
     
-    private static func routeToSyllabusWithId(courseId: Int) {
+    private static func routeToSyllabusWithId(courseId: Int, completion: (UIViewController? -> Void)) {
         
     }
 }
