5abe6e8eb66b1906a38e809df2a32bb2b419f87a
Alexander Karpov <akarpov.work@gmail.com>
2016-08-18 22:48:43 +0300

    appropriate event names with underscores instead of spaces #APPS-441

--- Stepic/AnalyticsEvents.swift
+++ Stepic/AnalyticsEvents.swift
@@ -11,61 +11,61 @@ import Foundation
 struct AnalyticsEvents {
     
     struct Logout {
-        static let clicked = "clicked logout"
+        static let clicked = "clicked_logout"
     }
         
     struct SignIn {
-        static let onLaunchScreen = "clicked SignIn on launch screen"
-        static let onSignInScreen = "clicked SignIn on sign in screen"
+        static let onLaunchScreen = "clicked_SignIn_on_launch_screen"
+        static let onSignInScreen = "clicked_SignIn_on_sign_in_screen"
     }
     
     struct SignUp {
-        static let onLaunchScreen = "clicked SignUp on launch screen"
-        static let onSignUpScreen = "clicked SignUp on sign up screen"
+        static let onLaunchScreen = "clicked_SignUp_on_launch_screen"
+        static let onSignUpScreen = "clicked_SignUp_on_sign_up_screen"
     }
     
     struct Syllabus {
-        static let shared = "share syllabus clicked"
+        static let shared = "share_syllabus_clicked"
     }
     
     struct Section {
-        static let cache = "clicked cache section"
-        static let cancel = "clicked cancel section"
-        static let delete = "clicked delete cached section"
+        static let cache = "clicked_cache_section"
+        static let cancel = "clicked_cancel_section"
+        static let delete = "clicked_delete_cached_section"
     }
     
     struct Unit {
-        static let cache = "clicked cache unit"
-        static let cancel = "clicked cancel unit"
-        static let delete = "clicked delete cached unit"
+        static let cache = "clicked_cache_unit"
+        static let cancel = "clicked_cancel_unit"
+        static let delete = "clicked_delete_cached_unit"
     }
     
     struct Downloads {
-        static let clear = "clicked clear cache"
-        static let acceptedClear = "clicked accepted clear cache"
+        static let clear = "clicked_clear_cache"
+        static let acceptedClear = "clicked_accepted_clear_cache"
     }
     
     struct CourseOverview {
-        static let shared = "share course clicked"
+        static let shared = "share_course_clicked"
     }
     
     struct Step {        
         struct Submission {
-            static let submit = "clicked submit"
-            static let newAttempt = "clicked generate new attempt"
-            static let solveInWebPressed = "clicked solve in web"
+            static let submit = "clicked_submit"
+            static let newAttempt = "clicked_generate_new_attempt"
+            static let solveInWebPressed = "clicked_solve_in_web"
         }
     }
     
     struct VideoPlayer {
-        static let rateChanged = "video rate changed"
-        static let qualityChanged = "video quality changed"
+        static let rateChanged = "video_rate_changed"
+        static let qualityChanged = "video_quality_changed"
     }
     
     struct Discussion {
-        static let liked = "discussion liked"
-        static let unliked = "discussion unliked"
-        static let abused = "discussion abused"
+        static let liked = "discussion_liked"
+        static let unliked = "discussion_unliked"
+        static let abused = "discussion_abused"
     }
     
 }
