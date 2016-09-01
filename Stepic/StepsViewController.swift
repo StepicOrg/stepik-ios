4a4a7569970d71b3b73e8d7410575e3a1c77d272
Alexander Karpov <akarpov.work@gmail.com>
2016-09-01 11:07:52 +0300

    try to fix crashlytics issue on viewWillAppear in StepsViewController

--- Stepic/StepsViewController.swift
+++ Stepic/StepsViewController.swift
@@ -14,7 +14,7 @@ enum StepsControllerPresentationContext {
 }
 
 class StepsViewController: RGPageViewController {
-
+    
     //TODO: really need optionals here?
     var lesson : Lesson?
     var startStepId : Int?
@@ -81,13 +81,13 @@ class StepsViewController: RGPageViewController {
         super.viewDidLoad()
         
         self.navigationItem.title = lesson?.title
-
+        
         datasource = self
         delegate = self
         UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.LightContent
         UICustomizer.sharedCustomizer.setStepicNavigationBar(self.navigationController?.navigationBar)
         UICustomizer.sharedCustomizer.setStepicTabBar(self.tabBarController?.tabBar)
-
+        
         if numberOfPagesForViewController(self) == 0 {
             self.view.userInteractionEnabled = false
         }
@@ -118,26 +118,26 @@ class StepsViewController: RGPageViewController {
             if let s = self {
                 let newStepsSet = Set(s.lesson!.stepsArray)
                 let prevStepsSet = Set(prevStepsIds)
-            
+                
                 var reloadBlock : (Void->Void) = {
                     [weak self] in 
                     self?.reloadData()
                 }
-            
+                
                 if newStepsSet.exclusiveOr(prevStepsSet).count == 0 {
-                //need to reload one by one
+                    //need to reload one by one
                     reloadBlock = {
                         NSNotificationCenter.defaultCenter().postNotificationName(StepsViewController.stepUpdatedNotification, object: nil)
                         print("did send step updated notification")
                     }
                 } 
-            
+                
                 UIThread.performUI {
                     s.view.userInteractionEnabled = true
                     reloadBlock()
                     s.doesPresentWarningView = false
                     s.doesPresentActivityIndicatorView = false
-                
+                    
                     if let id = s.startStepId {
                         if id < s.lesson!.steps.count {
                             if !s.didSelectTab {
@@ -169,7 +169,7 @@ class StepsViewController: RGPageViewController {
         super.viewWillAppear(animated)
         self.navigationItem.backBarButtonItem?.title = " "
         if let l = lesson, id = startStepId {
-            if l.steps.count != 0  && id < l.steps.count {
+            if l.steps.count != 0  && id < pagesCount {
                 print("id -> \(id)")
                 didSelectTab = true
                 self.selectTabAtIndex(id, updatePage: true)
@@ -238,11 +238,14 @@ class StepsViewController: RGPageViewController {
             return 8.0
         }
     }
+    
+    var pagesCount = 0
 }
 
 extension StepsViewController : RGPageViewControllerDataSource {
     func numberOfPagesForViewController(pageViewController: RGPageViewController) -> Int {
-        return lesson?.steps.count ?? 0
+        pagesCount = lesson?.steps.count ?? 0
+        return pagesCount
     }
     
     func tabViewForPageAtIndex(pageViewController: RGPageViewController, index: Int) -> UIView {
@@ -251,11 +254,11 @@ extension StepsViewController : RGPageViewControllerDataSource {
         if index >= lesson!.steps.count {
             return UIView()
         }
-
+        
         if let step = lesson?.steps[index] {
-//            if tabViewsForStepId[step.id] == nil {
-                tabViewsForStepId[step.id] = StepTabView(frame: CGRect(x: 0, y: 0, width: 25, height: 25), image: step.block.image, stepId: step.id, passed: step.progress?.isPassed ?? false)
-//            }
+            //            if tabViewsForStepId[step.id] == nil {
+            tabViewsForStepId[step.id] = StepTabView(frame: CGRect(x: 0, y: 0, width: 25, height: 25), image: step.block.image, stepId: step.id, passed: step.progress?.isPassed ?? false)
+            //            }
             
             return tabViewsForStepId[step.id]!
         } else {
@@ -265,63 +268,66 @@ extension StepsViewController : RGPageViewControllerDataSource {
     
     func viewControllerForPageAtIndex(pageViewController: RGPageViewController, index: Int) -> UIViewController? {
         
-        //Just a try to fix a strange bug
-        if index >= lesson!.steps.count {
-            return nil
-        }
-        
-        if lesson!.steps[index].block.name == "video" {
-            let stepController = storyboard?.instantiateViewControllerWithIdentifier("VideoStepViewController") as! VideoStepViewController
-            stepController.video = lesson!.steps[index].block.video!
-            stepController.nItem = self.navigationItem
-            stepController.step = lesson!.steps[index]
-            stepController.parentNavigationController = self.navigationController
-            if context == .Unit {
-                stepController.assignment = lesson!.unit?.assignments[index]
-                
-                if index == 0 && shouldNavigateToPrev {
-                    stepController.prevLessonHandler = {
-                        [weak self] in
-                        self?.sectionNavigationDelegate?.displayPrev()
-                    } 
-                }
-                
-                if index == lesson!.steps.count - 1 && shouldNavigateToNext {
-                    stepController.nextLessonHandler = {
-                        [weak self] in
-                        self?.sectionNavigationDelegate?.displayNext()
-                    } 
-                }
+        if let lesson = lesson {
+            //Just a try to fix a strange bug
+            if index >= lesson.steps.count {
+                return nil
             }
             
-            return stepController
-        } else {
-            let stepController = storyboard?.instantiateViewControllerWithIdentifier("WebStepViewController") as! WebStepViewController
-            stepController.parent = self
-            stepController.step = lesson!.steps[index]
-            stepController.lesson = lesson
-            stepController.stepId = index + 1
-            stepController.nItem = self.navigationItem
-            if context == .Unit {
-                stepController.assignment = lesson!.unit?.assignments[index]
-                
-                if index == 0 && shouldNavigateToPrev {
-                    stepController.prevLessonHandler = {
-                        [weak self] in
-                        self?.sectionNavigationDelegate?.displayPrev()
-                    } 
+            if lesson.steps[index].block.name == "video" {
+                let stepController = storyboard?.instantiateViewControllerWithIdentifier("VideoStepViewController") as! VideoStepViewController
+                stepController.video = lesson.steps[index].block.video!
+                stepController.nItem = self.navigationItem
+                stepController.step = lesson.steps[index]
+                stepController.parentNavigationController = self.navigationController
+                if context == .Unit {
+                    stepController.assignment = lesson.unit?.assignments[index]
+                    
+                    if index == 0 && shouldNavigateToPrev {
+                        stepController.prevLessonHandler = {
+                            [weak self] in
+                            self?.sectionNavigationDelegate?.displayPrev()
+                        } 
+                    }
+                    
+                    if index == lesson.steps.count - 1 && shouldNavigateToNext {
+                        stepController.nextLessonHandler = {
+                            [weak self] in
+                            self?.sectionNavigationDelegate?.displayNext()
+                        } 
+                    }
                 }
                 
-                if index == lesson!.steps.count - 1 && shouldNavigateToNext {
-                    stepController.nextLessonHandler = {
-                        [weak self] in
-                        self?.sectionNavigationDelegate?.displayNext()
-                    } 
+                return stepController
+            } else {
+                let stepController = storyboard?.instantiateViewControllerWithIdentifier("WebStepViewController") as! WebStepViewController
+                stepController.parent = self
+                stepController.step = lesson.steps[index]
+                stepController.lesson = lesson
+                stepController.stepId = index + 1
+                stepController.nItem = self.navigationItem
+                if context == .Unit {
+                    stepController.assignment = lesson.unit?.assignments[index]
+                    
+                    if index == 0 && shouldNavigateToPrev {
+                        stepController.prevLessonHandler = {
+                            [weak self] in
+                            self?.sectionNavigationDelegate?.displayPrev()
+                        } 
+                    }
+                    
+                    if index == lesson.steps.count - 1 && shouldNavigateToNext {
+                        stepController.nextLessonHandler = {
+                            [weak self] in
+                            self?.sectionNavigationDelegate?.displayNext()
+                        } 
+                    }
                 }
+                
+                return stepController
             }
-            
-            return stepController
         }
+        return nil
     } 
 }
 
