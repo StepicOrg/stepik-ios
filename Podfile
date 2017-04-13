3aeccb2a667b7a81c665c49c7410f31451f1604f
Alexander Karpov <x1te.rus@gmail.com>
2017-04-13 19:28:29 +0300

    Added Quick and Nimble for testing Certificates - Initial CertificatesViewTestMock implementation

--- Podfile
+++ Podfile
@@ -44,11 +44,17 @@ def all_pods
     pod 'Presentr'
 end
 
+def testing_pods
+    pod 'Quick'
+    pod 'Nimble'
+end
+
 target 'Stepic' do
     all_pods
     target 'StepicTests' do
         inherit! :search_paths
         all_pods
+        testing_pods
     end
 end
 
---
+++ Stepic/CertificatesViewTestMock.swift
@@ -0,0 +1,43 @@
+//
+//  CertificatesViewTestMock.swift
+//  Stepic
+//
+//  Created by Ostrenkiy on 13.04.17.
+//  Copyright © 2017 Alex Karpov. All rights reserved.
+//
+
+import Foundation
+
+class CertificatesViewTestMock : CertificatesView {
+    
+    var ids : [Int] = []
+    
+    func setCertificates(certificates: [CertificateViewData], hasNextPage: Bool) {
+        
+    }
+    
+    func displayAnonymous() {
+        
+    }
+    
+    func displayError() {
+        
+    }
+    
+    func displayEmpty() {
+        
+    }
+    
+    func displayRefreshing() {
+        
+    }
+    
+    func displayLoadNextPageError() {
+        
+    }
+    
+    func updateData() {
+        
+    }
+    
+}
