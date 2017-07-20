73d76f8a9b0af256d02703ea9c8ddd10e6f0b15d
Vladislav Kiryukhin <kiryukhinv@gmail.com>
2017-07-20 18:16:52 +0300

    Change rating formulas

--- StepicAdaptiveCourse/RatingHelper.swift
+++ StepicAdaptiveCourse/RatingHelper.swift
@@ -37,30 +37,10 @@ class RatingHelper {
     }
     
     static func getLevel(for rating: Int) -> Int {
-        switch rating {
-        case _ where rating < 1:
-            return 1
-        case _ where rating < 2:
-            return 2
-        case _ where rating < 5:
-            return 3
-        default:
-            return 4 + Int(log(Double(rating) / 5.0) / log(2.0))
-        }
+        return rating < 5 ? 1 : 2 + Int(log(Double(rating) / 5.0) / log(2.0))
     }
     
     static func getRating(for level: Int) -> Int {
-        switch level {
-        case 1:
-            return 0
-        case 2:
-            return 1
-        case 3:
-            return 2
-        case 4:
-            return 5
-        default:
-            return 5 * Int(pow(2.0, Double(level - 4)))
-        }
+        return level == 0 ? 0 : (level == 1 ? 5 : 5 * Int(pow(2.0, Double(level - 1))))
     }
 }
