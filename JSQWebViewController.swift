23439c39255b9926e1824e7651d257ec36f7f233
Vladislav Kiryukhin <kiryukhinv@gmail.com>
2017-08-03 13:48:47 +0300

    Change user agent

--- JSQWebViewController.swift
+++ JSQWebViewController.swift
@@ -77,7 +77,7 @@ class WebViewController: UIViewController {
         webView.allowsBackForwardNavigationGestures = true
         if #available(iOS 9.0, *) {
             webView.allowsLinkPreview = true
-            webView.customUserAgent = "Mozilla/5.0"
+            webView.customUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4"
         }
         return webView
         }()
