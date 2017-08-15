69e0d6f0833c4b245b9ae31e4d26aac65632228e
Vladislav Kiryukhin <kiryukhinv@gmail.com>
2017-08-15 12:27:36 +0300

    Add raiting API methods

---
+++ StepicAdaptiveCourse/RatingsAPI.swift
@@ -0,0 +1,56 @@
+//
+//  RatingsAPI.swift
+//  Stepic
+//
+//  Created by Vladislav Kiryukhin on 15.08.2017.
+//  Copyright © 2017 Alex Karpov. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class RatingsAPI {
+    
+    typealias RatingRecord = (userId: Int, exp: Int)
+    
+    @discardableResult func retrieve(courseId: Int, count: Int = 10, days: Int = 7, headers: [String: String] = AuthInfo.shared.initialHTTPHeaders, success: @escaping ([RatingRecord]) -> Void, error errorHandler: @escaping (String) -> Void) -> Request? {
+
+        let params: Parameters = [
+            "course": courseId,
+            "count": count,
+            "days": days
+        ]
+
+        return Alamofire.request("\(StepicApplicationsInfo.adaptiveRatingURL)/rating", method: .get, parameters: params, encoding: URLEncoding.default, headers: headers).responseSwiftyJSON({
+            response in
+
+            var error = response.result.error
+            var json: JSON = [:]
+            if response.result.value == nil {
+                if error == nil {
+                    error = NSError()
+                }
+            } else {
+                json = response.result.value!
+            }
+            let response = response.response
+            
+            if let e = error {
+                let d = (e as NSError).localizedDescription
+                print(d)
+                errorHandler(d)
+                return
+            }
+
+            if response?.statusCode == 200 {
+                let leaders = json.arrayValue.map({return RatingRecord(userId: $0["user"].intValue, exp: $0["exp"].intValue)})
+                success(leaders)
+                return
+            } else {
+                errorHandler("Response status code is wrong(\(String(describing: response?.statusCode)))")
+                return
+            }
+        })
+    }
+}
