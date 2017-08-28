e82cd48896b2201b5305e9d41a68623fa59da3d3
Alexander Karpov <x1te.rus@gmail.com>
2017-08-29 00:50:22 +0300

    Added states and data loading to profile

--- Stepic/ProfilePresenter.swift
+++ Stepic/ProfilePresenter.swift
@@ -11,18 +11,82 @@ import Foundation
 protocol ProfileView: class {
     func presentStreak()
     func setLearningNotifications(on: Bool)
-    func setProfile(profile: ProfileData)
-    func presentStreakData(streak: StreakData)
+    func set(profile: ProfileData?)
+    func set(state: ProfileState)
+    func set(streaks: StreakData?)
+    func logout(onBack:(()->Void)?)
 }
 
 class ProfilePresenter {
     
+    weak var view: ProfileView?
+    
+    init(view: ProfileView) {
+        self.view = view
+    }
+    
+    func updateStreaks(user: User) {
+        _ = ApiDataDownloader.userActivities.retrieve(user: user.id, success: {
+            [weak self]
+            activity in
+                self?.view?.set(streaks: StreakData(userActivity: activity))
+            }, error: {
+                _ in
+                self.view?.set(streaks: nil)
+        })
+    }
+    
+    func updateProfile() {
+        if AuthInfo.shared.isAuthorized {
+            if let user = AuthInfo.shared.user {
+                self.view?.set(profile: ProfileData(user: user))
+                self.view?.set(state: .authorized)
+            } else {
+                self.view?.set(state: .refreshing)
+                performRequest({
+                    [weak self] in
+                    if let user = AuthInfo.shared.user {
+                        self?.view?.set(profile: ProfileData(user: user))
+                        self?.view?.set(state: .authorized)
+                    } else {
+                        self?.view?.set(profile: nil)
+                        self?.view?.set(state: .error)
+                    }
+                }, error: {
+                    [weak self]
+                    error in
+                    if error == PerformRequestError.noAccessToRefreshToken {
+                        self?.view?.logout(onBack: {
+                            [weak self] in
+                            self?.updateProfile()
+                        })
+                    } else {
+                        self?.view?.set(profile: nil)
+                        self?.view?.set(state: .error)
+                    }
+                })
+            }
+        } else {
+            self.view?.set(state: .anonymous)
+        }
+    }
+}
+
+enum ProfileState {
+    case authorized
+    case refreshing
+    case error
+    case anonymous
 }
 
 struct ProfileData {
-    
+    init(user: User) {
+        
+    }
 }
 
 struct StreakData {
-    
+    init(userActivity: UserActivity) {
+        
+    }
 }
