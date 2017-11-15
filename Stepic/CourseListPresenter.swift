7e0ccb2df9bd446a56b0ebc8057f70929d02a444
Alexander Karpov <akarpov.work@gmail.com>
2017-11-15 23:24:15 +0300

    Explore screen (#175)
    * Added CourseList entity
    
    * Added CourseList API and ContentLanguage enum
    
    * added language to course lists
    
    * swiftlint
    
    * Explore Courses without language or filters
    
    * Added language selection to explore
    
    * Added language to popular courses
    
    * Added search support to CourseList & added Search UI to Explore
    
    * New search based on CourseList & MVP pattern
    Need to update empty states
    
    * Empty states for search
    
    * Search language applies now
    
    * Removed FindCourses module from tab bar
    
    * Added fade animation to search
    
    * Added choose language localization
    
    * added searching on return
    
    * Fixed refresh bug for course list & reference cycles
    
    * hiding keyboard after swipe & some behavior fixes
    
    * fixed tests
    
    * Added Catalog localization
    
    * removed comments & added deprecation to search api
    
    * Added CourseSubscriptionManager as a dependency
    
    * Added reachability listening to explore screen & fixed localizations

--- Stepic/CourseListPresenter.swift
+++ Stepic/CourseListPresenter.swift
@@ -42,8 +42,11 @@ class CourseListPresenter {
     private var coursesAPI: CoursesAPI
     private var progressesAPI: ProgressesAPI
     private var reviewSummariesAPI: CourseReviewSummariesAPI
+    private var searchResultsAPI: SearchResultsAPI
+    private var subscriptionManager: CourseSubscriptionManager
 
     private var colorMode: CourseListColorMode
+    private var onlyLocal: Bool
 
     private var ID: String
 
@@ -55,8 +58,7 @@ class CourseListPresenter {
     private var hasNextPage: Bool = false
 
     private var lastUser: User?
-
-    private var subscriptionManager = CourseSubscriptionManager()
+    private var lastLanguage: ContentLanguage
 
     weak var lastStepDataSource: LastStepWidgetDataSource?
     weak var couseListCountDelegate: CourseListCountDelegate?
@@ -103,7 +105,7 @@ class CourseListPresenter {
         return result
     }
 
-    init(view: CourseListView, ID: String, limit: Int?, listType: CourseListType, colorMode: CourseListColorMode, coursesAPI: CoursesAPI, progressesAPI: ProgressesAPI, reviewSummariesAPI: CourseReviewSummariesAPI) {
+    init(view: CourseListView, ID: String, limit: Int?, listType: CourseListType, colorMode: CourseListColorMode, onlyLocal: Bool, subscriptionManager: CourseSubscriptionManager, coursesAPI: CoursesAPI, progressesAPI: ProgressesAPI, reviewSummariesAPI: CourseReviewSummariesAPI, searchResultsAPI: SearchResultsAPI) {
         self.view = view
         self.ID = ID
         self.coursesAPI = coursesAPI
@@ -113,6 +115,10 @@ class CourseListPresenter {
         self.listType = listType
         self.colorMode = colorMode
         self.lastUser = AuthInfo.shared.user
+        self.lastLanguage = ContentLanguage.sharedContentLanguage
+        self.onlyLocal = onlyLocal
+        self.searchResultsAPI = searchResultsAPI
+        self.subscriptionManager = subscriptionManager
         subscriptionManager.handleUpdatesBlock = {
             [weak self] in
             self?.handleCourseSubscriptionUpdates()
@@ -171,6 +177,10 @@ class CourseListPresenter {
         if courses.isEmpty {
             displayCached(ids: listType.cachedListCourseIds)
         }
+        if onlyLocal {
+            state = courses.isEmpty ? .empty : .displaying
+            return
+        }
         state = courses.isEmpty ? .emptyRefreshing : .displayingWithRefreshing
         checkToken().then {
             [weak self] in
@@ -196,7 +206,7 @@ class CourseListPresenter {
         }
         self.view?.setPaginationStatus(status: .loading)
         coursesAPI.cancelAllTasks()
-        listType.request(page: currentPage + 1, withAPI: coursesAPI, progressesAPI: progressesAPI)?.then {
+        listType.request(page: currentPage + 1, language: ContentLanguage.sharedContentLanguage, withAPI: coursesAPI, progressesAPI: progressesAPI, searchResultsAPI: searchResultsAPI)?.then {
             [weak self]
             (courses, meta) -> Void in
             guard let strongSelf = self else {
@@ -218,7 +228,7 @@ class CourseListPresenter {
     }
 
     func willAppear() {
-        if lastUser != AuthInfo.shared.user {
+        if lastUser != AuthInfo.shared.user || lastLanguage != ContentLanguage.sharedContentLanguage {
             courses = []
             self.view?.display(courses: [])
             lastUser = AuthInfo.shared.user
@@ -318,15 +328,15 @@ class CourseListPresenter {
     }
 
     private func requestNonCollection(updateProgresses: Bool, completion: (() -> Void)? = nil) {
-        listType.request(page: 1, withAPI: coursesAPI, progressesAPI: progressesAPI)?.then {
+        listType.request(page: 1, language: ContentLanguage.sharedContentLanguage, withAPI: coursesAPI, progressesAPI: progressesAPI, searchResultsAPI: searchResultsAPI)?.then {
             [weak self]
             (courses, meta) -> Void in
             guard let strongSelf = self else {
                 return
             }
             strongSelf.courses = courses
+            strongSelf.state = courses.isEmpty ? .empty: .displaying
             strongSelf.view?.display(courses: strongSelf.getData(from: strongSelf.displayingCourses))
-
             strongSelf.updateReviewSummaries(for: courses)
             if updateProgresses {
                 strongSelf.updateProgresses(for: courses)
@@ -334,7 +344,6 @@ class CourseListPresenter {
             strongSelf.currentPage = meta.page
             strongSelf.hasNextPage = meta.hasNext
             strongSelf.view?.setPaginationStatus(status: strongSelf.shouldLoadNextPage ? .loading : .none)
-            strongSelf.state = courses.isEmpty ? .empty: .displaying
             strongSelf.didRefreshOnce = true
             completion?()
             }.catch {
@@ -362,14 +371,14 @@ class CourseListPresenter {
                 guard let strongSelf = self else {
                     return
                 }
-                strongSelf.courses = courses
+                strongSelf.courses = Sorter.sort(courses, byIds: ids)
+                strongSelf.state = courses.isEmpty ? .empty: .displaying
                 strongSelf.view?.display(courses: strongSelf.getData(from: strongSelf.displayingCourses))
                 strongSelf.updateReviewSummaries(for: courses)
                 strongSelf.updateProgresses(for: courses)
                 strongSelf.currentPage = 1
                 strongSelf.hasNextPage = false
                 strongSelf.view?.setPaginationStatus(status: strongSelf.shouldLoadNextPage ? .loading : .none)
-                strongSelf.state = courses.isEmpty ? .empty: .displaying
                 strongSelf.didRefreshOnce = true
             }.catch {
                 [weak self]
@@ -599,6 +608,7 @@ enum CourseListType {
     case enrolled
     case popular
     case collection(ids: [Int])
+    case search(query: String)
 
     private func requestAllEnrolled(coursesAPI: CoursesAPI, progressesAPI: ProgressesAPI) -> Promise<([Course], Meta)>? {
         return Promise {
@@ -676,12 +686,34 @@ enum CourseListType {
         }
     }
 
-    func request(page: Int, withAPI coursesAPI: CoursesAPI, progressesAPI: ProgressesAPI) -> Promise<([Course], Meta)>? {
+    func request(page: Int, language: ContentLanguage, withAPI coursesAPI: CoursesAPI, progressesAPI: ProgressesAPI, searchResultsAPI: SearchResultsAPI) -> Promise<([Course], Meta)>? {
+
+        let requestedLanguage: ContentLanguage? = language == .russian ? nil : language
+
         switch self {
         case .popular:
-            return coursesAPI.retrieve(featured: true, excludeEnded: true, isPublic: true, order: "-activity", page: page)
+            return coursesAPI.retrieve(featured: true, excludeEnded: true, isPublic: true, order: "-activity", language: requestedLanguage, page: page)
         case .enrolled:
             return requestAllEnrolled(coursesAPI: coursesAPI, progressesAPI: progressesAPI)
+        case let .search(query: query):
+            var resultMeta: Meta = Meta(hasNext: false, hasPrev: false, page: 1)
+            var searchCoursesIDs: [Int] = []
+            return Promise<([Course], Meta)> {
+                fulfill, reject in
+                searchResultsAPI.searchCourse(query: query, language: requestedLanguage, page: page).then {
+                    (searchResults, meta) -> Promise<([Course])> in
+                    resultMeta = meta
+                    searchCoursesIDs = searchResults.flatMap { $0.courseId }
+                    return coursesAPI.retrieve(ids: searchCoursesIDs, existing: Course.getCourses(searchCoursesIDs))
+                    }.then {
+                        (courses) -> Void in
+                        let resultCourses = Sorter.sort(courses, byIds: searchCoursesIDs)
+                        fulfill((resultCourses, resultMeta))
+                    }.catch {
+                        error in
+                        reject(error)
+                }
+            }
         default:
             return nil
         }
@@ -699,7 +731,7 @@ enum CourseListType {
     private var cacheId: String? {
         switch self {
         case .popular:
-            return "PopularCoursesInfo"
+            return "PopularCoursesInfo_\(ContentLanguage.sharedContentLanguage.languageString)"
         case .enrolled:
             return "MyCoursesInfo"
         default:
---
+++ Stepic/SearchResultsPresenter.swift
@@ -0,0 +1,87 @@
+//
+//  SearchResultsPresenter.swift
+//  Stepic
+//
+//  Created by Ostrenkiy on 13.11.2017.
+//  Copyright © 2017 Alex Karpov. All rights reserved.
+//
+
+import Foundation
+
+protocol SearchResultsView: class {
+    func set(state: CoursesSearchResultsState)
+    func set(controller: UIViewController, forState: CoursesSearchResultsState)
+}
+
+class SearchResultsPresenter {
+    weak var view: SearchResultsView?
+
+    var updateQueryBlock: ((String) -> Void)?
+    var hideKeyboardBlock: (() -> Void)?
+
+    //TODO: Somehow refactor this
+    private var suggestionsVC: SearchQueriesViewController?
+    private var resultsVC: CourseListVerticalViewController?
+
+    var query: String = ""
+
+    init(view: SearchResultsView) {
+        self.view = view
+    }
+
+    func queryChanged(to query: String) {
+        self.query = query
+        guard query != "" else {
+            view?.set(state: .waiting)
+            resultsVC = nil
+            suggestionsVC = nil
+            return
+        }
+        if suggestionsVC == nil {
+            suggestionsVC = SearchQueriesViewController()
+            suggestionsVC?.delegate = self
+            suggestionsVC?.hideKeyboardBlock = self.hideKeyboardBlock
+            self.view?.set(controller: suggestionsVC!, forState: .suggestions)
+        }
+        suggestionsVC?.query = query
+        view?.set(state: .suggestions)
+        resultsVC = nil
+    }
+
+    func search(query: String) {
+        self.query = query
+        if resultsVC == nil {
+            resultsVC = ControllerHelper.instantiateViewController(identifier: "CourseListVerticalViewController", storyboardName: "CourseLists") as? CourseListVerticalViewController
+            if let resultsVC = resultsVC {
+                resultsVC.presenter = CourseListPresenter(view: resultsVC, ID: "SearchCourses", limit: nil, listType: .search(query: query), colorMode: .light, onlyLocal: false, subscriptionManager: CourseSubscriptionManager(), coursesAPI: CoursesAPI(), progressesAPI: ProgressesAPI(), reviewSummariesAPI: CourseReviewSummariesAPI(), searchResultsAPI: SearchResultsAPI())
+                self.view?.set(controller: resultsVC, forState: .courses)
+            }
+        } else {
+            //Don't actually know if this code is ever being executed
+            resultsVC?.presenter?.listType = .search(query: query)
+            resultsVC?.presenter?.refresh()
+        }
+        view?.set(state: .courses)
+        suggestionsVC = nil
+    }
+
+    func searchStarted() {
+        queryChanged(to: query)
+    }
+
+    func searchCancelled() {
+        query = ""
+    }
+}
+
+extension SearchResultsPresenter: SearchQueriesViewControllerDelegate {
+    func didSelectSuggestion(suggestion: String, position: Int) {
+        AnalyticsReporter.reportEvent(AnalyticsEvents.Search.selected, parameters: ["query": self.query.lowercased(), "position": position, "suggestion": suggestion])
+        search(query: suggestion)
+        updateQueryBlock?(suggestion)
+    }
+}
+
+enum CoursesSearchResultsState {
+    case waiting, suggestions, courses
+}
