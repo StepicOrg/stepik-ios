336ce9105384bf8749d675d34f0de0415eeabdbb
Alexander Ponomarev <gatman55rus@gmail.com>
2018-03-02 19:05:54 +0300

    Fix UIImageView+SVGDownload repeating code problem

--- Stepic/UIImageView+SVGDownload.swift
+++ Stepic/UIImageView+SVGDownload.swift
@@ -15,61 +15,43 @@ import SVGKit
 
 extension UIImageView {
 
-    #if os(tvOS)
-    func setImageWithURL(url optionalURL: URL?, placeholder: UIImage) {
-        self.setImageWithURL(url: optionalURL, placeholder: placeholder) { }
-    }
-
-    func setImageWithURL(url optionalURL: URL?, placeholder: UIImage, completion: @escaping () -> Void) {
+    func setImageWithURL(url optionalURL: URL?, placeholder: UIImage, completion: (() -> Void)? = nil) {
         guard let url = optionalURL else {
             self.image = placeholder
-            completion()
+            completion?()
             return
         }
 
         guard url.pathExtension != "svg" else {
+            #if !os(tvOS)
+                self.image = placeholder
+                Alamofire.request(url).responseData(completionHandler: {
+                    response in
+                    if response.result.error != nil {
+                        return
+                    }
+
+                    guard let data = response.result.value else {
+                        return
+                    }
+
+                    DispatchQueue.global(qos: .userInitiated).async {
+                        let svgImage = SVGKImage(data: data)
+                        if !(svgImage?.hasSize() ?? true) {
+                            svgImage?.size = CGSize(width: 200, height: 200)
+                        }
+                        let img = svgImage?.uiImage ?? placeholder
+                        DispatchQueue.main.async {
+                            self.image = img
+                        }
+                    }
+                })
+            #endif
             return
         }
 
         self.sd_setImage(with: optionalURL, placeholderImage: placeholder, options: SDWebImageOptions.retryFailed, completed: { _, _, _, _ in
-            completion()
+            completion?()
         })
     }
-    #else
-    func setImageWithURL(url optionalURL: URL?, placeholder: UIImage) {
-
-        guard let url = optionalURL else {
-            self.image = placeholder
-            return
-        }
-
-        if url.pathExtension != "svg" {
-            self.sd_setImage(with: url, placeholderImage: placeholder)
-        } else {
-            self.image = placeholder
-            Alamofire.request(url).responseData(completionHandler: {
-                response in
-                if response.result.error != nil {
-                    return
-                }
-
-                guard let data = response.result.value else {
-                    return
-                }
-
-                DispatchQueue.global(qos: .userInitiated).async {
-                    let svgImage = SVGKImage(data: data)
-                    if !(svgImage?.hasSize() ?? true) {
-                        svgImage?.size = CGSize(width: 200, height: 200)
-                    }
-                    let img = svgImage?.uiImage ?? placeholder
-                    DispatchQueue.main.async {
-                        self.image = img
-                    }
-                }
-
-            })
-        }
-    }
-    #endif
 }
