4eca3a4c3d6095018fda0a09f4e915832bad2939
Alexander Karpov <x1te.rus@gmail.com>
2018-03-28 00:37:44 +0300

    Added retrieve with parameters

--- Stepic/RetrieveRequestMaker.swift
+++ Stepic/RetrieveRequestMaker.swift
@@ -12,7 +12,7 @@ import PromiseKit
 import SwiftyJSON
 
 class RetrieveRequestMaker {
-    func request<T: JSONSerializable>(requestEndpoint: String, id: T.idType, updatingObject: T? = nil, withManager manager: Alamofire.SessionManager) -> Promise<T> {
+    func request<T: JSONSerializable>(requestEndpoint: String, paramName: String, id: T.idType, updatingObject: T? = nil, withManager manager: Alamofire.SessionManager) -> Promise<T> {
         return Promise { fulfill, reject in
             checkToken().then {
                 manager.request("\(StepicApplicationsInfo.apiURL)/\(requestEndpoint)/\(id)", method: .get, encoding: JSONEncoding.default).validate().responseSwiftyJSON { response in
@@ -20,16 +20,46 @@ class RetrieveRequestMaker {
                     case .failure(let error):
                         reject(error)
                     case .success(let json):
-                        if updatingObject == nil {
-                            updatingObject?.update(json: json[requestEndpoint].arrayValue[0])
+                        if updatingObject != nil {
+                            updatingObject?.update(json: json[paramName].arrayValue[0])
                         } else {
-                            fulfill(updatingObject)
+                            fulfill(T(json: json))
                         }
                     }
                 }
             }.catch {
                 error in
                 reject(error)
+            }
+        }
+    }
+
+    func request<T: JSONSerializable>(requestEndpoint: String, paramName: String, params: Parameters, updatingObjects: [T], withManager manager: Alamofire.SessionManager) -> Promise<([T], Meta, JSON)> {
+        return Promise { fulfill, reject in
+            checkToken().then {
+                manager.request("\(StepicApplicationsInfo.apiURL)/\(requestEndpoint)/", method: .get, parameters: params, encoding: JSONEncoding.default).validate().responseSwiftyJSON { response in
+                    switch response.result {
+                    case .failure(let error):
+                        reject(error)
+                    case .success(let json):
+                        let jsonArray: [JSON] = json[paramName].array ?? []
+                        let resultArray: [T] = jsonArray.map {
+                            objectJSON in
+                            if let recoveredIndex = updatingObjects.index(where: { $0.hasEqualId(json: objectJSON) }) {
+                                updatingObjects[recoveredIndex].update(json: objectJSON)
+                                return updatingObjects[recoveredIndex]
+                            } else {
+                                return T(json: objectJSON)
+                            }
+                        }
+                        let meta = Meta(json: json["meta"])
+                        fulfill((resultArray, meta, json))
+                        CoreDataHelper.instance.save()
+                    }
+                }
+                }.catch {
+                    error in
+                    reject(error)
             }
         }
     }
