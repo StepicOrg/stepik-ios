f2ed9b835545c4783fa450b7144414a3a5a5fe5f
Alexander Karpov <x1te.rus@gmail.com>
2018-03-29 00:45:21 +0300

    minor

--- Stepic/NotificationsAPI.swift
+++ Stepic/NotificationsAPI.swift
@@ -16,7 +16,7 @@ import PromiseKit
 class NotificationsAPI: APIEndpoint {
     override var name: String { return "notifications" }
 
-    func retrieve(page: Int = 1, notificationType: NotificationType? = nil, headers: [String: String] = AuthInfo.shared.initialHTTPHeaders) -> Promise<( [Notification], Meta)> {
+    func retrieve(page: Int = 1, notificationType: NotificationType? = nil) -> Promise<( [Notification], Meta)> {
 
         var parameters = [
             "page": "\(page)"
@@ -28,46 +28,6 @@ class NotificationsAPI: APIEndpoint {
 
         return retrieve.requestWithFetching(requestEndpoint: "notifications", paramName: "notifications", params: parameters, withManager: manager)
     }
-
-//    func retrieve(page: Int = 1, notificationType: NotificationType? = nil, headers: [String: String] = AuthInfo.shared.initialHTTPHeaders) -> Promise<(Meta, [Notification])> {
-//
-//        return Promise { fulfill, reject in
-//            var parameters = [
-//                "page": "\(page)"
-//            ]
-//
-//            if let notificationType = notificationType {
-//                parameters["type"] = notificationType.rawValue
-//            }
-//
-//            manager.request("\(StepicApplicationsInfo.apiURL)/\(self.name)", parameters: parameters, headers: headers).responseSwiftyJSON { response in
-//                switch response.result {
-//                case .failure(let error):
-//                    reject(RetrieveError(error: error))
-//                case .success(let json):
-//                    let savedNotifications = Notification.fetch(json["notifications"].arrayValue.map { $0["id"].intValue })
-//                    var newNotifications: [Notification] = []
-//                    for objectJSON in json["notifications"].arrayValue {
-//                        let existing = savedNotifications.filter { obj in obj.hasEqualId(json: objectJSON) }
-//
-//                        switch existing.count {
-//                        case 0:
-//                            newNotifications.append(Notification(json: objectJSON))
-//                        default:
-//                            let obj = existing[0]
-//                            obj.update(json: objectJSON)
-//                            newNotifications.append(obj)
-//                        }
-//                    }
-//
-//                    CoreDataHelper.instance.save()
-//
-//                    let meta = Meta(json: json["meta"])
-//                    fulfill((meta, newNotifications))
-//                }
-//            }
-//        }
-//    }
 
     func update(_ notification: Notification) -> Promise<Notification> {
         return update.request(requestEndpoint: "notifications", paramName: "notification", updatingObject: notification, withManager: manager)
