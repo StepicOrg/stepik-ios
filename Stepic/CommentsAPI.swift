55d428f6008cf3a2af40a873fcb4a911c84f50b8
Alexander Karpov <x1te.rus@gmail.com>
2018-03-30 21:39:56 +0300

    Added deprecations to CommentsAPI

--- Stepic/CommentsAPI.swift
+++ Stepic/CommentsAPI.swift
@@ -45,60 +45,6 @@ class CommentsAPI: APIEndpoint {
         }
     }
 
-    @discardableResult func retrieve(_ ids: [Int], headers: [String: String] = AuthInfo.shared.initialHTTPHeaders, success: @escaping ([Comment]) -> Void, error errorHandler: @escaping (String) -> Void) -> Request {
-        let idsString = ApiUtil.constructIdsString(array: ids)
-        return Alamofire.request("\(StepicApplicationsInfo.apiURL)/\(name)?\(idsString)", headers: headers).responseSwiftyJSON({
-                response in
-
-                var error = response.result.error
-                var json: JSON = [:]
-                if response.result.value == nil {
-                    if error == nil {
-                        error = NSError()
-                    }
-                } else {
-                    json = response.result.value!
-                }
-                let response = response.response
-
-                if let e = error as NSError? {
-                    errorHandler("RETRIEVE comments: error \(e.localizedDescription)")
-                    return
-                }
-
-                if response?.statusCode != 200 {
-                    errorHandler("RETRIEVE comments: bad response status code \(String(describing: response?.statusCode))")
-                    return
-                }
-
-                let comments: [Comment] = json["comments"].arrayValue.flatMap {
-                    Comment(json: $0)
-                }
-
-                var usersDict: [Int : UserInfo] = [Int: UserInfo]()
-
-                json["users"].arrayValue.forEach {
-                    let user = UserInfo(json: $0)
-                    usersDict[user.id] = user
-                }
-
-                var votesDict: [String: Vote] = [String: Vote]()
-
-                json["votes"].arrayValue.forEach({
-                    let vote = Vote(json: $0)
-                    votesDict[vote.id] = vote
-                })
-
-                for comment in comments {
-                    comment.userInfo = usersDict[comment.userId]
-                    comment.vote = votesDict[comment.voteId]
-                }
-
-                success(comments)
-            }
-        )
-    }
-
     func create(_ comment: Comment) -> Promise<Comment> {
         return Promise { fulfill, reject in
             create.request(requestEndpoint: "comments", paramName: "comment", creatingObject: comment, withManager: manager).then {
@@ -124,6 +70,19 @@ extension CommentsAPI {
     @available(*, deprecated, message: "Legacy method with callbacks")
     @discardableResult func create(_ comment: Comment, success: @escaping (Comment) -> Void, error errorHandler: @escaping (String) -> Void) -> Request? {
         create(comment).then { success($0) }.catch { errorHandler($0.localizedDescription) }
+        return nil
+    }
+    
+    
+    @available(*, deprecated, message: "Legacy method with callbacks")
+    @discardableResult func retrieve(_ ids: [Int], headers: [String: String] = AuthInfo.shared.initialHTTPHeaders, success: @escaping ([Comment]) -> Void, error errorHandler: @escaping (String) -> Void) -> Request? {
+        retrieve(ids: ids).then {
+            comments in
+            success(comments)
+            }.catch {
+                error in
+                errorHandler(error.localizedDescription)
+        }
         return nil
     }
 }
