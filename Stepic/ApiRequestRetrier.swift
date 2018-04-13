6b661b3d30b104fe1b241f80cf3adf763b2ab7be
Vladislav Kiryukhin <kiryukhinv@gmail.com>
2018-04-13 17:38:06 +0300

    Set user-agent for adapted requests

--- Stepic/ApiRequestRetrier.swift
+++ Stepic/ApiRequestRetrier.swift
@@ -10,9 +10,26 @@ import Alamofire
 import PromiseKit
 
 class ApiRequestRetrier: RequestRetrier, RequestAdapter {
+    private func buildUserAgent() -> String {
+        if let bundleId = Bundle.main.bundleIdentifier,
+           let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String,
+           let build = Bundle.main.infoDictionary?[kCFBundleVersionKey as String] as? String {
+            let osVersion = ["\(DeviceInfo.current.OSVersion.major)",
+                             "\(DeviceInfo.current.OSVersion.minor)",
+                             "\(DeviceInfo.current.OSVersion.patch)"].joined(separator: ".")
+            return "Stepik/\(version) (\(bundleId); build \(build); iOS \(osVersion))"
+        }
+
+        return "Stepik (iOS app)"
+    }
 
     func adapt(_ urlRequest: URLRequest) throws -> URLRequest {
         var urlRequest = urlRequest
+
+        // Add user-agent header
+        urlRequest.setValue(buildUserAgent(), forHTTPHeaderField: "User-Agent")
+
+        // Add cookie/token headers
         for (headerField, value) in AuthInfo.shared.initialHTTPHeaders {
             urlRequest.setValue(value, forHTTPHeaderField: headerField)
         }
