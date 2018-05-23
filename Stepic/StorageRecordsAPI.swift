4a16c10f25c5063654b6cf5726d469f726520e83
Alexander Karpov <x1te.rus@gmail.com>
2018-05-23 23:00:25 +0300

    Added StorageRecordsAPI class

---
+++ Stepic/StorageRecordsAPI.swift
@@ -0,0 +1,36 @@
+//
+//  StorageRecordsAPI.swift
+//  Stepic
+//
+//  Created by Ostrenkiy on 23.05.2018.
+//  Copyright © 2018 Alex Karpov. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+import PromiseKit
+
+class StorageRecordsAPI: APIEndpoint {
+    override var name: String { return "storage-records" }
+    
+    func retrieve(kind: StorageKind?, user: Int) -> Promise<([StorageRecord], Meta)> {
+        let params: Parameters = [
+            "kind": kind?.getName() ?? "",
+            "user": user
+        ]
+        return retrieve.request(requestEndpoint: "storage-records", paramName: "storage-records", params: params, withManager: manager)
+    }
+    
+    func delete(id: Int) -> Promise<Void> {
+        return delete.request(requestEndpoint: "storage-records", deletingId: id, withManager: manager)
+    }
+    
+    func create(record: StorageRecord) -> Promise<StorageRecord> {
+        return create.request(requestEndpoint: "storage-records", paramName: "storage-record", creatingObject: record, withManager: manager)
+    }
+    
+    func update(record: StorageRecord) -> Promise<StorageRecord> {
+        return update.request(requestEndpoint: "storage-records", paramName: "storage-record", updatingObject: record, withManager: manager)
+    }
+}
