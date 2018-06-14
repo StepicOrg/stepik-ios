b26dce96376d383da678bafe2536827699feb9d4
Alexander Karpov <x1te.rus@gmail.com>
2018-06-14 16:13:14 +0300

    removed string constants

--- Stepic/SettingsMenuBlock.swift
+++ Stepic/SettingsMenuBlock.swift
@@ -9,17 +9,17 @@
 import Foundation
 
 enum SettingsMenuBlock: String {
-    case videoHeader = "video_header"
-    case onlyWifiSwitch = "only_wifi_switch"
-    case loadedVideoQuality = "loaded_video_quality"
-    case onlineVideoQuality = "online_video_quality"
-    case adaptiveHeader = "adaptive_header"
-    case adaptiveModeSwitch = "use_adaptive_mode"
-    case codeEditorSettingsHeader = "code_editor_header"
-    case codeEditorSettings = "code_editor_settings"
-    case downloads = "downloads"
-    case logout = "logout"
-    case emptyHeader = "empty_header"
-    case languageSettingsHeader = "language_settings"
-    case contentLanguage = "content_language_settings"
+    case videoHeader
+    case onlyWifiSwitch
+    case loadedVideoQuality
+    case onlineVideoQuality
+    case adaptiveHeader
+    case adaptiveModeSwitch
+    case codeEditorSettingsHeader
+    case codeEditorSettings
+    case downloads
+    case logout
+    case emptyHeader
+    case languageSettingsHeader
+    case contentLanguage
 }
