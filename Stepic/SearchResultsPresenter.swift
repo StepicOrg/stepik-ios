0e26cd73f7d44d81bc6d83b05939e1395a791536
Alexander Karpov <x1te.rus@gmail.com>
2018-06-20 17:52:15 +0300

    Added search event

--- Stepic/SearchResultsPresenter.swift
+++ Stepic/SearchResultsPresenter.swift
@@ -78,6 +78,7 @@ class SearchResultsPresenter {
 extension SearchResultsPresenter: SearchQueriesViewControllerDelegate {
     func didSelectSuggestion(suggestion: String, position: Int) {
         AnalyticsReporter.reportEvent(AnalyticsEvents.Search.selected, parameters: ["query": self.query.lowercased(), "position": position, "suggestion": suggestion])
+        AnalyticsReporter.reportEvent(AmplitudeAnalyticsEvents.Search.searched, parameters: ["query": self.query.lowercased(), "position": position, "suggestion": suggestion.lowercased()])
         search(query: suggestion)
         updateQueryBlock?(suggestion)
     }
