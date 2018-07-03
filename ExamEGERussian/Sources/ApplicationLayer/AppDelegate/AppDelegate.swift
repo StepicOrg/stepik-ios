e478a8c887e8cbaad6abec24170a23a1c6935816
Ivan Magda <imagda15@gmail.com>
2018-07-03 15:02:18 +0300

    Create ExamEGERussian target

---
+++ ExamEGERussian/Sources/ApplicationLayer/AppDelegate/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ExamEGERussian
+//
+//  Created by Ivan Magda on 03/07/2018.
+//  Copyright © 2018 Alex Karpov. All rights reserved.
+//
+
+import UIKit
+
+// MARK: AppDelegate: UIResponder, UIApplicationDelegate
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // MARK: - Instance Properties
+    
+    var window: UIWindow?
+    
+    // MARK: - UIApplicationDelegate
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+}
---
+++ ExamEGERussian/Sources/PresentationLayer/ViewControllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ExamEGERussian
+//
+//  Created by Ivan Magda on 03/07/2018.
+//  Copyright © 2018 Alex Karpov. All rights reserved.
+//
+
+import UIKit
+
+// MARK: ViewController: UIViewController
+
+final class ViewController: UIViewController {
+
+    // MARK: - UIViewController Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
