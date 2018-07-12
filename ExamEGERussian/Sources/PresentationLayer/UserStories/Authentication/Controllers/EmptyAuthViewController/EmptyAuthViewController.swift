c19b173e1673c205ac7ff45cb725de56f5068691
Ivan Magda <imagda15@gmail.com>
2018-07-13 02:57:55 +0300

    Create EmptyAuthViewController

---
+++ ExamEGERussian/Sources/PresentationLayer/UserStories/Authentication/Controllers/EmptyAuthViewController/EmptyAuthViewController.swift
@@ -0,0 +1,61 @@
+//
+//  EmptyAuthViewController.swift
+//  ExamEGERussian
+//
+//  Created by Ivan Magda on 12/07/2018.
+//  Copyright © 2018 Alex Karpov. All rights reserved.
+//
+
+import UIKit
+
+final class EmptyAuthViewController: UIViewController {
+
+    private let gradientLayer = CAGradientLayer(
+        colors: [UIColor.stepicGreen, UIColor(hex: 0x4CAF50), UIColor(hex: 0x8BC34A)],
+        rotationAngle: 0.0
+    )
+
+    // MARK: UIViewController Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        gradientLayer.frame = view.bounds
+    }
+
+    // MARK: Actions
+
+    @IBAction func onSignIn(_ sender: Any) {
+    }
+
+    @IBAction func onSignUp(_ sender: Any) {
+    }
+
+    @IBAction func onLater(_ sender: Any) {
+        dismiss(sender)
+    }
+    
+    // MARK: Private API
+
+    private func setup() {
+        view.backgroundColor = .clear
+        view.layer.insertSublayer(gradientLayer, at: 0)
+
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: UIImage(named: "close"),
+            style: .plain,
+            target: self,
+            action: #selector(dismiss(_:))
+        )
+        navigationItem.leftBarButtonItem?.tintColor = .white
+    }
+
+    @objc private func dismiss(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+
+}
---
+++ ExamEGERussian/Sources/PresentationLayer/Views/RoundedButton.swift
@@ -0,0 +1,103 @@
+//
+//  RoundedButton.swift
+//  ExamEGERussian
+//
+//  Created by Ivan Magda on 13/07/2018.
+//  Copyright © 2018 Alex Karpov. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+public class RoundedButton: UIButton {
+
+    @IBInspectable
+    public var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+
+    @IBInspectable
+    public var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable
+    public var borderColor: UIColor? {
+        get {
+            guard let cgColor = layer.borderColor else { return nil }
+            return UIColor(cgColor: cgColor)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+
+    @IBInspectable
+    public var bottomInset: CGFloat {
+        get {
+            return contentInsets.bottom
+        }
+        set {
+            contentInsets.bottom = newValue
+        }
+    }
+    @IBInspectable
+    public var leftInset: CGFloat {
+        get {
+            return contentInsets.left
+        }
+        set {
+            contentInsets.left = newValue
+        }
+    }
+    @IBInspectable
+    public var rightInset: CGFloat {
+        get {
+            return contentInsets.right
+        }
+        set {
+            contentInsets.right = newValue
+        }
+    }
+    @IBInspectable
+    public var topInset: CGFloat {
+        get {
+            return contentInsets.top
+        }
+        set {
+            contentInsets.top = newValue
+        }
+    }
+
+    private var contentInsets = UIEdgeInsets.zero {
+        didSet {
+            contentEdgeInsets = contentInsets
+        }
+    }
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    private func commonInit() {
+        clipsToBounds = true
+        contentEdgeInsets = contentInsets
+    }
+
+}
