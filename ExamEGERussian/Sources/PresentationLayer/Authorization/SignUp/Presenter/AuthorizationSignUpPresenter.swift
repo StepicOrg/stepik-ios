6177265975fcf4abcd48998f14a0275deb3803b0
Ivan Magda <imagda15@gmail.com>
2018-07-18 13:12:08 +0300

    Add comment

--- ExamEGERussian/Sources/PresentationLayer/Authorization/SignUp/Presenter/AuthorizationSignUpPresenter.swift
+++ ExamEGERussian/Sources/PresentationLayer/Authorization/SignUp/Presenter/AuthorizationSignUpPresenter.swift
@@ -11,6 +11,7 @@ import PromiseKit
 
 final class AuthorizationSignUpPresenter: RegistrationPresenter {
     override func register(with name: String, email: String, password: String) {
+        // When a user logged in, the API returns 403 status code.
         AuthInfo.shared.clearToken {
             super.register(with: name, email: email, password: password)
         }
