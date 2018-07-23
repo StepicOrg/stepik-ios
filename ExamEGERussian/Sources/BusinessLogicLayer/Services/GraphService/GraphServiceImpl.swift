2b48623ca56af0407cf87493d33ca77bed23ddbe
Ivan Magda <imagda15@gmail.com>
2018-07-23 15:14:07 +0300

    Use Alamofire in graph service implementation

--- ExamEGERussian/Sources/BusinessLogicLayer/Services/GraphService/GraphServiceImpl.swift
+++ ExamEGERussian/Sources/BusinessLogicLayer/Services/GraphService/GraphServiceImpl.swift
@@ -8,15 +8,16 @@
 
 import Foundation
 import PromiseKit
+import Alamofire
 
 final class GraphServiceImpl: GraphService {
     private static let url = URL(string: "https://www.dropbox.com/s/l8n1wny8qu0gbqt/example.json?dl=1")!
-    
+
     func obtainGraph(_ completionHandler: @escaping (StepicResult<KnowledgeGraphPlainObject>) -> Void) {
         firstly {
-            URLSession.shared.dataTask(.promise, with: GraphServiceImpl.url).validate()
-        }.map {
-            try JSONDecoder().decode(KnowledgeGraphPlainObject.self, from: $0.data)
+            Alamofire
+                .request(GraphServiceImpl.url)
+                .responseDecodable(KnowledgeGraphPlainObject.self)
         }.done {
             completionHandler(.success($0))
         }.catch {
