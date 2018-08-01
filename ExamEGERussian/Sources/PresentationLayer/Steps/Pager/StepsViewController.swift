8adcce487162445af8382c439d9ce1715a6b4285
Ivan Magda <imagda15@gmail.com>
2018-08-01 20:08:01 +0300

    Display tabs and their corresponding content

--- ExamEGERussian/Sources/PresentationLayer/Steps/Pager/StepsViewController.swift
+++ ExamEGERussian/Sources/PresentationLayer/Steps/Pager/StepsViewController.swift
@@ -8,19 +8,22 @@
 
 import UIKit
 import SnapKit
-import Pager
+import PromiseKit
 
 final class StepsViewController: PagerController {
+
+    // MARK: Instance Properties
+
     private let lesson: LessonPlainObject
     private var steps = [StepPlainObject]() {
         didSet {
-            assert(Thread.isMainThread)
-            self.view.layoutIfNeeded()
             self.reloadData()
         }
     }
 
     private let stepsService: StepsService
+
+    // MARK: - Init
 
     init(lesson: LessonPlainObject, stepsService: StepsService) {
         self.lesson = lesson
@@ -32,23 +35,62 @@ final class StepsViewController: PagerController {
         fatalError("init(coder:) has not been implemented")
     }
 
+    // MARK: - UIViewController Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .any, barMetrics: .default)
-        navigationController?.navigationBar.shadowImage = UIImage()
-        navigationItem.backBarButtonItem?.title = " "
-
-        title = lesson.title
-        view.backgroundColor = .white
-
-        initTabs()
-        dataSource = self
-
+        setup()
         fetchSteps()
     }
 
-    private func initTabs() {
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.showBottomHairline()
+    }
+
+    // MARK: - Overrides
+
+    override func makeConstraints() {
+        self.tabsView!.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(self.view)
+            make.height.equalTo(44)
+        }
+
+        self.contentView.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(self.view)
+            make.top.equalTo(self.tabsView!.snp.bottom)
+            make.bottom.equalTo(self.view)
+        }
+
+        let shadowView = UIView()
+        self.contentView.addSubview(shadowView)
+        shadowView.backgroundColor = .lightGray
+        shadowView.snp.makeConstraints { make in
+            make.height.equalTo(0.5)
+            make.top.equalTo(contentView)
+            make.leading.trailing.equalTo(contentView)
+        }
+    }
+
+    // MARK: - Private API -
+
+    private func setup() {
+        edgesForExtendedLayout = [.left, .right, .bottom]
+        navigationController?.view.backgroundColor = .white
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.hideBottomHairline()
+
+        view.backgroundColor = .white
+        title = lesson.title
+
+        setupTabs()
+        dataSource = self
+    }
+
+    private func setupTabs() {
         indicatorColor = .mainDark
         tabsViewBackgroundColor = .white
 
@@ -58,19 +100,37 @@ final class StepsViewController: PagerController {
         tabOffset = 8.0
 
         centerCurrentTab = true
-//        fixFormerTabsPositions = true
         fixLaterTabsPosition = true
     }
 
     private func fetchSteps() {
         stepsService.fetchSteps(for: lesson).done { [weak self] steps in
-            DispatchQueue.main.async {
-                self?.steps = steps.filter { $0.type == .text }
-            }
+            self?.steps = steps.filter { $0.type == .text }
         }.catch { error in
             print(error)
         }
     }
+}
+
+// MARK: - StepsViewController: PagerDataSource -
+
+extension StepsViewController: PagerDataSource {
+    public func numberOfTabs(_ pager: PagerController) -> Int {
+        return steps.count
+    }
+
+    public func tabViewForIndex(_ index: Int, pager: PagerController) -> UIView {
+        let step = steps[index]
+        let frame = CGRect(origin: .zero, size: CGSize(width: 25, height: 25))
+
+        return StepTabView(frame: frame, image: step.image, stepId: step.id, passed: false)
+    }
+
+    public func controllerForTabAtIndex(_ index: Int, pager: PagerController) -> UIViewController {
+        return stepController(for: index)
+    }
+
+    // MARK: Private Helpers
 
     private func stepController(for index: Int) -> StepViewController {
         let controller = StepViewController()
@@ -85,19 +145,31 @@ final class StepsViewController: PagerController {
     }
 }
 
-extension StepsViewController: PagerDataSource {
-    public func numberOfTabs(_ pager: PagerController) -> Int {
-        return steps.count
+// MARK: - UINavigationBar+hideBottomHairline -
+
+private extension UINavigationBar {
+    func hideBottomHairline() {
+        let navigationBarImageView = hairlineImageViewInNavigationBar(self)
+        navigationBarImageView!.isHidden = true
     }
 
-    public func tabViewForIndex(_ index: Int, pager: PagerController) -> UIView {
-        let step = steps[index]
-        let rect = CGRect(origin: .zero, size: CGSize(width: 25, height: 25))
-
-        return StepTabView(frame: rect, image: step.image, stepId: step.id, passed: false)
+    func showBottomHairline() {
+        let navigationBarImageView = hairlineImageViewInNavigationBar(self)
+        navigationBarImageView!.isHidden = false
     }
 
-    public func controllerForTabAtIndex(_ index: Int, pager: PagerController) -> UIViewController {
-        return stepController(for: index)
+    private func hairlineImageViewInNavigationBar(_ view: UIView) -> UIImageView? {
+        if let view = view as? UIImageView, view.bounds.size.height <= 1.0 {
+            return view
+        }
+
+        let subviews = (view.subviews as [UIView])
+        for subview: UIView in subviews {
+            if let imageView: UIImageView = hairlineImageViewInNavigationBar(subview) {
+                return imageView
+            }
+        }
+
+        return nil
     }
 }
