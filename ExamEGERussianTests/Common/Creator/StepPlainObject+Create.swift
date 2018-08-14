57fe33d92d45a837dfd942a2dcf3dbdb80fe630d
Ivan Magda <imagda15@gmail.com>
2018-08-14 19:53:05 +0300

    Test StepPlainObject

--- ExamEGERussianTests/Common/Creator/StepPlainObject+Create.swift
+++ ExamEGERussianTests/Common/Creator/StepPlainObject+Create.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+@testable import ExamEGERussian
+
+extension StepPlainObject {
+    private static let allTypes: [StepPlainObject.StepType] = [
+        .text, .choice, .string, .number, .freeAnswer, .math, .sorting, .matching, .fillBlanks,
+        .code, .sql, .table, .video, .dataset, .admin
+    ]
+
+    static func create(type: StepPlainObject.StepType? = nil) -> StepPlainObject {
+        let stepType = type == nil
+            ? StepPlainObject.allTypes.randomElement()!
+            : type!
+
+        return StepPlainObject(
+            id: randomNumber(),
+            lessonId: randomNumber(),
+            position: randomNumber(),
+            text: "Some text goes here",
+            type: stepType,
+            progressId: nil,
+            isPassed: false
+        )
+    }
+
+    private static func randomNumber() -> Int {
+        return Int.random(in: 1...100)
+    }
+}
