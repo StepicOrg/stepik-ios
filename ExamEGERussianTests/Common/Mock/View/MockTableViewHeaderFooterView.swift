003d28d9775885d28e88ad18e6fa108ddbb321ca
Ivan Magda <imagda15@gmail.com>
2018-08-16 16:58:28 +0300

    Test UITableView specific extensions

---
+++ ExamEGERussianTests/Common/Mock/View/MockTableViewHeaderFooterView.swift
@@ -0,0 +1,12 @@
+//
+//  MockTableViewHeaderFooterView.swift
+//  ExamEGERussianTests
+//
+//  Created by Ivan Magda on 16/08/2018.
+//  Copyright © 2018 Alex Karpov. All rights reserved.
+//
+
+import UIKit
+
+final class MockTableViewHeaderFooterView: UITableViewHeaderFooterView {
+}
