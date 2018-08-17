3dc72db7626594ab92075d331fde5cafdc9cb55d
Ivan Magda <imagda15@gmail.com>
2018-08-17 14:22:21 +0300

    Create ReactionService

---
+++ ExamEGERussianTests/Common/Mock/API/ReactionsAPIMock.swift
@@ -0,0 +1,19 @@
+//
+//  ReactionsAPIMock.swift
+//  ExamEGERussianTests
+//
+//  Created by Ivan Magda on 17/08/2018.
+//  Copyright © 2018 Alex Karpov. All rights reserved.
+//
+
+import Foundation
+import PromiseKit
+@testable import ExamEGERussian
+
+final class ReactionsAPIMock: RecommendationsAPI, PromiseReturnable {
+    var resultToBeReturned: Promise<Void> = Promise(error: NSError.mockError)
+
+    override func sendReaction(user userId: Int, lesson lessonId: Int, reaction: Reaction, headers: [String : String]) -> Promise<Void> {
+        return resultToBeReturned
+    }
+}
