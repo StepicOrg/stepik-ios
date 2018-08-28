8354d95730ff5b8714c3b33c3fcc7063290bd34f
Ivan Magda <imagda15@gmail.com>
2018-08-28 10:09:25 +0300

    Make tab bar controller as a root controller (#354)
    * Make tab bar controller application root view controller
    
    * Hides tab bar on push

--- ExamEGERussian/Sources/ApplicationLayer/Assembly/ApplicationAssemblyImpl.swift
+++ ExamEGERussian/Sources/ApplicationLayer/Assembly/ApplicationAssemblyImpl.swift
@@ -14,18 +14,68 @@ final class ApplicationAssemblyImpl: BaseAssembly, ApplicationAssembly {
             return savedModule
         }
 
-        let navigationController = UINavigationController()
-        let controller = assemblyFactory.topicsAssembly.module(navigationController: navigationController)
-        navigationController.setViewControllers([controller], animated: false)
+        let tabBarController = UITabBarController()
+        // Hides dark shadow on navigation bar during transition.
+        tabBarController.view.backgroundColor = .white
+
+        let learningNavigationController = makeLearningController()
+        let trainingNavigationController = makeTrainingController()
+
+        tabBarController.setViewControllers(
+            [learningNavigationController, trainingNavigationController],
+            animated: false
+        )
+        tabBarController.selectedIndex = 0
 
         let router = AppRouter(
-            assemblyFactory: assemblyFactory,
-            navigationController: navigationController
+            tabBarController: tabBarController,
+            navigationController: learningNavigationController,
+            assemblyFactory: assemblyFactory
         )
         let applicationModule = ApplicationModule(router: router)
 
         ApplicationModuleHolder.instance.applicationModule = applicationModule
 
         return applicationModule
+    }
+
+    private func makeLearningController() -> UINavigationController {
+        let navigationController = UINavigationController()
+        guard let controller = assemblyFactory.topicsAssembly.module(
+            navigationController: navigationController
+        ) as? TopicsTableViewController else {
+            fatalError("TopicsTableViewController expected")
+        }
+
+        controller.title = NSLocalizedString("LearningTabTitle", comment: "")
+        controller.tabBarItem = UITabBarItem(
+            title: controller.title,
+            image: UIImage(named: "learning-tab-bar"),
+            tag: 0
+        )
+        controller.presenter.selectSegment(at: 0)
+        navigationController.setViewControllers([controller], animated: false)
+
+        return navigationController
+    }
+
+    private func makeTrainingController() -> UINavigationController {
+        let navigationController = UINavigationController()
+        guard let controller = assemblyFactory.topicsAssembly.module(
+            navigationController: navigationController
+        ) as? TopicsTableViewController else {
+            fatalError("TopicsTableViewController expected")
+        }
+
+        controller.title = NSLocalizedString("TrainingTabTitle", comment: "")
+        controller.tabBarItem = UITabBarItem(
+            title: controller.title,
+            image: UIImage(named: "training-tab-bar"),
+            tag: 1
+        )
+        controller.presenter.selectSegment(at: 1)
+        navigationController.setViewControllers([controller], animated: false)
+
+        return navigationController
     }
 }
