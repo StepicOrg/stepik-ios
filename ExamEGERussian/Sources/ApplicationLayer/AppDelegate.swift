daf8497fc1c5b8f0b655112edd38794ab8945757
Ivan Magda <imagda15@gmail.com>
2018-09-18 11:30:33 +0300

    Format code

--- ExamEGERussian/Sources/ApplicationLayer/AppDelegate.swift
+++ ExamEGERussian/Sources/ApplicationLayer/AppDelegate.swift
@@ -12,7 +12,10 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+    ) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         guard let window = window else {
             fatalError("Could't initialize window")
