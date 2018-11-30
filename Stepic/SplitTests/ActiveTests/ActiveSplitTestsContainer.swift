c37e2d516f70f147bdae88454569488dfe48af7c
Ivan Magda <imagda15@gmail.com>
2018-11-30 19:10:14 +0300

    Subscribe to notifications on launch AB (#416)
    * Create group with folder
    
    * Split test
    
    * Fix
    
    * Respect current group
    
    * Do not request authorization before onboarding
    
    * Request authorization after onboarding
    
    * Request authorization for all users if they participants
    
    * Make decisions based on current group
    
    * Refactor rename isParticipant -> shouldShowOnFirstLaunch
    
    * Suggest streak

--- Stepic/SplitTests/ActiveTests/ActiveSplitTestsContainer.swift
+++ Stepic/SplitTests/ActiveTests/ActiveSplitTestsContainer.swift
@@ -12,5 +12,6 @@ class ActiveSplitTestsContainer {
     private static let splitTestingService = SplitTestingService(analyticsService: AnalyticsUserProperties(), storage: UserDefaults.standard)
 
     static func setActiveTestsGroups() {
+        self.splitTestingService.fetchSplitTest(SubscribeNotificationsOnLaunchSplitTest.self).setSplitTestGroup()
     }
 }
