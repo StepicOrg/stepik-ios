f1601b118180554d121e3d86922da9a10448e885
Vladislav Kiryukhin <kiryukhinv@gmail.com>
2019-06-05 16:21:18 +0300

    Remove height attribute for images

--- Stepic/Sources/Frameworks/ContentProcessor/ContentProcessingRule.swift
+++ Stepic/Sources/Frameworks/ContentProcessor/ContentProcessingRule.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Regex
 
 protocol ContentProcessingRule: class {
     func process(content: String) -> String
@@ -27,8 +28,8 @@ final class FixRelativeProtocolURLsRule: ContentProcessingRule {
 /// Add Stepik domain to all relative paths, e.g replace "/content" with "http://stepik.org/content"
 final class AddStepikSiteForRelativeURLsRule: BaseHTMLExtractionRule {
     override func process(content: String) -> String {
-        let links = self.extractorType.extractAllATagLinks(from: content).map { $0.link }
-            + self.extractorType.extractAllImageTagSrcLinks(from: content)
+        let links = self.extractorType.extractAllTagsAttribute(tag: "a", attribute: "href", from: content)
+            + self.extractorType.extractAllTagsAttribute(tag: "img", attribute: "src", from: content)
 
         var content = content
         for link in Array(Set(links)) {
@@ -52,5 +53,23 @@ final class AddStepikSiteForRelativeURLsRule: BaseHTMLExtractionRule {
         }
 
         return "\(StepicApplicationsInfo.stepicURL)\(link)"
+    }
+}
+
+/// Remove image fixed height
+final class RemoveImageFixedHeightRule: BaseHTMLExtractionRule {
+    override func process(content: String) -> String {
+        var content = content
+
+        let images = self.extractorType.extractAllTags(tag: "img", from: content)
+        for image in images {
+            var replacedImage = image
+            if let regex = try? Regex(string: "(height=\"\\d+\")", options: [.ignoreCase]) {
+                replacedImage.replaceFirst(matching: regex, with: "")
+            }
+
+            content = content.replacingOccurrences(of: image, with: replacedImage)
+        }
+        return content
     }
 }
