3a43f81382a7d47b4e20724a1f334fdb6ba7f4fe
Ivan Magda <imagda15@gmail.com>
2019-06-21 17:58:45 +0300

    Check for existing percent escapes first to prevent double-escaping

--- Stepic/DiscussionAlertConstructor.swift
+++ Stepic/DiscussionAlertConstructor.swift
@@ -8,45 +8,50 @@
 
 import Foundation
 
-class DiscussionAlertConstructor {
-    static func getCommentAlert(_ comment: Comment, replyBlock: @escaping (() -> Void), likeBlock: @escaping (() -> Void), abuseBlock: @escaping (() -> Void), openURLBlock: @escaping ((URL) -> Void)) -> UIAlertController {
+final class DiscussionAlertConstructor {
+    static func getCommentAlert(
+        _ comment: Comment,
+        replyBlock: @escaping (() -> Void),
+        likeBlock: @escaping (() -> Void),
+        abuseBlock: @escaping (() -> Void),
+        openURLBlock: @escaping ((URL) -> Void)
+    ) -> UIAlertController {
         let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
 
-        let links = HTMLParsingUtil.getAllLinksWithText(comment.text)
-
-        for link in links {
-            alert.addAction(UIAlertAction(title: link.text, style: .default, handler: {
-                    _ in
-                    if let url = URL(string: link.link.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!) {
-                        openURLBlock(url)
-                    }
-                })
-            )
+        let links = HTMLParsingUtil.getAllLinksWithText(comment.text).compactMap { item -> (url: URL, text: String)? in
+            if let url = URL(stringToEncode: item.link) {
+                return (url: url, text: item.text)
+            } else {
+                return nil
+            }
         }
 
-        alert.addAction(UIAlertAction(title: NSLocalizedString("Reply", comment: ""), style: .default, handler: {
-                _ in
-                replyBlock()
-        })
-        )
+        for link in links {
+            alert.addAction(UIAlertAction(title: link.text, style: .default, handler: { _ in
+                openURLBlock(link.url)
+            }))
+        }
+
+        alert.addAction(UIAlertAction(title: NSLocalizedString("Reply", comment: ""), style: .default, handler: { _ in
+            replyBlock()
+        }))
 
         if comment.userId != AuthInfo.shared.userId {
-
-            let likeTitle: String = (comment.vote.value == VoteValue.Epic) ? NSLocalizedString("Unlike", comment: "") : NSLocalizedString("Like", comment: "")
-
+            let likeTitle: String = (comment.vote.value == VoteValue.Epic)
+                ? NSLocalizedString("Unlike", comment: "")
+                : NSLocalizedString("Like", comment: "")
             alert.addAction(UIAlertAction(title: likeTitle, style: .default, handler: {
-                    _ in
-                    likeBlock()
-            })
-            )
+                _ in
+                likeBlock()
+            }))
 
-            let abuseTitle: String = (comment.vote.value == VoteValue.Abuse) ? NSLocalizedString("Unabuse", comment: "") : NSLocalizedString("Abuse", comment: "")
-
+            let abuseTitle: String = (comment.vote.value == VoteValue.Abuse)
+                ? NSLocalizedString("Unabuse", comment: "")
+                : NSLocalizedString("Abuse", comment: "")
             alert.addAction(UIAlertAction(title: abuseTitle, style: .destructive, handler: {
-                    _ in
-                    abuseBlock()
-                })
-            )
+                _ in
+                abuseBlock()
+            }))
         }
         alert.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .cancel, handler: nil))
 
