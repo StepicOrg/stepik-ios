fbec4c6f2fdb0082dffa360240ce10cefe2b111c
Ivan Magda <ivan.magda@stepik.org>
2019-07-04 15:07:33 +0300

    Create CommentsNetworkService

---
+++ Stepic/Sources/Services/Models/Network/CommentsNetworkService.swift
@@ -0,0 +1,40 @@
+import Foundation
+import PromiseKit
+
+protocol CommentsNetworkServiceProtocol: class {
+    func fetch(ids: [Comment.IdType]) -> Promise<[Comment]>
+    func create(comment: Comment) -> Promise<Comment>
+}
+
+final class CommentsNetworkService: CommentsNetworkServiceProtocol {
+    private let commentsAPI: CommentsAPI
+
+    init(commentsAPI: CommentsAPI) {
+        self.commentsAPI = commentsAPI
+    }
+
+    func fetch(ids: [Comment.IdType]) -> Promise<[Comment]> {
+        return Promise { seal in
+            self.commentsAPI.retrieve(ids: ids).done { comments in
+                let comments = comments.reordered(order: ids, transform: { $0.id })
+                seal.fulfill(comments)
+            }.catch { _ in
+                seal.reject(Error.fetchFailed)
+            }
+        }
+    }
+
+    func create(comment: Comment) -> Promise<Comment> {
+        return Promise { seal in
+            self.commentsAPI.create(comment).done { comment in
+                seal.fulfill(comment)
+            }.catch { _ in
+                seal.reject(Error.fetchFailed)
+            }
+        }
+    }
+
+    enum Error: Swift.Error {
+        case fetchFailed
+    }
+}
