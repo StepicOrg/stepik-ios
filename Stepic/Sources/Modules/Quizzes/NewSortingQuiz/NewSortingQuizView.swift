bb858bcde3b382134324db282c1cad01194e5c6e
Ivan Magda <ivan.magda@stepik.org>
2019-08-21 19:06:34 +0300

    use stack view

--- Stepic/Sources/Modules/Quizzes/NewSortingQuiz/NewSortingQuizView.swift
+++ Stepic/Sources/Modules/Quizzes/NewSortingQuiz/NewSortingQuizView.swift
@@ -11,47 +11,30 @@ protocol NewSortingQuizViewDelegate: class {
 }
 
 extension NewSortingQuizView {
-    struct Appearance {
+    enum Animation {
+        static let appearanceAnimationDuration: TimeInterval = 0.33
+        static let moveSortingOptionAnimationDuration: TimeInterval = 0.33
     }
 }
 
 final class NewSortingQuizView: UIView {
-    let appearance: Appearance
     weak var delegate: NewSortingQuizViewDelegate?
 
-    private lazy var tableView: UITableView = {
-        let tableView = FullHeightTableView()
-        tableView.isScrollEnabled = false
-        tableView.showsVerticalScrollIndicator = false
-        tableView.showsHorizontalScrollIndicator = false
-        tableView.backgroundColor = .clear
-        tableView.separatorStyle = .none
-        tableView.allowsSelection = false
-
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.estimatedRowHeight = 100.0
-
-        tableView.register(cellClass: NewSortingQuizTableViewCell.self)
-
-        tableView.dataSource = self
-        tableView.delegate = self
-
-        return tableView
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        return stackView
     }()
 
     var isEnabled = true {
         didSet {
-            self.tableView.isUserInteractionEnabled = self.isEnabled
+            self.stackView.isUserInteractionEnabled = self.isEnabled
         }
     }
 
     private(set) var options: [NewSortingQuiz.Option] = []
 
-    init(
-        frame: CGRect = .zero,
-        appearance: Appearance = Appearance()
-    ) {
-        self.appearance = appearance
+    override init(frame: CGRect = .zero) {
         super.init(frame: frame)
 
         self.setupView()
@@ -64,67 +47,40 @@ final class NewSortingQuizView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
-    override func layoutSubviews() {
-        super.layoutSubviews()
-
-        print(self.tableView.frame.size)
-        print(self.tableView.contentSize)
-    }
-
     func set(options: [NewSortingQuiz.Option]) {
-        self.options = options
-        self.tableView.reloadData()
-    }
-}
-
-extension NewSortingQuizView: ProgrammaticallyInitializableViewProtocol {
-    func addSubviews() {
-        self.addSubview(self.tableView)
-    }
-
-    func makeConstraints() {
-        self.tableView.translatesAutoresizingMaskIntoConstraints = false
-        self.tableView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+        if self.options.map({ $0.text }) == options.map({ $0.text }) {
+            return
         }
-    }
-}
 
-extension NewSortingQuizView: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.options.count
-    }
+        self.options = options
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: NewSortingQuizTableViewCell = tableView.dequeueReusableCell(for: indexPath)
-        cell.updateConstraintsIfNeeded()
-
-        let option = self.options[indexPath.row]
-
-        cell.tag = option.id
-        cell.delegate = self
-
-        cell.configure(
-            viewModel: .init(
-                text: option.text,
-                direction: self.getAvailableNavigationDirection(for: cell, atIndexPath: indexPath)
+        self.stackView.removeAllArrangedSubviews()
+        options.enumerated().forEach { index, option in
+            let sortingOptionView = NewSortingQuizElementView()
+            sortingOptionView.delegate = self
+            sortingOptionView.tag = option.id
+            sortingOptionView.configure(
+                viewModel: .init(
+                    text: option.text,
+                    direction: self.getAvailableNavigationDirectionAtIndex(index)
+                )
             )
-        )
 
-        return cell
+            self.stackView.addArrangedSubview(sortingOptionView)
+        }
+
+        self.stackView.setNeedsLayout()
+        self.stackView.layoutIfNeeded()
     }
 
-    private func getAvailableNavigationDirection(
-        for cell: NewSortingQuizTableViewCell,
-        atIndexPath indexPath: IndexPath
-    ) -> NewSortingQuizTableViewCell.Direction {
-        var direction: NewSortingQuizTableViewCell.Direction = []
+    private func getAvailableNavigationDirectionAtIndex(_ index: Int) -> NewSortingQuizElementView.Direction {
+        var direction: NewSortingQuizElementView.Direction = []
 
-        if indexPath.row != 0 {
+        if index != 0 {
             direction.insert(.top)
         }
 
-        if indexPath.row != self.options.count - 1 {
+        if index != self.options.count - 1 {
             direction.insert(.bottom)
         }
 
@@ -132,50 +88,63 @@ extension NewSortingQuizView: UITableViewDataSource {
     }
 }
 
-extension NewSortingQuizView: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
+extension NewSortingQuizView: ProgrammaticallyInitializableViewProtocol {
+    func addSubviews() {
+        self.addSubview(self.stackView)
     }
 
-    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
-        return nil
+    func makeConstraints() {
+        self.stackView.translatesAutoresizingMaskIntoConstraints = false
+        self.stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
 }
 
 extension NewSortingQuizView: NewSortingQuizTableViewCellDelegate {
-    func newSortingQuizTableViewCellDidLoadContent(_ view: NewSortingQuizTableViewCell) {
-        print(#function)
+    func newSortingQuizTableViewCellDidLoadContent(_ view: NewSortingQuizElementView) {
+        self.stackView.setNeedsLayout()
+        self.stackView.layoutIfNeeded()
     }
 
-    func newSortingQuizTableViewCellDidRequestMoveTop(_ view: NewSortingQuizTableViewCell) {
-        self.move(cell: view, direction: .top)
+    func newSortingQuizTableViewCellDidRequestMoveTop(_ view: NewSortingQuizElementView) {
+        self.move(view, direction: .top)
     }
 
-    func newSortingQuizTableViewCellDidRequestMoveDown(_ view: NewSortingQuizTableViewCell) {
-        self.move(cell: view, direction: .bottom)
+    func newSortingQuizTableViewCellDidRequestMoveDown(_ view: NewSortingQuizElementView) {
+        self.move(view, direction: .bottom)
     }
 
-    private func move(cell: NewSortingQuizTableViewCell, direction: Direction) {
-        guard let option = self.options.first(where: { $0.id == cell.tag }),
+    private func move(_ view: NewSortingQuizElementView, direction: Direction) {
+        guard let option = self.options.first(where: { $0.id == view.tag }),
               let sourceIndex = self.options.firstIndex(where: { $0.id == option.id }) else {
             return
         }
 
         let destinationIndex = direction == .top ? sourceIndex - 1 : sourceIndex + 1
-        let destinationIndexPath = IndexPath(row: destinationIndex, section: 0)
-        let sourceIndexPath = IndexPath(row: sourceIndex, section: 0)
 
-        self.tableView.moveRow(at: sourceIndexPath, to: destinationIndexPath)
+        self.stackView.removeArrangedSubview(view)
+        UIView.animate(
+            withDuration: Animation.moveSortingOptionAnimationDuration,
+            animations: {
+                self.stackView.insertArrangedSubview(view, at: destinationIndex)
+                self.stackView.setNeedsLayout()
+                self.stackView.layoutIfNeeded()
+            },
+            completion: { isFinished in
+                guard isFinished else {
+                    return
+                }
+
+                view.updateNavigation(self.getAvailableNavigationDirectionAtIndex(destinationIndex))
+                if let affectedView = self.stackView.arrangedSubviews[safe: sourceIndex] as? NewSortingQuizElementView {
+                    affectedView.updateNavigation(self.getAvailableNavigationDirectionAtIndex(sourceIndex))
+                }
+            }
+        )
 
         self.options.remove(at: sourceIndex)
         self.options.insert(option, at: destinationIndex)
-
-        cell.updateNavigation(self.getAvailableNavigationDirection(for: cell, atIndexPath: destinationIndexPath))
-        if let affectedCell = self.tableView.cellForRow(at: sourceIndexPath) as? NewSortingQuizTableViewCell {
-            affectedCell.updateNavigation(
-                self.getAvailableNavigationDirection(for: affectedCell, atIndexPath: sourceIndexPath)
-            )
-        }
 
         self.delegate?.newSortingQuizView(self, didMoveOption: option, atIndex: sourceIndex, toIndex: destinationIndex)
     }
