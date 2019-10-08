02813f2503792478a76215b39ee3a1e3915a318a
Ivan Magda <ivan.magda@stepik.org>
2019-10-08 03:04:01 +0300

    Navigate to new lesson from downloads

--- Stepic/DownloadTableViewCell.swift
+++ Stepic/DownloadTableViewCell.swift
@@ -6,24 +6,26 @@
 //  Copyright © 2015 Alex Karpov. All rights reserved.
 //
 
-import UIKit
 import DownloadButton
+import UIKit
 
-class DownloadTableViewCell: UITableViewCell {
+final class DownloadTableViewCell: UITableViewCell {
     @IBOutlet weak var thumbnailImageView: UIImageView!
     @IBOutlet weak var lessonNameLabel: StepikLabel!
     @IBOutlet weak var qualityLabel: StepikLabel!
     @IBOutlet weak var sizeLabel: StepikLabel!
 
-    var video: Video!
+    func configure(video: Video) {
+        self.thumbnailImageView.sd_setImage(
+            with: URL(string: video.thumbnailURL),
+            placeholderImage: Images.videoPlaceholder
+        )
+        self.lessonNameLabel.text = "\(video.managedBlock?.managedStep?.managedLesson?.title ?? "")"
 
-    func initWith( _ video: Video) {
-        thumbnailImageView.sd_setImage(with: URL(string: video.thumbnailURL), placeholderImage: Images.videoPlaceholder)
-        lessonNameLabel.text = "\(video.managedBlock?.managedStep?.managedLesson?.title ?? "")"
-        self.video = video
+        let videoStoredFileManager = VideoStoredFileManager(fileManager: .default)
+        let size = videoStoredFileManager.getVideoStoredFile(videoID: video.id)?.size ?? 0
 
-        let size = VideoStoredFileManager(fileManager: FileManager.default).getVideoStoredFile(videoID: video.id)?.size ?? 0
         self.sizeLabel.text = "\(size / 1024 / 1024) \(NSLocalizedString("Mb", comment: ""))"
-        self.qualityLabel.text = "\(self.video.cachedQuality ?? "0")p"
+        self.qualityLabel.text = "\(video.cachedQuality ?? "0")p"
     }
 }
--- Stepic/DownloadsViewController.swift
+++ Stepic/DownloadsViewController.swift
@@ -6,32 +6,38 @@
 //  Copyright © 2015 Alex Karpov. All rights reserved.
 //
 
-import UIKit
 import DownloadButton
 import SVProgressHUD
+import UIKit
 
-class DownloadsViewController: UIViewController {
-
+final class DownloadsViewController: UIViewController {
     @IBOutlet weak var tableView: StepikTableView!
 
-    var stored: [Video] = []
+    var cachedVideos: [Video] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        edgesForExtendedLayout = []
+        self.edgesForExtendedLayout = []
 
-        tableView.register(UINib(nibName: "DownloadTableViewCell", bundle: nil), forCellReuseIdentifier: "DownloadTableViewCell")
+        self.tableView.register(
+            UINib(nibName: "DownloadTableViewCell", bundle: nil),
+            forCellReuseIdentifier: "DownloadTableViewCell"
+        )
 
-        tableView.emptySetPlaceholder = StepikPlaceholder(.emptyDownloads) { [weak self] in
+        self.tableView.emptySetPlaceholder = StepikPlaceholder(.emptyDownloads) { [weak self] in
             self?.tabBarController?.selectedIndex = 1
         }
         self.tableView.tableFooterView = UIView()
 
         if #available(iOS 11.0, *) {
-            tableView.contentInsetAdjustmentBehavior = UIScrollView.ContentInsetAdjustmentBehavior.never
+            self.tableView.contentInsetAdjustmentBehavior = .never
         }
-        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.fetchVideos()
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -39,73 +45,25 @@ class DownloadsViewController: UIViewController {
         AmplitudeAnalyticsEvents.Downloads.downloadsScreenOpened.send()
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        fetchVideos()
-    }
-
-    func fetchVideos() {
-        stored = []
-        let videos = Video.getAllVideos()
-
-        for video in videos {
-            if video.state == VideoState.cached {
-                stored += [video]
-            }
-        }
-
-        tableView.reloadData()
-    }
-
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-        if segue.identifier == "showProfile" {
-            let dvc = segue.destination
-            dvc.hidesBottomBarWhenPushed = true
-        }
-
-        if segue.identifier == "showSteps" {
-            let dvc = segue.destination as! LessonViewController
-            dvc.hidesBottomBarWhenPushed = true
-
-            let step = sender as! Step
-            if let lesson = step.managedLesson {
-                dvc.initObjects = (lesson: lesson, startStepId: lesson.steps.index(of: step) ?? 0, context: .lesson)
-            }
-        }
-    }
-
-    func askForClearCache(remove: @escaping (() -> Void)) {
-        let alert = UIAlertController(title: NSLocalizedString("ClearCacheTitle", comment: ""), message: NSLocalizedString("ClearCacheMessage", comment: ""), preferredStyle: UIAlertController.Style.alert)
-
-        alert.addAction(UIAlertAction(title: NSLocalizedString("Remove", comment: ""), style: UIAlertAction.Style.destructive, handler: {
-            _ in
-            remove()
-        }))
-
-        alert.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: UIAlertAction.Style.cancel, handler: {
-            _ in
-        }))
-
-        self.present(alert, animated: true, completion: nil)
+    private func fetchVideos() {
+        self.cachedVideos = Video.getAllVideos().filter { $0.state == .cached }
+        self.tableView.reloadData()
     }
 
     @IBAction func clearCachePressed(_ sender: UIBarButtonItem) {
         AnalyticsReporter.reportEvent(AnalyticsEvents.Downloads.clear, parameters: nil)
-        askForClearCache(remove: {
+
+        self.askForClearCache(remove: {
             AnalyticsReporter.reportEvent(AnalyticsEvents.Downloads.acceptedClear, parameters: nil)
             SVProgressHUD.show()
 
             let videos = Video.getAllVideos()
-
             var shouldBeRemovedCount = videos.count
+
             for video in videos {
                 do {
-                    try VideoStoredFileManager(fileManager: FileManager.default).removeVideoStoredFile(videoID: video.id)
+                    let videoStoredFileManager = VideoStoredFileManager(fileManager: .default)
+                    try videoStoredFileManager.removeVideoStoredFile(videoID: video.id)
                     video.cachedQuality = nil
                     CoreDataHelper.instance.save()
 
@@ -114,103 +72,152 @@ class DownloadsViewController: UIViewController {
             }
 
             let completed = videos.count - shouldBeRemovedCount
+
             if shouldBeRemovedCount == 0 {
-                UIThread.performUI({SVProgressHUD.showError(withStatus: "\(NSLocalizedString("FailedToRemoveMessage", comment: "")) \(shouldBeRemovedCount)/\(videos.count) \(NSLocalizedString((completed % 10 == 1 && completed != 11) ? "Video" : "Videos", comment: ""))")})
+                DispatchQueue.main.async {
+                    SVProgressHUD.showError(
+                        withStatus: "\(NSLocalizedString("FailedToRemoveMessage", comment: "")) \(shouldBeRemovedCount)/\(videos.count) \(NSLocalizedString((completed % 10 == 1 && completed != 11) ? "Video" : "Videos", comment: ""))"
+                    )
+                }
             } else {
-                UIThread.performUI({SVProgressHUD.showSuccess(withStatus: "\(NSLocalizedString("RemovedAllMessage", comment: "")) \(completed) \(NSLocalizedString((completed % 10 == 1 && completed != 11) ? "Video" : "Videos", comment: ""))")})
+                DispatchQueue.main.async {
+                    SVProgressHUD.showSuccess(
+                        withStatus: "\(NSLocalizedString("RemovedAllMessage", comment: "")) \(completed) \(NSLocalizedString((completed % 10 == 1 && completed != 11) ? "Video" : "Videos", comment: ""))"
+                    )
+                }
             }
 
-            UIThread.performUI({self.fetchVideos()})
+            DispatchQueue.main.async {
+                self.fetchVideos()
+            }
         })
     }
 
-}
+    private func askForClearCache(remove: @escaping (() -> Void)) {
+        let alert = UIAlertController(
+            title: NSLocalizedString("ClearCacheTitle", comment: ""),
+            message: NSLocalizedString("ClearCacheMessage", comment: ""),
+            preferredStyle: .alert
+        )
 
-extension DownloadsViewController : UITableViewDelegate {
+        alert.addAction(
+            UIAlertAction(
+                title: NSLocalizedString("Remove", comment: ""),
+                style: .destructive,
+                handler: { _ in
+                    remove()
+                }
+            )
+        )
+        alert.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .cancel))
 
-    func showLessonControllerWith(step: Step) {
-        self.performSegue(withIdentifier: "showSteps", sender: step)
-    }
-
-    func showNotAbleToOpenLessonAlert(lesson: Lesson, enroll: @escaping (() -> Void)) {
-        let alert = UIAlertController(title: NSLocalizedString("NoAccess", comment: ""), message: "\(NSLocalizedString("NotEnrolledToCourseMessage", comment: "")) \"\(lesson.managedUnit!.managedSection!.managedCourse!.title)\". \(NSLocalizedString("JoinCourse", comment: ""))?", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: NSLocalizedString("JoinCourse", comment: ""), style: .default, handler: {
-            _ in
-            enroll()
-        }))
-
-        alert.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .cancel, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
+}
 
+// MARK: - DownloadsViewController: UITableViewDataSource -
+
+extension DownloadsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.cachedVideos.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: "DownloadTableViewCell",
+            for: indexPath
+        ) as? DownloadTableViewCell else {
+            return UITableViewCell()
+        }
+
+        if let video = self.cachedVideos[safe: indexPath.row] {
+            cell.configure(video: video)
+        }
+
+        return cell
+    }
+}
+
+// MARK: - DownloadsViewController: UITableViewDelegate -
+
+extension DownloadsViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedVideo: Video! = stored[(indexPath as NSIndexPath).row]
+        tableView.deselectRow(at: indexPath, animated: true)
 
-        if let course = selectedVideo.managedBlock?.managedStep?.managedLesson?.managedUnit?.managedSection?.managedCourse {
-            let enterDownloadBlock = {
-                [weak self] in
-                if course.enrolled {
-                    self?.showLessonControllerWith(step: selectedVideo.managedBlock!.managedStep!)
-                } else {
-                    if selectedVideo.managedBlock!.managedStep!.managedLesson!.isPublic {
-                        self?.showLessonControllerWith(step: selectedVideo.managedBlock!.managedStep!)
-                    } else {
-                        self?.showNotAbleToOpenLessonAlert(lesson: selectedVideo.managedBlock!.managedStep!.managedLesson!, enroll: {
-                            let joinBlock : (() -> Void) = {
-                                [weak self] in
-                                UIThread.performUI({
-                                    SVProgressHUD.show()
-                                })
-                                ApiDataDownloader.enrollments.joinCourse(course, delete: false, success: {
-                                    UIThread.performUI({SVProgressHUD.showSuccess(withStatus: "")})
-                                    self?.showLessonControllerWith(step: selectedVideo.managedBlock!.managedStep!)
-                                    }, error: {
-                                        status in
-                                        UIThread.performUI({SVProgressHUD.showError(withStatus: status)})
-                                        UIThread.performUI({
-                                            if let navigation = self?.navigationController {
-                                                Messages.sharedManager.showConnectionErrorMessage(inController: navigation)
-                                            }
-                                        })
-                                })
+        guard let selectedVideo = self.cachedVideos[safe: indexPath.row],
+              let course = selectedVideo.managedBlock?.managedStep?.managedLesson?.managedUnit?.managedSection?.managedCourse else {
+            return
+        }
+
+        if course.enrolled {
+            self.showLessonControllerWith(step: selectedVideo.managedBlock!.managedStep!)
+        } else {
+            if selectedVideo.managedBlock!.managedStep!.managedLesson!.isPublic {
+                self.showLessonControllerWith(step: selectedVideo.managedBlock!.managedStep!)
+            } else {
+                self.showNotAbleToOpenLessonAlert(lesson: selectedVideo.managedBlock!.managedStep!.managedLesson!, enroll: {
+                    let joinBlock : (() -> Void) = { [weak self] in
+                        DispatchQueue.main.async {
+                            SVProgressHUD.show()
+                        }
+
+                        ApiDataDownloader.enrollments.joinCourse(course, delete: false, success: {
+                            DispatchQueue.main.async {
+                                SVProgressHUD.showSuccess(withStatus: "")
                             }
-                            if AuthInfo.shared.isAuthorized {
-                                joinBlock()
-                            } else {
-                                if let s = self {
-                                    RoutingManager.auth.routeFrom(controller: s, success: {
-                                            joinBlock()
-                                    }, cancel: nil)
+                            self?.showLessonControllerWith(step: selectedVideo.managedBlock!.managedStep!)
+                        }, error: { status in
+                            DispatchQueue.main.async {
+                                SVProgressHUD.showError(withStatus: status)
+
+                                if let navigationController = self?.navigationController {
+                                    Messages.sharedManager.showConnectionErrorMessage(
+                                        inController: navigationController
+                                    )
                                 }
                             }
                         })
                     }
-                }
 
+                    if AuthInfo.shared.isAuthorized {
+                        joinBlock()
+                    } else {
+                        RoutingManager.auth.routeFrom(controller: self, success: {
+                            joinBlock()
+                        }, cancel: nil)
+                    }
+                })
             }
-            enterDownloadBlock()
-        } else {
-            print("Something bad happened")
         }
-
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-}
-
-extension DownloadsViewController : UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return stored.count
     }
 
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+    private func showLessonControllerWith(step: Step) {
+        let assembly = NewLessonAssembly(
+            initialContext: .lesson(id: step.lessonId),
+            startStep: .id(step.id)
+        )
+
+        self.push(module: assembly.makeModule())
     }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "DownloadTableViewCell", for: indexPath) as! DownloadTableViewCell
+    private func showNotAbleToOpenLessonAlert(lesson: Lesson, enroll: @escaping (() -> Void)) {
+        let alert = UIAlertController(
+            title: NSLocalizedString("NoAccess", comment: ""),
+            message: "\(NSLocalizedString("NotEnrolledToCourseMessage", comment: "")) \"\(lesson.managedUnit!.managedSection!.managedCourse!.title)\". \(NSLocalizedString("JoinCourse", comment: ""))?",
+            preferredStyle: .alert
+        )
 
-        cell.initWith(stored[(indexPath as NSIndexPath).row])
+        alert.addAction(
+            UIAlertAction(
+                title: NSLocalizedString("JoinCourse", comment: ""),
+                style: .default,
+                handler: { _ in
+                    enroll()
+                }
+            )
+        )
+        alert.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .cancel))
 
-        return cell
+        self.present(alert, animated: true, completion: nil)
     }
 }
