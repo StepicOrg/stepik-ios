9405dd8a77780d5ebf27cc8b2edbdfb4c3887c73
Ivan Magda <ivan.magda@stepik.org>
2019-12-24 02:56:34 +0300

    Use Timer instead of CATransaction completion block

--- Stepic/Sources/Views/PlayNextCircleControlView.swift
+++ Stepic/Sources/Views/PlayNextCircleControlView.swift
@@ -16,6 +16,12 @@ extension PlayNextCircleControlView {
 final class PlayNextCircleControlView: UIControl {
     let appearance: Appearance
 
+    override var isHighlighted: Bool {
+        didSet {
+            self.alpha = self.isHighlighted ? 0.5 : 1.0
+        }
+    }
+
     private lazy var iconImageView: UIImageView = {
         let image = UIImage(named: "play-next")?.withRenderingMode(.alwaysTemplate)
         let imageView = UIImageView(image: image)
@@ -59,6 +65,11 @@ final class PlayNextCircleControlView: UIControl {
     private var iconImageViewWidthConstraint: Constraint?
     private var iconImageViewHeightConstraint: Constraint?
 
+    // Use `Timer` instead of `CATransaction.setCompletionBlock()` because the completion block object called when
+    // animation has been removed, with this video player logic makes complicated.
+    private var countdownTimer: Timer?
+    private var countdownCompletionBlock: (() -> Void)?
+
     private var circleRadius: CGFloat {
         self.bounds.width / 2 - self.appearance.circleWidth
     }
@@ -75,6 +86,10 @@ final class PlayNextCircleControlView: UIControl {
     @available(*, unavailable)
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    deinit {
+        self.countdownTimer?.invalidate()
     }
 
     override func layoutSubviews() {
@@ -95,9 +110,10 @@ final class PlayNextCircleControlView: UIControl {
         self.iconImageViewHeightConstraint?.update(offset: self.circleRadius)
     }
 
+    // MARK: Public API
+
     func startCountdown(duration: TimeInterval, completion completionBlock: (() -> Void)? = nil) {
         CATransaction.begin()
-        CATransaction.setCompletionBlock(completionBlock)
 
         let animation = CABasicAnimation(keyPath: AnimationKeyPath.strokeEnd.rawValue)
         animation.toValue = 1
@@ -107,17 +123,17 @@ final class PlayNextCircleControlView: UIControl {
 
         self.stopCountdown()
         self.progressLayer.add(animation, forKey: AnimationKeyPath.progressAnimation.rawValue)
+        self.scheduleTimer(duration: duration, completion: completionBlock)
 
         CATransaction.commit()
     }
 
     func stopCountdown() {
+        self.stopTimer()
         self.setProgress(to: 0)
     }
 
-    func completeCountdown() {
-        self.setProgress(to: 1)
-    }
+    // MARK: Private API
 
     private func makeCircularPath() -> UIBezierPath {
         UIBezierPath(
@@ -133,6 +149,31 @@ final class PlayNextCircleControlView: UIControl {
         self.progressLayer.removeAnimation(forKey: AnimationKeyPath.progressAnimation.rawValue)
         self.progressLayer.strokeEnd = value
     }
+
+    private func stopTimer() {
+        self.countdownTimer?.invalidate()
+        self.countdownTimer = nil
+        self.countdownCompletionBlock = nil
+    }
+
+    private func scheduleTimer(duration: TimeInterval, completion completionBlock: (() -> Void)?) {
+        self.countdownTimer?.invalidate()
+        self.countdownTimer = Timer.scheduledTimer(
+            timeInterval: duration,
+            target: self,
+            selector: #selector(self.onTimerElapsed),
+            userInfo: nil,
+            repeats: false
+        )
+        self.countdownCompletionBlock = completionBlock
+    }
+
+    @objc
+    private func onTimerElapsed() {
+        self.countdownCompletionBlock?()
+    }
+
+    // MARK: Inner types
 
     private enum AnimationKeyPath: String {
         case strokeEnd
