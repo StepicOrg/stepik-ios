d24e6e8816ab3b980b420d29e75ee3dec8b95a4d
Ivan Magda <ivan.magda@stepik.org>
2020-02-04 17:50:28 +0300

    Remove IT.Mail.Ru auth provider (#645)
    * Remove itMailRu provider

--- Stepic/SocialAuthViewController.swift
+++ Stepic/SocialAuthViewController.swift
@@ -41,11 +41,23 @@ extension SocialAuthViewController: SocialAuthView {
     }
 
     func presentWebController(with url: URL) {
-        WebControllerManager.sharedManager.presentWebControllerWithURL(url, inController: self, withKey: webControllerKey, allowsSafari: false, backButtonStyle: BackButtonStyle.close, forceCustom: true)
+        WebControllerManager.sharedManager.presentWebControllerWithURL(
+            url,
+            inController: self,
+            withKey: webControllerKey,
+            allowsSafari: false,
+            backButtonStyle: .close,
+            forceCustom: true
+        )
     }
 
     func dismissWebController() {
-        WebControllerManager.sharedManager.dismissWebControllerWithKey(webControllerKey, animated: true, completion: nil, error: nil)
+        WebControllerManager.sharedManager.dismissWebControllerWithKey(
+            webControllerKey,
+            animated: true,
+            completion: nil,
+            error: nil
+        )
     }
 }
 
@@ -103,7 +115,12 @@ final class SocialAuthViewController: UIViewController {
     @IBAction func moreButtonClick(_ sender: Any) {
         isExpanded.toggle()
 
-        moreButton.setTitle(isExpanded ? NSLocalizedString("SignInLessButton", comment: "") : NSLocalizedString("SignInMoreButton", comment: ""), for: .normal)
+        moreButton.setTitle(
+            isExpanded
+                ? NSLocalizedString("SignInLessButton", comment: "")
+                : NSLocalizedString("SignInMoreButton", comment: ""),
+            for: .normal
+        )
 
         collectionView.collectionViewLayout.invalidateLayout()
         self.updateCollectionViewHeight()
@@ -119,12 +136,20 @@ final class SocialAuthViewController: UIViewController {
 
         localize()
 
-        presenter = SocialAuthPresenter(authAPI: ApiDataDownloader.auth, stepicsAPI: ApiDataDownloader.stepics, notificationStatusesAPI: NotificationStatusesAPI(), splitTestingService: SplitTestingService(analyticsService: AnalyticsUserProperties(), storage: UserDefaults.standard), view: self)
+        presenter = SocialAuthPresenter(
+            authAPI: ApiDataDownloader.auth,
+            stepicsAPI: ApiDataDownloader.stepics,
+            notificationStatusesAPI: NotificationStatusesAPI(),
+            view: self
+        )
         presenter?.update()
 
         collectionView.delegate = self
         collectionView.dataSource = self
-        collectionView.register(UINib(nibName: "SocialAuthCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: SocialAuthCollectionViewCell.reuseId)
+        collectionView.register(
+            UINib(nibName: "SocialAuthCollectionViewCell", bundle: nil),
+            forCellWithReuseIdentifier: SocialAuthCollectionViewCell.reuseId
+        )
 
         let collectionViewLayout = SocialCollectionViewFlowLayout()
         collectionViewLayout.numberOfColumns = numberOfColumns
@@ -134,10 +159,6 @@ final class SocialAuthViewController: UIViewController {
         if DeviceInfo.current.diagonal <= 4 {
             stepikLogoHeightConstraint.constant = 38
         }
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
     }
 
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
@@ -151,18 +172,22 @@ final class SocialAuthViewController: UIViewController {
         }
 
         // Add additional offset for shadows
-        collectionViewHeight.constant = isExpanded ? (2 * layout.itemSize.height + layout.minimumInteritemSpacing + headerHeight) + 10 : (layout.itemSize.height + headerHeight) + 5
+        collectionViewHeight.constant = isExpanded
+            ? (2 * layout.itemSize.height + layout.minimumInteritemSpacing + headerHeight) + 10
+            : (layout.itemSize.height + headerHeight) + 5
     }
 
     private func localize() {
-        signInButton.setTitle(NSLocalizedString("SignInEmailButton", comment: ""), for: .normal)
-        signUpButton.setTitle(NSLocalizedString("SignUpButton", comment: ""), for: .normal)
-        moreButton.setTitle(NSLocalizedString("SignInMoreButton", comment: ""), for: .normal)
+        self.signInButton.setTitle(NSLocalizedString("SignInEmailButton", comment: ""), for: .normal)
+        self.signUpButton.setTitle(NSLocalizedString("SignUpButton", comment: ""), for: .normal)
+        self.moreButton.setTitle(NSLocalizedString("SignInMoreButton", comment: ""), for: .normal)
     }
 }
 
 extension SocialAuthViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    func numberOfSections(in collectionView: UICollectionView) -> Int { self.numberOfRows }
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        self.numberOfRows
+    }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         section == 0 ? self.numberOfColumns : self.numberOfColumns - 1
@@ -205,26 +230,41 @@ extension SocialAuthViewController: UICollectionViewDelegate, UICollectionViewDa
         collectionView.cellForItem(at: indexPath)?.isHighlighted = false
     }
 
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        insetForSectionAt section: Int
+    ) -> UIEdgeInsets {
         guard let layout = collectionViewLayout as? SocialCollectionViewFlowLayout else {
             return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         }
 
         // Center grid
         let width = collectionView.bounds.width
-        let contentWidth = CGFloat(CGFloat(layout.numberOfColumns - 1) * layout.minimumInteritemSpacing + CGFloat(layout.numberOfColumns) * layout.itemSize.width)
+        let contentWidth = CGFloat(
+            CGFloat(layout.numberOfColumns - 1) * layout.minimumInteritemSpacing + CGFloat(layout.numberOfColumns) * layout.itemSize.width
+        )
         let leftOffset = (width - contentWidth) / 2
 
         // Add bottom offset for first section
         if section == 0 {
             return UIEdgeInsets(top: 0, left: leftOffset, bottom: layout.minimumLineSpacing, right: leftOffset)
         }
+
         return UIEdgeInsets(top: 0, left: leftOffset, bottom: 0, right: leftOffset)
     }
 
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        viewForSupplementaryElementOfKind kind: String,
+        at indexPath: IndexPath
+    ) -> UICollectionReusableView {
         if kind == UICollectionView.elementKindSectionHeader {
-            if let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: SocialAuthHeaderView.reuseId, for: indexPath) as? SocialAuthHeaderView {
+            if let header = collectionView.dequeueReusableSupplementaryView(
+                ofKind: kind,
+                withReuseIdentifier: SocialAuthHeaderView.reuseId,
+                for: indexPath
+            ) as? SocialAuthHeaderView {
                 header.setup(title: presenter?.socialAuthHeaderString ?? "")
                 return header
             }
@@ -232,7 +272,11 @@ extension SocialAuthViewController: UICollectionViewDelegate, UICollectionViewDa
         return UICollectionReusableView()
     }
 
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        referenceSizeForHeaderInSection section: Int
+    ) -> CGSize {
         // Add caption only for first section
         return section == 0 ? CGSize(width: collectionView.bounds.size.width, height: headerHeight) : CGSize.zero
     }
@@ -242,7 +286,14 @@ extension SocialAuthViewController: VKSocialSDKProviderDelegate {
     func presentAuthController(_ controller: UIViewController) {
         // FIXME: register URL
         let registerURL = SocialProvider.vk.info.registerURL
-        WebControllerManager.sharedManager.presentWebControllerWithURL(registerURL, inController: self, withKey: "social auth", allowsSafari: false, backButtonStyle: BackButtonStyle.close, forceCustom: true)
+        WebControllerManager.sharedManager.presentWebControllerWithURL(
+            registerURL,
+            inController: self,
+            withKey: "social auth",
+            allowsSafari: false,
+            backButtonStyle: .close,
+            forceCustom: true
+        )
     }
 }
 
