03788022ecb46c3c8856f6e33ce0f181c2115bbc
Ivan Magda <ivan.magda@stepik.org>
2020-02-06 19:42:06 +0300

    Present exam alert

--- Stepic/LastStepRouter.swift
+++ Stepic/LastStepRouter.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2017 Alex Karpov. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import PromiseKit
 import SVProgressHUD
 
@@ -23,13 +23,17 @@ final class LastStepRouter {
         using navigationController: UINavigationController,
         skipSyllabus: Bool = false
     ) {
-        guard let lastStepId = course.lastStepId,
-              course.canContinue else {
+        guard course.canContinue,
+              let lastStepID = course.lastStepId else {
             return
         }
+
         SVProgressHUD.show()
-        ApiDataDownloader.lastSteps.getObjectsByIds(ids: [lastStepId], updating: course.lastStep != nil ? [course.lastStep!] : []).done {
-            newLastSteps in
+
+        ApiDataDownloader.lastSteps.getObjectsByIds(
+            ids: [lastStepID],
+            updating: course.lastStep != nil ? [course.lastStep!] : []
+        ).done { newLastSteps in
             guard let newLastStep = newLastSteps.first else {
                 throw LastStepError.multipleLastSteps
             }
@@ -37,10 +41,15 @@ final class LastStepRouter {
             course.lastStep = newLastStep
             CoreDataHelper.shared.save()
         }.ensure {
-            self.navigate(for: course, isAdaptive: isAdaptive, didJustSubscribe: didJustSubscribe, using: navigationController, skipSyllabus: skipSyllabus)
-        }.catch {
-            _ in
-            print("error while updating lastStep")
+            self.navigate(
+                for: course,
+                isAdaptive: isAdaptive,
+                didJustSubscribe: didJustSubscribe,
+                using: navigationController,
+                skipSyllabus: skipSyllabus
+            )
+        }.catch { error in
+            print("error while updating lastStep: \(error)")
         }
     }
 
@@ -51,37 +60,44 @@ final class LastStepRouter {
         using navigationController: UINavigationController,
         skipSyllabus: Bool = false
     ) {
-        let shouldOpenInAdaptiveMode = isAdaptive ?? AdaptiveStorageManager.shared.canOpenInAdaptiveMode(courseId: course.id)
+        let shouldOpenInAdaptiveMode = isAdaptive
+            ?? AdaptiveStorageManager.shared.canOpenInAdaptiveMode(courseId: course.id)
         if shouldOpenInAdaptiveMode {
-            guard let cardsViewController = ControllerHelper.instantiateViewController(identifier: "CardsSteps", storyboardName: "Adaptive") as? BaseCardsStepsViewController else {
+            guard let cardsViewController = ControllerHelper.instantiateViewController(
+                identifier: "CardsSteps",
+                storyboardName: "Adaptive"
+            ) as? BaseCardsStepsViewController else {
                 return
             }
+
             cardsViewController.hidesBottomBarWhenPushed = true
             cardsViewController.course = course
             cardsViewController.didJustSubscribe = didJustSubscribe
             navigationController.pushViewController(cardsViewController, animated: true)
+
             SVProgressHUD.showSuccess(withStatus: "")
+
             return
         }
 
-        let sectionsVC = CourseInfoAssembly(courseID: course.id, initialTab: .syllabus).makeModule()
+        let courseInfoController = CourseInfoAssembly(courseID: course.id, initialTab: .syllabus).makeModule()
 
         func openSyllabus() {
             SVProgressHUD.showSuccess(withStatus: "")
-            navigationController.pushViewController(sectionsVC, animated: true)
+            navigationController.pushViewController(courseInfoController, animated: true)
             AnalyticsReporter.reportEvent(AnalyticsEvents.Continue.sectionsOpened, parameters: nil)
             return
         }
 
-        func checkUnitAndNavigate(for unitId: Int) {
-            if let unit = Unit.getUnit(id: unitId) {
+        func checkUnitAndNavigate(for unitID: Int) {
+            if let unit = Unit.getUnit(id: unitID) {
                 checkSectionAndNavigate(in: unit)
             } else {
-                ApiDataDownloader.units.retrieve(ids: [unitId], existing: [], refreshMode: .update, success: { units in
+                ApiDataDownloader.units.retrieve(ids: [unitID], existing: [], refreshMode: .update, success: { units in
                     if let unit = units.first {
                         checkSectionAndNavigate(in: unit)
                     } else {
-                        print("last step router: unit not found, id = \(unitId)")
+                        print("last step router: unit not found, id = \(unitID)")
                         openSyllabus()
                     }
                 }, error: { err in
@@ -93,18 +109,21 @@ final class LastStepRouter {
 
         func navigateToStep(in unit: Unit) {
             // If last step does not exist then take first step in unit
-            let stepIdPromise = Promise<Int> { seal in
-                if let stepId = course.lastStep?.stepId {
-                    seal.fulfill(stepId)
+            let stepIDPromise = Promise<Int> { seal in
+                if let stepID = course.lastStep?.stepId {
+                    seal.fulfill(stepID)
                 } else {
                     let cachedLesson = unit.lesson ?? Lesson.getLesson(unit.lessonId)
-                    ApiDataDownloader.lessons.retrieve(ids: [unit.lessonId], existing: cachedLesson == nil ? [] : [cachedLesson!]).done { lessons in
+                    ApiDataDownloader.lessons.retrieve(
+                        ids: [unit.lessonId],
+                        existing: cachedLesson == nil ? [] : [cachedLesson!]
+                    ).done { lessons in
                         if let lesson = lessons.first {
                             unit.lesson = lesson
                         }
 
-                        if let firstStepId = lessons.first?.stepsArray.first {
-                            seal.fulfill(firstStepId)
+                        if let firstStepID = lessons.first?.stepsArray.first {
+                            seal.fulfill(firstStepID)
                         } else {
                             seal.reject(NSError(domain: "error", code: 100, userInfo: nil)) // meh.
                         }
@@ -114,16 +133,16 @@ final class LastStepRouter {
                 }
             }
 
-            stepIdPromise.done { targetStepId in
+            stepIDPromise.done { targetStepID in
                 let lessonAssembly = LessonAssembly(
                     initialContext: .unit(id: unit.id),
-                    startStep: .id(targetStepId)
+                    startStep: .id(targetStepID)
                 )
 
                 SVProgressHUD.showSuccess(withStatus: "")
 
                 if !skipSyllabus {
-                    navigationController.pushViewController(sectionsVC, animated: false)
+                    navigationController.pushViewController(courseInfoController, animated: false)
                 }
                 navigationController.pushViewController(lessonAssembly.makeModule(), animated: true)
 
@@ -142,60 +161,104 @@ final class LastStepRouter {
             }
 
             // Always refresh section to prevent obsolete `isReachable` state
-            ApiDataDownloader.sections.retrieve(ids: [unit.sectionId], existing: sectionForUpdate == nil ? [] : [sectionForUpdate!], refreshMode: .update, success: { sections in
-                if let section = sections.first {
-                    unit.section = section
-                    CoreDataHelper.shared.save()
+            ApiDataDownloader.sections.retrieve(
+                ids: [unit.sectionId],
+                existing: sectionForUpdate == nil ? [] : [sectionForUpdate!],
+                refreshMode: .update,
+                success: { sections in
+                    if let section = sections.first {
+                        unit.section = section
+                        CoreDataHelper.shared.save()
 
-                    if section.isReachable {
+                        // Check whether unit is in exam section
+                        if section.isExam && section.isReachable {
+                            self.presentExamAlert(presentationController: navigationController)
+                        } else if section.isReachable {
+                            navigateToStep(in: unit)
+                        } else {
+                            openSyllabus()
+                        }
+                    } else {
+                        print("last step router: section not found, id = \(unit.sectionId)")
+                        openSyllabus()
+                    }
+                },
+                error: { err in
+                    print("last step router: error while loading section, error = \(err)")
+
+                    // Fallback: use cached section
+                    guard let section = sectionForUpdate else {
+                        return openSyllabus()
+                    }
+
+                    print("last step router: using cached section")
+                    // Check whether unit is in exam section
+                    if section.isExam && section.isReachable {
+                        self.presentExamAlert(presentationController: navigationController)
+                    } else if section.isReachable {
                         navigateToStep(in: unit)
                     } else {
                         openSyllabus()
                     }
-                } else {
-                    print("last step router: section not found, id = \(unit.sectionId)")
-                    openSyllabus()
                 }
-            }, error: { err in
-                print("last step router: error while loading section, error = \(err)")
-
-                // Fallback: use cached section
-                guard let section = sectionForUpdate else {
-                    openSyllabus()
-                    return
-                }
-
-                print("last step router: using cached section")
-                if section.isReachable {
-                    navigateToStep(in: unit)
-                } else {
-                    openSyllabus()
-                }
-            })
+            )
         }
 
-        guard let unitId = course.lastStep?.unitId else {
+        guard let unitID = course.lastStep?.unitId else {
             // If last step does not exist then take first section and its first unit
-            guard let sectionId = course.sectionsArray.first,
-                  let sections = try? Section.getSections(sectionId),
+            guard let sectionID = course.sectionsArray.first,
+                  let sections = try? Section.getSections(sectionID),
                   let cachedSection = sections.first else {
-                openSyllabus()
-                return
+                return openSyllabus()
             }
 
-            ApiDataDownloader.sections.retrieve(ids: [sectionId], existing: [cachedSection]).done { section in
-                guard let unitId = section.first?.unitsArray.first else {
-                    print("last step router: section has no units")
-                    return
+            ApiDataDownloader.sections.retrieve(ids: [sectionID], existing: [cachedSection]).done { section in
+                guard let unitID = section.first?.unitsArray.first else {
+                    return print("last step router: section has no units")
                 }
-                checkUnitAndNavigate(for: unitId)
+                checkUnitAndNavigate(for: unitID)
             }.catch { _ in
                 print("last step router: unable to load section when last step does not exists")
                 openSyllabus()
             }
+
             return
         }
 
-        checkUnitAndNavigate(for: unitId)
+        checkUnitAndNavigate(for: unitID)
+    }
+
+    private static func presentExamAlert(presentationController: UIViewController) {
+        SVProgressHUD.dismiss()
+
+        let alert = UIAlertController(
+            title: NSLocalizedString("ExamTitle", comment: ""),
+            message: NSLocalizedString("LastStepRouterShowExamInWeb", comment: ""),
+            preferredStyle: .alert
+        )
+        alert.addAction(
+            UIAlertAction(
+                title: NSLocalizedString("Open", comment: ""),
+                style: .default,
+                handler: { _ in
+                    WebControllerManager.sharedManager.presentWebControllerWithURLString(
+                        "?from_mobile_app=true",
+                        inController: presentationController,
+                        withKey: "exam",
+                        allowsSafari: true,
+                        backButtonStyle: .close
+                    )
+                }
+            )
+        )
+        alert.addAction(
+            UIAlertAction(
+                title: NSLocalizedString("Cancel", comment: ""),
+                style: .cancel,
+                handler: nil
+            )
+        )
+
+        presentationController.present(module: alert)
     }
 }
