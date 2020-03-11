08776b98f9c8f5298fcaa437b66121551a6b32a1
Ivan Magda <ivan.magda@stepik.org>
2020-03-11 17:38:18 +0300

    UIColor+DynamicColor

---
+++ Stepic/Sources/Extensions/UIKit/UIColor+DynamicColor.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+extension UIColor {
+    /// Create a UIColor with different colors for light and dark mode, and with a normal/high contrast level.
+    /// Fallbacks to light color on earlier versions.
+    /// - Parameters:
+    ///   - light: Color to use in light/unspecified mode.
+    ///   - dark: Color to use in dark mode.
+    ///   - lightAccessibility: Color to use in light/unspecified mode and with a high contrast level.
+    ///   - darkAccessibility: Color to use in dark mode and with a high contrast level.
+    static func dynamicColor(
+        light: UIColor,
+        dark: UIColor,
+        lightAccessibility: UIColor? = nil,
+        darkAccessibility: UIColor? = nil
+    ) -> UIColor {
+        if #available(iOS 13.0, *) {
+            return UIColor { (traitCollection: UITraitCollection) -> UIColor in
+                switch(traitCollection.userInterfaceStyle, traitCollection.accessibilityContrast) {
+                case (.dark, .high):
+                    return darkAccessibility ?? dark
+                case (.dark, _):
+                    return dark
+                case (_, .high):
+                    return lightAccessibility ?? light
+                default:
+                    return light
+                }
+            }
+        } else {
+            return light
+        }
+    }
+}
--- Stepic/Theme/ColorPalette.swift
+++ Stepic/Theme/ColorPalette.swift
@@ -1,9 +1,24 @@
-//
-//  ColorPalette.swift
-//  Stepic
-//
-//  Created by Ivan Magda on 3/11/20.
-//  Copyright © 2020 Alex Karpov. All rights reserved.
-//
+import UIKit
 
-import Foundation
+extension UIColor {
+    static let errorRed = UIColor(hex6: 0xff0033)
+
+    static let lightBlue = UIColor(hex6: 0x45B0FF)
+
+    static var stepikGreen: UIColor { StepikApplicationsInfo.Colors.mainGreen }
+
+    static let mainLight = UIColor(hex6: 0xf6f6f6)
+
+    static var mainDark: UIColor { StepikApplicationsInfo.Colors.mainDark }
+
+    static var mainText: UIColor { return StepikApplicationsInfo.Colors.mainText }
+
+    static let thirdColor = UIColor(hex6: 0x54a2ff)
+
+    static let correctQuizBackground = UIColor(hex6: 0xE9F9E9)
+    static let wrongQuizBackground = UIColor(hex6: 0xF5EBF2)
+    static let peerReviewYellow = UIColor(hex6: 0xFFFAE9)
+}
+
+private enum ColorPalette {
+}
