82dd59f8f47bd63f8a81d235acda082f79880a6a
Ivan Magda <ivan.magda@stepik.org>
2020-03-22 14:50:24 +0300

    Rate app

--- Stepic/RateAppViewController.swift
+++ Stepic/RateAppViewController.swift
@@ -13,10 +13,17 @@ import StoreKit
 import UIKit
 
 final class RateAppViewController: UIViewController {
+    enum AfterRateActionType {
+        case appStore
+        case email
+    }
+
     @IBOutlet weak var topLabel: StepikLabel!
     @IBOutlet weak var bottomLabel: StepikLabel!
     @IBOutlet weak var laterButton: UIButton!
     @IBOutlet weak var rightButton: UIButton!
+    @IBOutlet var buttonsContainerView: UIView!
+    @IBOutlet var buttonsSeparator: UIView!
 
     @IBOutlet weak var centerViewWidth: NSLayoutConstraint!
 
@@ -26,154 +33,36 @@ final class RateAppViewController: UIViewController {
 
     var lessonProgress: String?
 
-    var defaultAnalyticsParams: [String: Any] {
-        if let progress = lessonProgress {
+    private var defaultAnalyticsParams: [String: Any] {
+        if let progress = self.lessonProgress {
             return ["lesson_progress": progress]
         } else {
             return [:]
         }
     }
 
-    enum AfterRateActionType {
-        case appStore, email
-    }
-
-    var buttonState: AfterRateActionType? = nil {
+    private var buttonState: AfterRateActionType? = nil {
         didSet {
-            guard buttonState != nil else {
+            guard self.buttonState != nil else {
                 return
             }
 
-            switch buttonState! {
+            switch self.buttonState! {
             case .appStore:
-                rightButton.titleLabel?.text = NSLocalizedString("AppStore", comment: "")
-                rightButton.setTitle(NSLocalizedString("AppStore", comment: ""), for: .normal)
-                rightButton.setTitleColor(UIColor.stepikGreen, for: .normal)
+                self.rightButton.titleLabel?.text = NSLocalizedString("AppStore", comment: "")
+                self.rightButton.setTitle(NSLocalizedString("AppStore", comment: ""), for: .normal)
+                self.rightButton.setTitleColor(.stepikGreen, for: .normal)
             case .email:
-                rightButton.titleLabel?.text = NSLocalizedString("Email", comment: "")
-                rightButton.setTitle(NSLocalizedString("Email", comment: ""), for: .normal)
-                rightButton.setTitleColor(.stepikRed, for: .normal)
+                self.rightButton.titleLabel?.text = NSLocalizedString("Email", comment: "")
+                self.rightButton.setTitle(NSLocalizedString("Email", comment: ""), for: .normal)
+                self.rightButton.setTitleColor(.stepikRed, for: .normal)
             }
         }
     }
 
-    var bottomLabelWidth: Constraint?
+    private var bottomLabelWidth: Constraint?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        centerViewWidth.constant = 0.5
-        buttonsContainerHeight.constant = 0
-
-        laterButton.setTitle(NSLocalizedString("Later", comment: ""), for: .normal)
-        topLabel.text = String(format: NSLocalizedString("HowWouldYouRate", comment: ""), Bundle.main.infoDictionary?["CFBundleDisplayName"] as? String ?? "Stepik")
-        bottomLabel.text = ""
-
-        bottomLabel.snp.makeConstraints { make -> Void in
-            bottomLabelWidth = make.width.lessThanOrEqualTo(UIScreen.main.bounds.width - 48).constraint
-        }
-
-        for star in starImageViews {
-            print(star.tag)
-            let tapG = UITapGestureRecognizer(target: self, action: #selector(RateAppViewController.didTap(_:)))
-            star.isUserInteractionEnabled = true
-            star.image = Images.star.empty
-            star.highlightedImage = Images.star.filled
-            star.addGestureRecognizer(tapG)
-        }
-    }
-
-    @objc
-    func didTap(_ recognizer: UITapGestureRecognizer) {
-        guard let tappedIndex = recognizer.view?.tag else {
-            return
-        }
-
-        for star in starImageViews {
-            if star.tag <= tappedIndex {
-                star.isHighlighted = true
-            }
-            star.isUserInteractionEnabled = false
-        }
-
-        topLabel.text = NSLocalizedString("ThankYou", comment: "")
-        let rating = tappedIndex + 1
-
-        var params = defaultAnalyticsParams
-        params["rating"] = rating
-        AnalyticsReporter.reportEvent(AnalyticsEvents.Rate.rated, parameters: params)
-
-        if rating < 4 {
-            buttonState = .email
-            bottomLabel.text = NSLocalizedString("PleaseLeaveFeedbackEmail", comment: "")
-        } else {
-            buttonState = .appStore
-            bottomLabel.text = NSLocalizedString("PleaseLeaveFeedbackAppstore", comment: "")
-        }
-
-        buttonsContainerHeight.constant = 48
-
-        self.view.frame = CGRect(origin: self.view.frame.origin, size: CGSize(width: self.view.frame.width, height: self.view.frame.height + 48.0))
-        UIView.animate(withDuration: 0.2, animations: {
-            [weak self] in
-            self?.view.layoutIfNeeded()
-        })
-    }
-
-    func showEmail() {
-        AnalyticsReporter.reportEvent(AnalyticsEvents.Rate.Negative.email, parameters: defaultAnalyticsParams)
-
-        if !MFMailComposeViewController.canSendMail() {
-            //TODO: Present alert that mail is not supported on this device
-            self.dismiss(animated: true, completion: nil)
-            return
-        }
-
-        let composeVC = MFMailComposeViewController()
-        composeVC.mailComposeDelegate = self
-
-        composeVC.setToRecipients(["support@stepik.org"])
-        composeVC.setSubject(String(format: NSLocalizedString("FeedbackAbout", comment: ""), Bundle.main.infoDictionary?[kCFBundleNameKey as String] as? String ?? "Stepik"))
-        composeVC.setMessageBody("", isHTML: false)
-        self.customPresentViewController(mailPresenter, viewController: composeVC, animated: true, completion: nil)
-    }
-
-    func showAppStore() {
-        AnalyticsReporter.reportEvent(AnalyticsEvents.Rate.Positive.appstore, parameters: defaultAnalyticsParams)
-        self.dismiss(animated: true, completion: {
-            SKStoreReviewController.requestReview()
-        })
-    }
-
-    @IBAction func laterButtonPressed(_ sender: UIButton) {
-        RoutingManager.rate.pressedShowLater()
-        self.dismiss(animated: true, completion: nil)
-
-        guard buttonState != nil else {
-            return
-        }
-        switch buttonState! {
-        case .appStore:
-            AnalyticsReporter.reportEvent(AnalyticsEvents.Rate.Positive.later, parameters: defaultAnalyticsParams)
-        case .email:
-            AnalyticsReporter.reportEvent(AnalyticsEvents.Rate.Negative.later, parameters: defaultAnalyticsParams)
-        }
-    }
-
-    @IBAction func rightButtonPressed(_ sender: UIButton) {
-        guard buttonState != nil else {
-            return
-        }
-        RoutingManager.rate.neverShow()
-        switch buttonState! {
-        case .appStore:
-            showAppStore()
-        case .email:
-            showEmail()
-        }
-    }
-
-    let mailPresenter: Presentr = {
+    private let mailPresenter: Presentr = {
         let width = ModalSize.sideMargin(value: 0)
         let height = ModalSize.sideMargin(value: 0)
         let center = ModalCenterPosition.center
@@ -181,12 +70,163 @@ final class RateAppViewController: UIViewController {
 
         let presenter = Presentr(presentationType: customType)
         presenter.roundCorners = false
+
         return presenter
     }()
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.centerViewWidth.constant = 0.5
+        self.buttonsContainerHeight.constant = 0
+        self.buttonsContainerView.alpha = 0
+
+        self.laterButton.setTitle(NSLocalizedString("Later", comment: ""), for: .normal)
+        self.topLabel.text = String(
+            format: NSLocalizedString("HowWouldYouRate", comment: ""),
+            Bundle.main.infoDictionary?["CFBundleDisplayName"] as? String ?? "Stepik"
+        )
+        self.bottomLabel.text = ""
+
+        self.bottomLabel.snp.makeConstraints { make in
+            self.bottomLabelWidth = make.width.lessThanOrEqualTo(UIScreen.main.bounds.width - 48).constraint
+        }
+
+        for star in self.starImageViews {
+            print(star.tag)
+            let tapG = UITapGestureRecognizer(target: self, action: #selector(RateAppViewController.didTap(_:)))
+            star.isUserInteractionEnabled = true
+            star.image = Images.star.empty
+            star.highlightedImage = Images.star.filled
+            star.addGestureRecognizer(tapG)
+        }
+
+        self.colorize()
+    }
+
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
-        bottomLabelWidth?.update(offset: UIScreen.main.bounds.height - 48)
+        self.bottomLabelWidth?.update(offset: UIScreen.main.bounds.height - 48)
+    }
+
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+
+        self.view.performBlockIfAppearanceChanged(from: previousTraitCollection) {
+            self.colorize()
+        }
+    }
+
+    private func colorize() {
+        self.view.backgroundColor = .stepikTertiaryBackground
+        self.buttonsContainerView.backgroundColor = .stepikTertiaryBackground
+        self.buttonsSeparator.backgroundColor = .stepikSeparator
+        self.laterButton.setTitleColor(.stepikAccent, for: .normal)
+    }
+
+    @objc
+    private func didTap(_ recognizer: UITapGestureRecognizer) {
+        guard let tappedIndex = recognizer.view?.tag else {
+            return
+        }
+
+        for star in self.starImageViews {
+            if star.tag <= tappedIndex {
+                star.isHighlighted = true
+            }
+            star.isUserInteractionEnabled = false
+        }
+
+        self.topLabel.text = NSLocalizedString("ThankYou", comment: "")
+        let rating = tappedIndex + 1
+
+        var params = defaultAnalyticsParams
+        params["rating"] = rating
+        AnalyticsReporter.reportEvent(AnalyticsEvents.Rate.rated, parameters: params)
+
+        if rating < 4 {
+            self.buttonState = .email
+            self.bottomLabel.text = NSLocalizedString("PleaseLeaveFeedbackEmail", comment: "")
+        } else {
+            self.buttonState = .appStore
+            self.bottomLabel.text = NSLocalizedString("PleaseLeaveFeedbackAppstore", comment: "")
+        }
+
+        self.buttonsContainerHeight.constant = 48
+        self.buttonsContainerView.alpha = 1
+
+        self.view.frame = CGRect(
+            origin: self.view.frame.origin,
+            size: CGSize(width: self.view.frame.width, height: self.view.frame.height + 48.0)
+        )
+
+        UIView.animate(
+            withDuration: 0.2,
+            animations: { [weak self] in
+                self?.view.layoutIfNeeded()
+            }
+        )
+    }
+
+    private func showEmail() {
+        AnalyticsReporter.reportEvent(AnalyticsEvents.Rate.Negative.email, parameters: defaultAnalyticsParams)
+
+        if !MFMailComposeViewController.canSendMail() {
+            return self.dismiss(animated: true, completion: nil)
+        }
+
+        let composeVC = MFMailComposeViewController()
+        composeVC.mailComposeDelegate = self
+        composeVC.setToRecipients(["support@stepik.org"])
+        composeVC.setSubject(
+            String(
+                format: NSLocalizedString("FeedbackAbout", comment: ""),
+                Bundle.main.infoDictionary?[kCFBundleNameKey as String] as? String ?? "Stepik"
+            )
+        )
+        composeVC.setMessageBody("", isHTML: false)
+
+        self.customPresentViewController(self.mailPresenter, viewController: composeVC, animated: true, completion: nil)
+    }
+
+    private func showAppStore() {
+        AnalyticsReporter.reportEvent(AnalyticsEvents.Rate.Positive.appstore, parameters: defaultAnalyticsParams)
+        self.dismiss(animated: true, completion: {
+            SKStoreReviewController.requestReview()
+        })
+    }
+
+    @IBAction
+    func laterButtonPressed(_ sender: UIButton) {
+        RoutingManager.rate.pressedShowLater()
+        self.dismiss(animated: true, completion: nil)
+
+        guard self.buttonState != nil else {
+            return
+        }
+
+        switch self.buttonState! {
+        case .appStore:
+            AnalyticsReporter.reportEvent(AnalyticsEvents.Rate.Positive.later, parameters: self.defaultAnalyticsParams)
+        case .email:
+            AnalyticsReporter.reportEvent(AnalyticsEvents.Rate.Negative.later, parameters: self.defaultAnalyticsParams)
+        }
+    }
+
+    @IBAction
+    func rightButtonPressed(_ sender: UIButton) {
+        guard self.buttonState != nil else {
+            return
+        }
+
+        RoutingManager.rate.neverShow()
+
+        switch self.buttonState! {
+        case .appStore:
+            self.showAppStore()
+        case .email:
+            self.showEmail()
+        }
     }
 }
 
