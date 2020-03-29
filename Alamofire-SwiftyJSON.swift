ce5f9739810249969e942eb07b5a83f7f478ad6f
Ivan Magda <ivan.magda@stepik.org>
2020-03-29 14:12:43 +0300

    Update SwiftyJSON response serializer

--- Alamofire-SwiftyJSON.swift
+++ Alamofire-SwiftyJSON.swift
@@ -2,53 +2,39 @@ import Alamofire
 import Foundation
 import SwiftyJSON
 
-// MARK: JSON Response Serializer -
+// MARK: SwiftyJSON Response Serializer -
 
 public final class SwiftyJSONResponseSerializer: ResponseSerializer {
-    public let dataPreprocessor: DataPreprocessor
-    public let emptyResponseCodes: Set<Int>
-    public let emptyRequestMethods: Set<HTTPMethod>
-    /// `JSONSerialization.ReadingOptions` used when serializing a response.
-    public let readingOptions: JSONSerialization.ReadingOptions
+    private let jsonResponseSerializer: JSONResponseSerializer
 
-    /// Creates an instance with the provided values.
-    ///
-    /// - Parameters:
-    ///   - dataPreprocessor:    `DataPreprocessor` used to prepare the received `Data` for serialization.
-    ///   - emptyResponseCodes:  The HTTP response codes for which empty responses are allowed. `[204, 205]` by default.
-    ///   - emptyRequestMethods: The HTTP request methods for which empty responses are allowed. `[.head]` by default.
-    ///   - readingOptions:      The options to use. `.allowFragments` by default.
     public init(
-        dataPreprocessor: DataPreprocessor = SwiftyJSONResponseSerializer.defaultDataPreprocessor,
-        emptyResponseCodes: Set<Int> = SwiftyJSONResponseSerializer.defaultEmptyResponseCodes,
-        emptyRequestMethods: Set<HTTPMethod> = SwiftyJSONResponseSerializer.defaultEmptyRequestMethods,
-        readingOptions: JSONSerialization.ReadingOptions = .allowFragments
+        dataPreprocessor: DataPreprocessor = JSONResponseSerializer.defaultDataPreprocessor,
+        emptyResponseCodes: Set<Int> = JSONResponseSerializer.defaultEmptyResponseCodes,
+        emptyRequestMethods: Set<HTTPMethod> = JSONResponseSerializer.defaultEmptyRequestMethods,
+        options: JSONSerialization.ReadingOptions = .allowFragments
     ) {
-        self.dataPreprocessor = dataPreprocessor
-        self.emptyResponseCodes = emptyResponseCodes
-        self.emptyRequestMethods = emptyRequestMethods
-        self.readingOptions = readingOptions
+        self.jsonResponseSerializer = JSONResponseSerializer(
+            dataPreprocessor: dataPreprocessor,
+            emptyResponseCodes: emptyResponseCodes,
+            emptyRequestMethods: emptyRequestMethods,
+            options: options
+        )
     }
 
-    public func serialize(request: URLRequest?, response: HTTPURLResponse?, data: Data?, error: Error?) throws -> JSON {
-        guard error == nil else { throw error! }
+    public func serialize(
+        request: URLRequest?,
+        response: HTTPURLResponse?,
+        data: Data?,
+        error: Error?
+    ) throws -> JSON {
+        let json = try self.jsonResponseSerializer.serialize(
+            request: request,
+            response: response,
+            data: data,
+            error: error
+        )
 
-        guard var data = data, !data.isEmpty else {
-            guard self.emptyResponseAllowed(forRequest: request, response: response) else {
-                throw AFError.responseSerializationFailed(reason: .inputDataNilOrZeroLength)
-            }
-
-            return JSON(booleanLiteral: false)
-        }
-
-        data = try self.dataPreprocessor.preprocess(data)
-
-        do {
-            let json = try JSONSerialization.jsonObject(with: data, options: self.readingOptions)
-            return JSON(json)
-        } catch {
-            throw AFError.responseSerializationFailed(reason: .jsonSerializationFailed(error: error))
-        }
+        return JSON(json)
     }
 }
 
@@ -70,7 +56,7 @@ extension DataRequest {
     ) -> Self {
         self.response(
             queue: queue,
-            responseSerializer: SwiftyJSONResponseSerializer(readingOptions: options),
+            responseSerializer: SwiftyJSONResponseSerializer(options: options),
             completionHandler: completionHandler
         )
     }
