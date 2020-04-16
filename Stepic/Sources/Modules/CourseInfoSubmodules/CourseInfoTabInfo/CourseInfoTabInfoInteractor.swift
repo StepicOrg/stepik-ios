12e919e6a534311fda9ebf9d3e742c5fab79f049
Ivan Magda <ivan.magda@stepik.org>
2020-04-16 14:11:07 +0300

    Course buying analytics (#697)
    * Remove redundant 'return'
    
    * Add 'is_paid' parameter for 'Course preview screen opened' event
    
    * Add 'Buy course pressed' event
    
    * Fix send 'Course preview screen opened' event
    
    * Refactor rename CourseBuyingSource.preview -> courseScreen

--- Stepic/Sources/Modules/CourseInfoSubmodules/CourseInfoTabInfo/CourseInfoTabInfoInteractor.swift
+++ Stepic/Sources/Modules/CourseInfoSubmodules/CourseInfoTabInfo/CourseInfoTabInfoInteractor.swift
@@ -11,7 +11,7 @@ final class CourseInfoTabInfoInteractor: CourseInfoTabInfoInteractorProtocol {
 
     private var course: Course?
 
-    private var shouldOpenedAnalyticsEventSend = false
+    private var shouldOpenedAnalyticsEventSend: Bool = true
 
     init(presenter: CourseInfoTabInfoPresenterProtocol, provider: CourseInfoTabInfoProviderProtocol) {
         self.presenter = presenter
@@ -52,7 +52,9 @@ final class CourseInfoTabInfoInteractor: CourseInfoTabInfoInteractorProtocol {
 extension CourseInfoTabInfoInteractor: CourseInfoTabInfoInputProtocol {
     func handleControllerAppearance() {
         if let course = self.course {
-            AmplitudeAnalyticsEvents.CoursePreview.opened(courseID: course.id, courseTitle: course.title).send()
+            AmplitudeAnalyticsEvents.CoursePreview
+                .opened(courseID: course.id, courseTitle: course.title, isPaid: course.isPaid)
+                .send()
             self.shouldOpenedAnalyticsEventSend = false
         } else {
             self.shouldOpenedAnalyticsEventSend = true
@@ -64,7 +66,9 @@ extension CourseInfoTabInfoInteractor: CourseInfoTabInfoInputProtocol {
         self.doCourseInfoRefresh(request: .init())
 
         if self.shouldOpenedAnalyticsEventSend {
-            AmplitudeAnalyticsEvents.CoursePreview.opened(courseID: course.id, courseTitle: course.title).send()
+            AmplitudeAnalyticsEvents.CoursePreview
+                .opened(courseID: course.id, courseTitle: course.title, isPaid: course.isPaid)
+                .send()
             self.shouldOpenedAnalyticsEventSend = false
         }
     }
