f751b305423989b187e8efad519ae938859403a3
Ivan Magda <ivan.magda@stepik.org>
2020-07-03 15:33:40 +0900

    Schedule notifications

--- Stepic/Sources/Modules/NewProfileSubmodules/StreakNotifications/NewProfileStreakNotificationsAssembly.swift
+++ Stepic/Sources/Modules/NewProfileSubmodules/StreakNotifications/NewProfileStreakNotificationsAssembly.swift
@@ -7,7 +7,8 @@ final class NewProfileStreakNotificationsAssembly: Assembly {
         let presenter = NewProfileStreakNotificationsPresenter()
         let interactor = NewProfileStreakNotificationsInteractor(
             presenter: presenter,
-            streakNotificationsStorageManager: StreakNotificationsStorageManager()
+            streakNotificationsStorageManager: StreakNotificationsStorageManager(),
+            notificationsService: NotificationsService()
         )
         let viewController = NewProfileStreakNotificationsViewController(interactor: interactor)
 
--- Stepic/Sources/Modules/NewProfileSubmodules/StreakNotifications/NewProfileStreakNotificationsInteractor.swift
+++ Stepic/Sources/Modules/NewProfileSubmodules/StreakNotifications/NewProfileStreakNotificationsInteractor.swift
@@ -14,13 +14,41 @@ protocol NewProfileStreakNotificationsInteractorProtocol {
 final class NewProfileStreakNotificationsInteractor: NewProfileStreakNotificationsInteractorProtocol {
     private let presenter: NewProfileStreakNotificationsPresenterProtocol
     private let streakNotificationsStorageManager: StreakNotificationsStorageManagerProtocol
+    private let notificationsService: NotificationsService
+    private let notificationsRegistrationService: NotificationsRegistrationServiceProtocol
+    private let analyticsUserProperties: AnalyticsUserProperties
+    private let analytics: Analytics
 
     init(
         presenter: NewProfileStreakNotificationsPresenterProtocol,
-        streakNotificationsStorageManager: StreakNotificationsStorageManagerProtocol
+        streakNotificationsStorageManager: StreakNotificationsStorageManagerProtocol,
+        notificationsService: NotificationsService,
+        notificationsRegistrationService: NotificationsRegistrationServiceProtocol = NotificationsRegistrationService(
+            presenter: NotificationsRequestOnlySettingsAlertPresenter(context: .streak),
+            analytics: .init(source: .streakControl)
+        ),
+        analyticsUserProperties: AnalyticsUserProperties = AnalyticsUserProperties.shared,
+        analytics: Analytics = StepikAnalytics.shared
     ) {
         self.presenter = presenter
         self.streakNotificationsStorageManager = streakNotificationsStorageManager
+        self.notificationsService = notificationsService
+        self.notificationsRegistrationService = notificationsRegistrationService
+        self.analyticsUserProperties = analyticsUserProperties
+        self.analytics = analytics
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(onPermissionStatusUpdate(_:)),
+            name: .notificationsRegistrationServiceDidUpdatePermissionStatus,
+            object: nil
+        )
+
+        self.checkPermissionStatus()
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
 
     func doStreakNotificationsLoad(request: NewProfileStreakNotifications.StreakNotificationsLoad.Request) {
@@ -33,7 +61,7 @@ final class NewProfileStreakNotificationsInteractor: NewProfileStreakNotificatio
         request: NewProfileStreakNotifications.StreakNotificationsPreferenceUpdate.Request
     ) {
         DispatchQueue.main.async {
-            self.streakNotificationsStorageManager.isStreakNotificationsEnabled = request.isOn
+            self.setStreakNotificationsEnabled(request.isOn)
             self.presentStreakNotifications()
         }
     }
@@ -45,13 +73,59 @@ final class NewProfileStreakNotificationsInteractor: NewProfileStreakNotificatio
         self.presenter.presentSelectStreakNotificationsTime(response: .init(startHour: startHour))
     }
 
-    private func presentStreakNotifications() {
+    // MARK: Private API
+
+    private func presentStreakNotifications(isStreakNotificationsEnabled: Bool? = nil) {
+        let isOn = isStreakNotificationsEnabled ?? self.streakNotificationsStorageManager.isStreakNotificationsEnabled
         self.presenter.presentStreakNotifications(
             response: .init(
-                isStreakNotificationsEnabled: self.streakNotificationsStorageManager.isStreakNotificationsEnabled,
+                isStreakNotificationsEnabled: isOn,
                 streaksNotificationsStartHour: self.streakNotificationsStorageManager.streakNotificationsStartHourUTC
             )
         )
+    }
+
+    private func setStreakNotificationsEnabled(_ enabled: Bool) {
+        self.analyticsUserProperties.setStreaksNotificationsEnabled(enabled)
+
+        if enabled {
+            self.streakNotificationsStorageManager.isStreakNotificationsEnabled = true
+            self.notificationsRegistrationService.registerForRemoteNotifications()
+
+            self.notificationsService.scheduleStreakLocalNotification(
+                UTCStartHour: self.streakNotificationsStorageManager.streakNotificationsStartHourUTC,
+                cancelPrevious: true
+            )
+
+            self.analytics.send(.streaksPreferenceOn)
+        } else {
+            self.turnOffStreakNotifications()
+        }
+    }
+
+    private func turnOffStreakNotifications() {
+        self.notificationsService.cancelStreakLocalNotifications()
+        self.streakNotificationsStorageManager.isStreakNotificationsEnabled = false
+        self.analytics.send(.streaksPreferenceOff)
+    }
+
+    @objc
+    private func onPermissionStatusUpdate(_ notification: Foundation.Notification) {
+        guard let permissionStatus = notification.object as? NotificationPermissionStatus else {
+            return
+        }
+
+        let isOn = self.streakNotificationsStorageManager.isStreakNotificationsEnabled && permissionStatus.isRegistered
+        self.presentStreakNotifications(isStreakNotificationsEnabled: isOn)
+    }
+
+    private func checkPermissionStatus() {
+        NotificationPermissionStatus.current.done { permissionStatus in
+            if self.streakNotificationsStorageManager.isStreakNotificationsEnabled && !permissionStatus.isRegistered {
+                self.turnOffStreakNotifications()
+                self.doStreakNotificationsLoad(request: .init())
+            }
+        }
     }
 }
 
