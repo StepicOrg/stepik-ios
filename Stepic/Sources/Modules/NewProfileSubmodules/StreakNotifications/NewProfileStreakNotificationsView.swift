00217ba81cb5031b392a0cebf421edcb2c12f76e
Ivan Magda <ivan.magda@stepik.org>
2020-07-03 12:18:37 +0900

    Add views

--- Stepic/Sources/Modules/NewProfileSubmodules/StreakNotifications/NewProfileStreakNotificationsView.swift
+++ Stepic/Sources/Modules/NewProfileSubmodules/StreakNotifications/NewProfileStreakNotificationsView.swift
@@ -2,11 +2,45 @@ import SnapKit
 import UIKit
 
 extension NewProfileStreakNotificationsView {
-    struct Appearance {}
+    struct Appearance {
+        let arrangedSubviewHeight: CGFloat = 44
+        let backgroundColor = UIColor.clear
+    }
 }
 
 final class NewProfileStreakNotificationsView: UIView {
     let appearance: Appearance
+
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        return stackView
+    }()
+
+    private lazy var streakNotificationsSwitchView: NewProfileStreakNotificationsSwitchView = {
+        let view = NewProfileStreakNotificationsSwitchView()
+        view.onSwitchValueChanged = { isOn in
+            print("SwitchValueChanged isOn=\(isOn)")
+        }
+        return view
+    }()
+
+    private lazy var streakNotificationsTimeSelectionView: NewProfileStreakNotificationsTimeSelectionView = {
+        let view = NewProfileStreakNotificationsTimeSelectionView()
+        view.addTarget(self, action: #selector(self.didTouchTimeSelection), for: .touchUpInside)
+        return view
+    }()
+
+    private lazy var streakNotificationsFooterView = NewProfileStreakNotificationsFooterView()
+
+    override var intrinsicContentSize: CGSize {
+        let stackViewIntrinsicContentSize = self.stackView
+            .systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        return CGSize(
+            width: UIView.noIntrinsicMetric,
+            height: stackViewIntrinsicContentSize.height
+        )
+    }
 
     init(
         frame: CGRect = .zero,
@@ -18,18 +52,49 @@ final class NewProfileStreakNotificationsView: UIView {
         self.setupView()
         self.addSubviews()
         self.makeConstraints()
+
+        self.streakNotificationsTimeSelectionView.subtitle = "8:00PM – 9:00 PM"
+        self.streakNotificationsFooterView.text = "Рекорды обновляются в 3:00 AM"
     }
 
     @available(*, unavailable)
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    @objc
+    private func didTouchTimeSelection() {
+        print(#function)
+    }
 }
 
 extension NewProfileStreakNotificationsView: ProgrammaticallyInitializableViewProtocol {
-    func setupView() {}
+    func setupView() {
+        self.backgroundColor = self.appearance.backgroundColor
+    }
 
-    func addSubviews() {}
+    func addSubviews() {
+        self.addSubview(self.stackView)
 
-    func makeConstraints() {}
+        self.stackView.addArrangedSubview(self.streakNotificationsSwitchView)
+        self.stackView.addArrangedSubview(self.streakNotificationsTimeSelectionView)
+        self.stackView.addArrangedSubview(self.streakNotificationsFooterView)
+    }
+
+    func makeConstraints() {
+        self.stackView.translatesAutoresizingMaskIntoConstraints = false
+        self.stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+
+        self.streakNotificationsSwitchView.translatesAutoresizingMaskIntoConstraints = false
+        self.streakNotificationsSwitchView.snp.makeConstraints { make in
+            make.height.equalTo(self.appearance.arrangedSubviewHeight)
+        }
+
+        self.streakNotificationsTimeSelectionView.translatesAutoresizingMaskIntoConstraints = false
+        self.streakNotificationsTimeSelectionView.snp.makeConstraints { make in
+            make.height.equalTo(self.appearance.arrangedSubviewHeight)
+        }
+    }
 }
---
+++ Stepic/Sources/Modules/NewProfileSubmodules/StreakNotifications/Views/NewProfileStreakNotificationsTimeSelectionView.swift
@@ -0,0 +1,120 @@
+import SnapKit
+import UIKit
+
+extension NewProfileStreakNotificationsTimeSelectionView {
+    struct Appearance {
+        let titleLabelFont = UIFont.systemFont(ofSize: 17, weight: .regular)
+        let titleLabelTextColor = UIColor.stepikSystemPrimaryText
+        let titleLabelInsets = LayoutInsets(left: 16)
+
+        let detailLabelFont = UIFont.systemFont(ofSize: 17, weight: .regular)
+        let detailLabelTextColor = UIColor.stepikSystemSecondaryText
+        let detailLabelInsets = LayoutInsets(left: 8)
+
+        let detailDisclosureImageSize = CGSize(width: 11, height: 14)
+        let detailDisclosureImageTintColor = UIColor.stepikSystemTertiaryText
+        let detailDisclosureImageInsets = LayoutInsets(left: 8, right: 16)
+
+        let backgroundColor = UIColor.stepikBackground
+    }
+}
+
+final class NewProfileStreakNotificationsTimeSelectionView: UIControl {
+    let appearance: Appearance
+
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = self.appearance.titleLabelTextColor
+        label.font = self.appearance.titleLabelFont
+        label.numberOfLines = 1
+        label.text = NSLocalizedString("NewProfileStreakNotificationsTimeSelection", comment: "")
+        return label
+    }()
+
+    private lazy var detailLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = self.appearance.detailLabelTextColor
+        label.font = self.appearance.detailLabelFont
+        label.numberOfLines = 1
+        return label
+    }()
+
+    private lazy var detailDisclosureImageView: UIImageView = {
+        let image = UIImage(named: "chevron.right")?.withRenderingMode(.alwaysTemplate)
+        let imageView = UIImageView(image: image)
+        imageView.tintColor = self.appearance.detailDisclosureImageTintColor
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    var subtitle: String? {
+        didSet {
+            self.detailLabel.text = self.subtitle
+        }
+    }
+
+    override var isHighlighted: Bool {
+        didSet {
+            let alpha: CGFloat = self.isHighlighted ? 0.5 : 1.0
+            self.titleLabel.alpha = alpha
+            self.detailLabel.alpha = alpha
+            self.detailDisclosureImageView.alpha = alpha
+        }
+    }
+
+    init(
+        frame: CGRect = .zero,
+        appearance: Appearance = Appearance()
+    ) {
+        self.appearance = appearance
+        super.init(frame: frame)
+
+        self.setupView()
+        self.addSubviews()
+        self.makeConstraints()
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension NewProfileStreakNotificationsTimeSelectionView: ProgrammaticallyInitializableViewProtocol {
+    func setupView() {
+        self.backgroundColor = self.appearance.backgroundColor
+    }
+
+    func addSubviews() {
+        self.addSubview(self.titleLabel)
+        self.addSubview(self.detailLabel)
+        self.addSubview(self.detailDisclosureImageView)
+    }
+
+    func makeConstraints() {
+        self.titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.titleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(self.appearance.titleLabelInsets.left)
+            make.centerY.equalToSuperview()
+        }
+
+        self.detailLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.detailLabel.snp.makeConstraints { make in
+            make.leading
+                .greaterThanOrEqualTo(self.titleLabel.snp.trailing)
+                .offset(self.appearance.detailLabelInsets.left)
+            make.centerY.equalToSuperview()
+        }
+
+        self.detailDisclosureImageView.translatesAutoresizingMaskIntoConstraints = false
+        self.detailDisclosureImageView.snp.makeConstraints { make in
+            make.leading
+                .equalTo(self.detailLabel.snp.trailing)
+                .offset(self.appearance.detailDisclosureImageInsets.left)
+            make.trailing.equalToSuperview().offset(-self.appearance.detailDisclosureImageInsets.right)
+            make.centerY.equalToSuperview()
+            make.width.equalTo(self.appearance.detailDisclosureImageSize.width)
+            make.height.equalTo(self.appearance.detailDisclosureImageSize.height)
+        }
+    }
+}
