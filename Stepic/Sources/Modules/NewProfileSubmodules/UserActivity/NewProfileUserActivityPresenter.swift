41505e8a5338c41ef6a52f453460f22ce3b5be2b
Ivan Magda <ivan.magda@stepik.org>
2020-07-15 12:02:25 +0900

    Update presentation

--- Stepic/Sources/Modules/NewProfileSubmodules/UserActivity/NewProfileUserActivityPresenter.swift
+++ Stepic/Sources/Modules/NewProfileSubmodules/UserActivity/NewProfileUserActivityPresenter.swift
@@ -9,21 +9,33 @@ final class NewProfileUserActivityPresenter: NewProfileUserActivityPresenterProt
 
     func presentUserActivity(response: NewProfileUserActivity.ActivityLoad.Response) {
         switch response.result {
-        case .success(let userActivity):
-            let viewModel = self.makeViewModel(userActivity: userActivity)
+        case .success(let data):
+            let viewModel = self.makeViewModel(
+                userActivity: data.userActivity,
+                isCurrentUserProfile: data.isCurrentUserProfile
+            )
             self.viewController?.displayUserActivity(viewModel: .init(state: .result(data: viewModel)))
         case .failure:
             self.viewController?.displayUserActivity(viewModel: .init(state: .error))
         }
     }
 
-    private func makeViewModel(userActivity: UserActivity) -> NewProfileUserActivityViewModel {
-        let currentStreakText = userActivity.currentStreak > 0
-            ? String(
-                format: NSLocalizedString("NewProfileUserActivityCurrentStreak", comment: ""),
-                arguments: [FormatterHelper.daysCount(userActivity.currentStreak)]
-              )
-            : NSLocalizedString("NewProfileUserActivityNoCurrentStreak", comment: "")
+    private func makeViewModel(
+        userActivity: UserActivity,
+        isCurrentUserProfile: Bool
+    ) -> NewProfileUserActivityViewModel {
+        let currentStreakText: String = {
+            if userActivity.currentStreak > 0 {
+                return String(
+                    format: NSLocalizedString("NewProfileUserActivityCurrentStreak", comment: ""),
+                    arguments: [FormatterHelper.daysCount(userActivity.currentStreak)]
+                )
+            } else if isCurrentUserProfile {
+                return NSLocalizedString("NewProfileUserActivityNoCurrentStreak", comment: "")
+            } else {
+                return ""
+            }
+        }()
 
         let longestStreakText = userActivity.longestStreak > 0
             ? String(
