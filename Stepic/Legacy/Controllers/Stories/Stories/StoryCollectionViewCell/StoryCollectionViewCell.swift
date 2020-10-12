fd6dbaf1770eab95a3e487c123b75a77bba2fa47
Ivan Magda <ivan.magda@stepik.org>
2020-10-12 19:34:31 +0300

    Update story collection view cell appearance

--- Stepic/Legacy/Controllers/Stories/Stories/StoryCollectionViewCell/StoryCollectionViewCell.swift
+++ Stepic/Legacy/Controllers/Stories/Stories/StoryCollectionViewCell/StoryCollectionViewCell.swift
@@ -1,21 +1,29 @@
-//
-//  StoryCollectionViewCell.swift
-//  stepik-stories
-//
-//  Created by Ostrenkiy on 06.08.2018.
-//  Copyright © 2018 Ostrenkiy. All rights reserved.
-//
-
 import Nuke
 import UIKit
 
+extension StoryCollectionViewCell {
+    struct Appearance {
+        let gradientImageViewCornerRadius: CGFloat = 15
+
+        let backgroundContentViewCornerRadius: CGFloat = 13
+        let backgroundContentViewBackgroundColor = UIColor.stepikBackground
+
+        let titleLabelTextColor = UIColor.white
+        let titleLabelFont = Typography.caption1Font
+
+        let overlayViewBackgroundColor = UIColor.stepikAccentFixed.withAlphaComponent(0.5)
+    }
+}
+
 final class StoryCollectionViewCell: UICollectionViewCell {
+    let appearance = Appearance()
+
+    @IBOutlet var gradientImageView: UIImageView!
+    @IBOutlet var backgroundContentView: UIView!
+    @IBOutlet var contentContainerView: UIView!
+    @IBOutlet var overlayView: UIView!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet var overlayView: UIView!
-
-    let cornerRadius: CGFloat = 16
-    let unwatchedColor = UIColor.stepikAccent
 
     var imagePath: String = "" {
         didSet {
@@ -33,60 +41,37 @@ final class StoryCollectionViewCell: UICollectionViewCell {
 
     var isWatched: Bool = true {
         didSet {
-            self.updateWatched()
-        }
-    }
-
-    private func updateWatched() {
-        if self.isDarkInterfaceStyle {
-            self.layer.borderColor = self.isWatched
-                ? UIColor.stepikSecondaryBackground.withAlphaComponent(0.5).cgColor
-                : UIColor.stepikTertiaryBackground.cgColor
-        } else {
-            self.layer.borderColor = self.isWatched
-                ? UIColor.stepikGrey.cgColor
-                : self.unwatchedColor.cgColor
+            self.gradientImageView.isHidden = self.isWatched
         }
     }
 
     override func awakeFromNib() {
         super.awakeFromNib()
 
-        self.contentView.layer.cornerRadius = self.cornerRadius
-        self.contentView.layer.borderWidth = 4
-        self.contentView.clipsToBounds = true
-        self.contentView.layer.masksToBounds = true
+        self.gradientImageView.layer.cornerRadius = self.appearance.gradientImageViewCornerRadius
+        self.gradientImageView.clipsToBounds = true
+        self.gradientImageView.layer.masksToBounds = true
 
-        self.layer.cornerRadius = self.cornerRadius
-        self.layer.borderWidth = 2
-        self.layer.borderColor = self.unwatchedColor.cgColor
-        self.clipsToBounds = true
-        self.layer.masksToBounds = true
+        self.backgroundContentView.layer.cornerRadius = self.appearance.backgroundContentViewCornerRadius
+        self.backgroundContentView.clipsToBounds = true
+        self.backgroundContentView.layer.masksToBounds = true
 
-        self.colorize()
+        self.contentContainerView.layer.cornerRadius = self.appearance.backgroundContentViewCornerRadius
+        self.contentContainerView.clipsToBounds = true
+        self.contentContainerView.layer.masksToBounds = true
+
+        self.backgroundContentView.backgroundColor = self.appearance.backgroundContentViewBackgroundColor
+        self.overlayView.backgroundColor = self.appearance.overlayViewBackgroundColor
+
+        self.titleLabel.textColor = self.appearance.titleLabelTextColor
+        self.titleLabel.font = self.appearance.titleLabelFont
+
         self.update(imagePath: self.imagePath, title: self.title, isWatched: self.isWatched)
-    }
-
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        super.traitCollectionDidChange(previousTraitCollection)
-
-        self.performBlockIfAppearanceChanged(from: previousTraitCollection) {
-            self.colorize()
-        }
     }
 
     func update(imagePath: String, title: String, isWatched: Bool) {
         self.imagePath = imagePath
         self.title = title
         self.isWatched = isWatched
-    }
-
-    private func colorize() {
-        self.contentView.layer.borderColor = UIColor.stepikBackground.cgColor
-
-        self.titleLabel.textColor = .white
-        self.overlayView.backgroundColor = UIColor.stepikAccentFixed.withAlphaComponent(0.5)
-
-        self.updateWatched()
     }
 }
