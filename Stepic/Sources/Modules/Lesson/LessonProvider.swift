23b82344bbfdb03aedbc07aaf976eea5f3c11136
Ivan Magda <ivan.magda@stepik.org>
2020-10-14 09:47:48 +0300

    Update provider to support DataSourceType

--- Stepic/Sources/Modules/Lesson/LessonProvider.swift
+++ Stepic/Sources/Modules/Lesson/LessonProvider.swift
@@ -2,11 +2,11 @@ import Foundation
 import PromiseKit
 
 protocol LessonProviderProtocol {
-    func fetchLesson(id: Lesson.IdType) -> Promise<FetchResult<Lesson?>>
-    func fetchLessonAndUnit(unitID: Unit.IdType) -> Promise<(FetchResult<Unit?>, FetchResult<Lesson?>)>
-    func fetchSteps(ids: [Step.IdType]) -> Promise<FetchResult<[Step]?>>
-    func fetchAssignments(ids: [Assignment.IdType]) -> Promise<FetchResult<[Assignment]?>>
-    func fetchProgresses(ids: [Progress.IdType]) -> Promise<FetchResult<[Progress]?>>
+    func fetchLesson(id: Lesson.IdType, dataSourceType: DataSourceType) -> Promise<Lesson?>
+    func fetchLessonAndUnit(unitID: Unit.IdType, dataSourceType: DataSourceType) -> Promise<(Unit?, Lesson?)>
+    func fetchSteps(ids: [Step.IdType], dataSourceType: DataSourceType) -> Promise<[Step]>
+    func fetchAssignments(ids: [Assignment.IdType], dataSourceType: DataSourceType) -> Promise<[Assignment]>
+    func fetchProgresses(ids: [Progress.IdType], dataSourceType: DataSourceType) -> Promise<[Progress]>
 
     func createView(stepID: Step.IdType, assignmentID: Assignment.IdType?) -> Promise<Void>
 }
@@ -52,82 +52,41 @@ final class LessonProvider: LessonProviderProtocol {
 
     // MARK: Public API
 
-    func fetchLesson(id: Lesson.IdType) -> Promise<FetchResult<Lesson?>> {
-        let persistenceServicePromise = Guarantee(self.lessonsPersistenceService.fetch(ids: [id]), fallback: nil)
-        let networkServicePromise = Guarantee(self.lessonsNetworkService.fetch(ids: [id]), fallback: nil)
-
-        return Promise { seal in
-            when(
-                fulfilled: persistenceServicePromise,
-                networkServicePromise
-            ).then { cachedLessons, remoteLessons -> Promise<FetchResult<Lesson?>> in
-                if let remoteLesson = remoteLessons?.first {
-                    let result = FetchResult<Lesson?>(value: remoteLesson, source: .remote)
-                    return Promise.value(result)
+    func fetchLesson(id: Lesson.IdType, dataSourceType: DataSourceType) -> Promise<Lesson?> {
+        Promise { seal in
+            firstly { () -> Promise<[Lesson]> in
+                switch dataSourceType {
+                case .remote:
+                    return self.lessonsNetworkService.fetch(ids: [id])
+                case .cache:
+                    return self.lessonsPersistenceService.fetch(ids: [id])
                 }
-
-                let result = FetchResult<Lesson?>(value: cachedLessons?.first, source: .cache)
-                return Promise.value(result)
-            }.done { result in
-                seal.fulfill(result)
+            }.done { lessons in
+                seal.fulfill(lessons.first)
             }.catch { _ in
                 seal.reject(Error.fetchFailed)
             }
         }
     }
 
-    func fetchLessonAndUnit(unitID: Unit.IdType) -> Promise<(FetchResult<Unit?>, FetchResult<Lesson?>)> {
-        let persistenceServicePromise = Guarantee(self.unitsPersistenceService.fetch(ids: [unitID]), fallback: nil)
-        let networkServicePromise = Guarantee(self.unitsNetworkService.fetch(ids: [unitID]), fallback: nil)
-
-        return Promise { seal in
-            when(
-                fulfilled: persistenceServicePromise,
-                networkServicePromise
-            ).then { cachedUnits, remoteUnits -> Promise<(FetchResult<Unit?>, FetchResult<Lesson?>)> in
-                if let remoteUnit = remoteUnits?.first {
-                    let result = FetchResult<Unit?>(value: remoteUnit, source: .remote)
+    func fetchLessonAndUnit(unitID: Unit.IdType, dataSourceType: DataSourceType) -> Promise<(Unit?, Lesson?)> {
+        Promise { seal in
+            firstly { () -> Promise<[Unit]> in
+                switch dataSourceType {
+                case .remote:
+                    return self.unitsNetworkService.fetch(ids: [unitID])
+                case .cache:
+                    return self.unitsPersistenceService.fetch(ids: [unitID])
+                }
+            }.then { units -> Promise<(Unit?, Lesson?)> in
+                if let unit = units.first {
                     return when(
-                        fulfilled: Promise.value(result),
-                        self.fetchLesson(id: remoteUnit.lessonId)
+                        fulfilled: Promise.value(unit as Unit?),
+                        self.fetchLesson(id: unit.lessonId, dataSourceType: dataSourceType)
                     )
+                } else {
+                    return .value((nil, nil))
                 }
-
-                if let cachedUnit = cachedUnits?.first {
-                    let result = FetchResult<Unit?>(value: cachedUnit, source: .cache)
-                    return when(
-                        fulfilled: Promise.value(result),
-                        self.fetchLesson(id: cachedUnit.lessonId)
-                    )
-                }
-
-                let unitResult = FetchResult<Unit?>(value: nil, source: .cache)
-                let lessonResult = FetchResult<Lesson?>(value: nil, source: .cache)
-                return Promise.value((unitResult, lessonResult))
-            }.done { unitResult, lessonResult in
-                seal.fulfill((unitResult, lessonResult))
-            }.catch { _ in
-                seal.reject(Error.fetchFailed)
-            }
-        }
-    }
-
-    func fetchSteps(ids: [Step.IdType]) -> Promise<FetchResult<[Step]?>> {
-        let persistenceServicePromise = Guarantee(self.stepsPersistenceService.fetch(ids: ids), fallback: nil)
-        let networkServicePromise = Guarantee(self.stepsNetworkService.fetch(ids: ids), fallback: nil)
-
-        return Promise { seal in
-            when(
-                fulfilled: persistenceServicePromise,
-                networkServicePromise
-            ).then { cachedSteps, remoteSteps -> Promise<FetchResult<[Step]?>> in
-                if let remoteSteps = remoteSteps {
-                    let result = FetchResult<[Step]?>(value: remoteSteps, source: .remote)
-                    return Promise.value(result)
-                }
-
-                let result = FetchResult<[Step]?>(value: cachedSteps, source: .cache)
-                return Promise.value(result)
             }.done { result in
                 seal.fulfill(result)
             }.catch { _ in
@@ -136,54 +95,51 @@ final class LessonProvider: LessonProviderProtocol {
         }
     }
 
-    func fetchAssignments(ids: [Assignment.IdType]) -> Promise<FetchResult<[Assignment]?>> {
-        let persistenceServicePromise = Guarantee(self.assignmentsPersistenceService.fetch(ids: ids), fallback: nil)
-        let networkServicePromise = Guarantee(self.assignmentsNetworkService.fetch(ids: ids), fallback: nil)
-
-        return Promise { seal in
-            when(
-                fulfilled: persistenceServicePromise,
-                networkServicePromise
-            ).then { cachedAssignments, remoteAssignments -> Promise<FetchResult<[Assignment]?>> in
-                if let remoteAssignments = remoteAssignments {
-                    let result = FetchResult<[Assignment]?>(value: remoteAssignments, source: .remote)
-                    return Promise.value(result)
+    func fetchSteps(ids: [Step.IdType], dataSourceType: DataSourceType) -> Promise<[Step]> {
+        Promise { seal in
+            firstly { () -> Promise<[Step]> in
+                switch dataSourceType {
+                case .remote:
+                    return self.stepsNetworkService.fetch(ids: ids)
+                case .cache:
+                    return self.stepsPersistenceService.fetch(ids: ids)
                 }
-
-                let result = FetchResult<[Assignment]?>(value: cachedAssignments, source: .cache)
-                return Promise.value(result)
-            }.done { result in
-                seal.fulfill(result)
+            }.done { steps in
+                seal.fulfill(steps)
             }.catch { _ in
                 seal.reject(Error.fetchFailed)
             }
         }
     }
 
-    func fetchProgresses(ids: [Progress.IdType]) -> Promise<FetchResult<[Progress]?>> {
-        let persistenceServicePromise = Guarantee(
-            self.progressesPersistenceService.fetch(ids: ids, page: 1),
-            fallback: nil
-        )
-        let networkServicePromise = Guarantee(
-            self.progressesNetworkService.fetch(ids: ids, page: 1),
-            fallback: nil
-        )
-
-        return Promise { seal in
-            when(
-                fulfilled: persistenceServicePromise,
-                networkServicePromise
-            ).then { cachedProgresses, remoteProgresses -> Promise<FetchResult<[Progress]?>> in
-                if let remoteProgresses = remoteProgresses?.0 {
-                    let result = FetchResult<[Progress]?>(value: remoteProgresses, source: .remote)
-                    return Promise.value(result)
+    func fetchAssignments(ids: [Assignment.IdType], dataSourceType: DataSourceType) -> Promise<[Assignment]> {
+        Promise { seal in
+            firstly { () -> Promise<[Assignment]> in
+                switch dataSourceType {
+                case .remote:
+                    return self.assignmentsNetworkService.fetch(ids: ids)
+                case .cache:
+                    return self.assignmentsPersistenceService.fetch(ids: ids)
                 }
+            }.done { assignments in
+                seal.fulfill(assignments)
+            }.catch { _ in
+                seal.reject(Error.fetchFailed)
+            }
+        }
+    }
 
-                let result = FetchResult<[Progress]?>(value: cachedProgresses?.0, source: .cache)
-                return Promise.value(result)
-            }.done { result in
-                seal.fulfill(result)
+    func fetchProgresses(ids: [Progress.IdType], dataSourceType: DataSourceType) -> Promise<[Progress]> {
+        Promise { seal in
+            firstly { () -> Promise<([Progress], Meta)> in
+                switch dataSourceType {
+                case .remote:
+                    return self.progressesNetworkService.fetch(ids: ids, page: 1)
+                case .cache:
+                    return self.progressesPersistenceService.fetch(ids: ids, page: 1)
+                }
+            }.done { progresses, _ in
+                seal.fulfill(progresses)
             }.catch { _ in
                 seal.reject(Error.fetchFailed)
             }
