a7f45c1a4a45ee6a2149dc6a570221a51d550466
Ivan Magda <ivan.magda@stepik.org>
2020-10-22 13:46:20 +0300

    Update HTMLExtractorTests APPS-3064

--- StepicTests/HTMLExtractorTests.swift
+++ StepicTests/HTMLExtractorTests.swift
@@ -4,15 +4,29 @@ import Stepic
 import XCTest
 
 final class HTMLExtractorTests: XCTestCase {
-    func testThatHTMLExtractorNotCrashesWhenParsingStringWithWhitespace() {
+    func testThatHTMLExtractorNotCrashesWhenParsingStringWithWhitespacesAndNewlines() {
         // Given
-        let value = " "
+        let characterSet = CharacterSet.whitespacesAndNewlines
+        var characters = [Character]()
+        for plane: UInt8 in 0...16 where characterSet.hasMember(inPlane: plane) {
+            for unicode in UInt32(plane) << 16 ..< UInt32(plane + 1) << 16 {
+                if let uniChar = UnicodeScalar(unicode), characterSet.contains(uniChar) {
+                    characters.append(Character(uniChar))
+                }
+            }
+        }
+        let strings = characters.map { String($0) }
+
         let extractorType: HTMLExtractorProtocol.Type = HTMLExtractor.self
 
         // When
-        let res = extractorType.extractAllTagsAttribute(tag: "a", attribute: "href", from: value)
+        let results = strings.map { string in
+            extractorType.extractAllTagsAttribute(tag: "a", attribute: "href", from: string)
+        }
 
         // Then
-        XCTAssertEqual(res, [])
+        results.forEach { result in
+            XCTAssertEqual(result, [])
+        }
     }
 }
