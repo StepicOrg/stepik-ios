09cc5652655f2108f99bdbe33916b08f04957b9b
Ivan Magda <ivan.magda@stepik.org>
2021-01-13 19:31:16 +0300

    Add stories submodule to home

--- Stepic/Sources/Modules/Home/HomeDataFlow.swift
+++ Stepic/Sources/Modules/Home/HomeDataFlow.swift
@@ -6,6 +6,7 @@ enum Home {
     // MARK: Submodules identifiers
 
     enum Submodule: String, UniqueIdentifiable {
+        case stories
         case streakActivity
         case continueCourse
         case enrolledCourses
@@ -72,6 +73,30 @@ enum Home {
         struct ViewModel {
             let module: Home.Submodule
             let result: State
+        }
+    }
+
+    /// Update stories visibility
+    enum StoriesVisibilityUpdate {
+        @available(*, deprecated, message: "Should be refactored with VIP cycle as CheckLanguageSwitchAvailability")
+        struct Response {
+            let isHidden: Bool
+        }
+
+        @available(*, deprecated, message: "Should be refactored with VIP cycle as CheckLanguageSwitchAvailability")
+        struct ViewModel {
+            let isHidden: Bool
+        }
+    }
+
+    /// Update status bar style (called by stories module)
+    enum StatusBarStyleUpdate {
+        struct Response {
+            let statusBarStyle: UIStatusBarStyle
+        }
+
+        struct ViewModel {
+            let statusBarStyle: UIStatusBarStyle
         }
     }
 }
