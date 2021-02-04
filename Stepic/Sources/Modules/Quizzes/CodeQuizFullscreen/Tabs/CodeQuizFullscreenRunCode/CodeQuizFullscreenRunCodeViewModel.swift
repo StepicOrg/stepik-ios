c220870d6d96c849927aa5d80a0c27a76241e1bf
Ivan Magda <ivan.magda@stepik.org>
2021-02-04 10:34:40 +0300

    Do not display success run code status

--- Stepic/Sources/Modules/Quizzes/CodeQuizFullscreen/Tabs/CodeQuizFullscreenRunCode/CodeQuizFullscreenRunCodeViewModel.swift
+++ Stepic/Sources/Modules/Quizzes/CodeQuizFullscreen/Tabs/CodeQuizFullscreenRunCode/CodeQuizFullscreenRunCodeViewModel.swift
@@ -4,7 +4,6 @@ struct CodeQuizFullscreenRunCodeViewModel {
     let testInput: String?
     let testOutput: String?
     let userCodeRunStatus: UserCodeRun.Status?
-    let isTestOutputMatchesSampleOutput: Bool
     let shouldShowTestInput: Bool
     let shouldShowTestOutput: Bool
     let isSamplesButtonEnabled: Bool
