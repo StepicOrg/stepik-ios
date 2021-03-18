2281b2c734e06e5b7211960a2b38409d659f3d00
Ivan Magda <ivan.magda@stepik.org>
2021-03-18 18:12:10 +0300

    Autocorrect StepicUITests

--- StepicUITests/Sources/StepicUITests.swift
+++ StepicUITests/Sources/StepicUITests.swift
@@ -176,12 +176,12 @@ class StepicUITests: XCTestCase {
         app.terminate()
     }
 
-//    func testLaunchPerformance() throws {
-//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-//            // This measures how long it takes to launch your application.
-//            measure(metrics: [XCTApplicationLaunchMetric()]) {
-//                XCUIApplication().launch()
-//            }
-//        }
-//    }
+    //    func testLaunchPerformance() throws {
+    //        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+    //            // This measures how long it takes to launch your application.
+    //            measure(metrics: [XCTApplicationLaunchMetric()]) {
+    //                XCUIApplication().launch()
+    //            }
+    //        }
+    //    }
 }
