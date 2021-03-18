a9bd7b7628290e00e6219b0712d17396960c6d0c
Ivan Magda <ivan.magda@stepik.org>
2021-03-18 18:11:11 +0300

    Add autocorrect.sh

---
+++ autocorrect.sh
@@ -0,0 +1,25 @@
+target_name=$1
+
+if [[ $target_name == "Stepic" ]]; then
+	path="Stepic/Sources"
+elif [[ $target_name == "StepicUITests" ]]; then
+	path="StepicUITests/Sources"
+else
+	echo "warning: unknown target name ${target_name}"
+fi
+
+if [[ -z $path ]]; then
+	exit 1
+fi
+
+swiftlint_executable="${PWD}/Pods/SwiftLint/swiftlint"
+
+if [[ -f $swiftlint_executable ]]; then
+    if [[ $target_name == "Stepic" ]]; then
+    	$swiftlint_executable --fix --config ${PWD}/.swiftlint.yml --path $path
+    else
+    	$swiftlint_executable --fix --config ${PWD}/.swiftlint.yml --path $path --format
+    fi
+else
+    echo "warning: SwiftLint not installed, run pod install"
+fi
