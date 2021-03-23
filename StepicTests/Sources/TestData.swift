9aac03e4c8496f02a52a68abca30c5d2c3db3547
Ivan Magda <ivan.magda@stepik.org>
2021-03-23 18:33:05 +0300

    Test DatasetValueTransformer via AttemptEntityTests

--- StepicTests/Sources/TestData.swift
+++ StepicTests/Sources/TestData.swift
@@ -14,6 +14,34 @@ enum TestData {
         self.loadJSON(path: "story-template-feedback")
     }
 
+    // MARK: Datasets
+
+    static var choiceDataset: JSON {
+        self.loadJSON(path: "dataset-choice")
+    }
+
+    static var fillBlanksDataset: JSON {
+        self.loadJSON(path: "dataset-fill-blanks")
+    }
+
+    static var freeAnswerDataset: JSON {
+        self.loadJSON(path: "dataset-free-answer")
+    }
+
+    static var matchingDataset: JSON {
+        self.loadJSON(path: "dataset-matching")
+    }
+
+    static var sortingDataset: JSON {
+        self.loadJSON(path: "dataset-sorting")
+    }
+
+    static var tableDataset: JSON {
+        self.loadJSON(path: "dataset-table")
+    }
+
+    // MARK: Private API
+
     private static func loadJSON(path: String) -> JSON {
         let testBundle = TestBundle().bundle
         let resourcePath = testBundle.path(forResource: path, ofType: "json")!
