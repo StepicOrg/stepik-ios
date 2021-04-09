3cb497f1a523d4e41ecf707ece48c28279d25743
Ivan Magda <ivan.magda@stepik.org>
2021-04-09 10:02:26 +0300

    Use mobile,ios filter for catalog blocks

--- Stepic/Legacy/Model/Network/Endpoints/CatalogBlocksAPI.swift
+++ Stepic/Legacy/Model/Network/Endpoints/CatalogBlocksAPI.swift
@@ -27,7 +27,7 @@ final class CatalogBlocksAPI: APIEndpoint {
 
     func retrieve(language: String, page: Int = 1) -> Promise<([CatalogBlock], Meta)> {
         let params: Parameters = [
-            "platform": "\(PlatformOptionSet.web.stringValue)",
+            "platform": "\(PlatformOptionSet.mobileIOS.stringValue)",
             "language": language,
             "page": page
         ]
