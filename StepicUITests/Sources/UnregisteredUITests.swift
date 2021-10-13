f50a660efb83ce9b340ee06acfe99f9609c8d1e1
Anton Kholin <ckub@inbox.ru>
2021-10-13 13:43:32 +0300

    UITests/add login tests (#1064)
    * Add LoginUITests class
    
    #SQA-840
    
    * Add login with Google test
    
    #SQA-840
    
    * Refactoring google login autotest
    
    #SQA-840
    
    * Add facebook login test
    Refactor Google login test
    
    * Last one)
    
    * Revert changes in LessonFinishedStepsPanModalFeedbackView
    
    Co-authored-by: AKholin <ckub.ckub@gmail.com>
    Co-authored-by: Ivan Magda <ivan.magda@stepik.org>

--- StepicUITests/Sources/UnregisteredUITests.swift
+++ StepicUITests/Sources/UnregisteredUITests.swift
@@ -1,11 +1,6 @@
 import XCTest
 
-/* WARNING!
- This tests needs make sure 'Hardware -> Keyboard -> Connect hardware keyboard' is off IN SIMULATOR.
- Use "defaults write com.apple.iphonesimulator ConnectHardwareKeyboard -bool false" in CI
- */
-
-class StepicUITests: XCTestCase {
+class UnregisteredUITests: XCTestCase {
     override func setUp() {
         super.setUp()
 
@@ -250,74 +245,6 @@ class StepicUITests: XCTestCase {
             XCTAssertTrue(elementsQuery.staticTexts["Activity"].exists, "No Activity section")
             XCTAssertTrue(elementsQuery.staticTexts["Achievements"].exists, "No Achievements section")
         }
-        app.terminate()
-    }
-
-    func testUserCanLogIn() throws {
-        let app = XCUIApplication()
-        app.launch()
-
-        if Common.isUserLoggedIn(app: app) {
-            Common.logOut(app: app)
-        }
-
-        Common.registerNewUserIfNeeded()
-
-        app.launch()
-        app.tabBars["Tab Bar"].buttons["Profile"].tap()
-        app.buttons["Sign In"].staticTexts["Sign In"].tap()
-        app.buttons["Sign In with e-mail"].tap()
-
-        app.textFields["Email"].tap()
-        sleep(2)
-        Common.pasteTextFieldText(
-            app: app,
-            element: app.textFields["Email"],
-            value: currentUserEmail
-        )
-
-        app.secureTextFields["Password"].tap()
-        sleep(2)
-        Common.pasteTextFieldText(
-            app: app,
-            element: app.secureTextFields["Password"],
-            value: kCurrentUserPassword
-        )
-
-        app.buttons["Log in"].tap()
-        // Check user profile loaded
-        if app.tabBars["Tab Bar"].buttons["Profile"].waitForExistence(timeout: 5) {
-            app.tabBars["Tab Bar"].buttons["Profile"].tap()
-        }
-
-        let elementsQuery = app.scrollViews.otherElements
-
-        if elementsQuery.staticTexts[currentUserName].waitForExistence(timeout: 5) {
-            XCTAssertTrue(elementsQuery.staticTexts["Activity"].exists, "No Activity section")
-            XCTAssertTrue(elementsQuery.staticTexts["Achievements"].exists, "No Achievements section")
-        } else {
-            XCTFail("User could not login")
-        }
-
-        app.terminate()
-    }
-
-    func testUserCanLogout() throws {
-        let app = XCUIApplication()
-        app.launch()
-
-        if !Common.isUserLoggedIn(app: app) {
-            Common.registerNewUserIfNeeded()
-            app.launch()
-            Common.logIn(app: app)
-        }
-
-        Common.logOut(app: app)
-
-        if Common.isUserLoggedIn(app: app) {
-            XCTFail("User was not logged out")
-        }
-
         app.terminate()
     }
 }
