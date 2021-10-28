1d6f8641d014abce49b641b35dadabec6351b4f8
Ivan Magda <ivan.magda@stepik.org>
2021-10-28 11:24:33 +0300

    Add checkPromoCode

--- Stepic/Legacy/Model/Network/Endpoints/MobileTiersAPI.swift
+++ Stepic/Legacy/Model/Network/Endpoints/MobileTiersAPI.swift
@@ -6,9 +6,20 @@ import SwiftyJSON
 final class MobileTiersAPI: APIEndpoint {
     override var name: String { "mobile-tiers" }
 
+    private var calculateRequestEndpoint: String { "\(self.name)/calculate" }
+
     func calculate(request: MobileTierCalculateRequest) -> Promise<MobileTierCalculateResponse> {
         self.create.request(
-            requestEndpoint: "\(self.name)/calculate",
+            requestEndpoint: self.calculateRequestEndpoint,
+            bodyJSONObject: request.bodyJSON,
+            withManager: self.manager
+        ).map(MobileTierCalculateResponse.init)
+    }
+
+    func checkPromoCode(name: String, courseID: Int) -> Promise<MobileTierCalculateResponse> {
+        let request = MobileTierCalculateRequest(params: [.init(courseID: courseID, promoCodeName: name)])
+        return self.create.request(
+            requestEndpoint: self.calculateRequestEndpoint,
             bodyJSONObject: request.bodyJSON,
             withManager: self.manager
         ).map(MobileTierCalculateResponse.init)
