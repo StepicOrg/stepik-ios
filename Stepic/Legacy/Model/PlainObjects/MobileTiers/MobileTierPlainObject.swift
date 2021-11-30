65d4162cd03cae465815cf2b5375a94d947a82d4
Ivan Magda <ivan.magda@stepik.org>
2021-12-01 01:23:23 +0300

    Delete mobile tier name

--- Stepic/Legacy/Model/PlainObjects/MobileTiers/MobileTierPlainObject.swift
+++ Stepic/Legacy/Model/PlainObjects/MobileTiers/MobileTierPlainObject.swift
@@ -15,30 +15,8 @@ extension MobileTierPlainObject {
     init(json: JSON) {
         self.id = json[JSONKey.id.rawValue].stringValue
         self.courseID = json[JSONKey.course.rawValue].intValue
-        self.priceTier = Self.sanitizedTier(json[JSONKey.priceTier.rawValue].string)
-        self.promoTier = Self.sanitizedTier(json[JSONKey.promoTier.rawValue].string)
-
-        if self.priceTier != nil {
-            print("MobileTierPlainObject :: courseID = \(self.courseID)")
-        }
-    }
-
-    // TODO: Remove before release
-    private static func sanitizedTier(_ tier: String?) -> String? {
-        guard let tier = tier else {
-            return nil
-        }
-
-        var result = tier.lowercased()
-
-        if result.starts(with: "tier ") {
-            result = "course_\(result.replacingOccurrences(of: " ", with: "_"))"
-        }
-
-        assert(result.starts(with: "course_tier_"))
-        print("MobileTierPlainObject :: tier = \(result)")
-
-        return result
+        self.priceTier = json[JSONKey.priceTier.rawValue].string
+        self.promoTier = json[JSONKey.promoTier.rawValue].string
     }
 
     enum JSONKey: String {
