d17bfabe391bdf0ef108ec9a33db4d42e1baa693
Ivan Magda <ivan.magda@stepik.org>
2022-03-23 22:01:24 +0700

    Comment FB code

--- Stepic/Legacy/Model/Network/SocialSDKProviders/FBSocialSDKProvider.swift
+++ Stepic/Legacy/Model/Network/SocialSDKProviders/FBSocialSDKProvider.swift
@@ -1,48 +1,44 @@
+//import FBSDKLoginKit
+//import Foundation
+//import PromiseKit
 //
-//  FBSocialSDKProvider.swift
-//  Stepic
+//final class FBSocialSDKProvider: NSObject, SocialSDKProvider {
+//    static let instance = FBSocialSDKProvider()
 //
-//  Created by Alexander Karpov on 22.11.16.
-//  Copyright © 2016 Alex Karpov. All rights reserved.
+//    let name = "facebook"
 //
-
-import FBSDKLoginKit
-import Foundation
-import PromiseKit
-
-final class FBSocialSDKProvider: NSObject, SocialSDKProvider {
-    static let instance = FBSocialSDKProvider()
-
-    let name = "facebook"
-
-    override private init() {
-        super.init()
-    }
-
-    func getAccessInfo() -> Promise<SocialSDKCredential> {
-        Promise { seal in
-            let loginManager = LoginManager()
-            loginManager.logIn(permissions: ["email"], from: nil, handler: { result, error in
-                if error != nil {
-                    seal.reject(SocialSDKError.connectionError)
-                    return
-                }
-
-                guard let result = result else {
-                    seal.reject(SocialSDKError.connectionError)
-                    return
-                }
-
-                if result.isCancelled {
-                    seal.reject(SocialSDKError.accessDenied)
-                    return
-                }
-
-                if let token = result.token?.tokenString {
-                    seal.fulfill(SocialSDKCredential(token: token))
-                    return
-                }
-            })
-        }
-    }
-}
+//    override private init() {
+//        super.init()
+//    }
+//
+//    func getAccessInfo() -> Promise<SocialSDKCredential> {
+//        Promise { seal in
+//            let loginManager = LoginManager()
+//            loginManager.logIn(
+//                permissions: ["email"],
+//                from: nil,
+//                handler: { result, error in
+//                    if error != nil {
+//                        seal.reject(SocialSDKError.connectionError)
+//                        return
+//                    }
+//
+//                    guard let result = result else {
+//                        seal.reject(SocialSDKError.connectionError)
+//                        return
+//                    }
+//
+//                    if result.isCancelled {
+//                        seal.reject(SocialSDKError.accessDenied)
+//                        return
+//                    }
+//
+//                    if let token = result.token?.tokenString {
+//                        seal.fulfill(SocialSDKCredential(token: token))
+//                        return
+//                    }
+//                }
+//            )
+//        }
+//    }
+//}
