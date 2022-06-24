01c212ad08c5d4808dc0079e7c852ee3d3a396d5
Ivan Magda <ivan.magda@stepik.org>
2022-06-24 18:36:34 +0700

    Refactor CodeInputAccessoryCollectionViewCell

--- Stepic/Legacy/Controllers/Quizzes/CodeQuiz/InputAccessory/Toolbar/CodeInputAccessoryCollectionViewCell/CodeInputAccessoryCollectionViewCell.swift
+++ Stepic/Legacy/Controllers/Quizzes/CodeQuiz/InputAccessory/Toolbar/CodeInputAccessoryCollectionViewCell/CodeInputAccessoryCollectionViewCell.swift
@@ -1,50 +1,87 @@
-//
-//  CodeInputAccessoryCollectionViewCell.swift
-//  Stepic
-//
-//  Created by Ostrenkiy on 18.07.17.
-//  Copyright © 2017 Alex Karpov. All rights reserved.
-//
-
+import SnapKit
 import UIKit
 
-final class CodeInputAccessoryCollectionViewCell: UICollectionViewCell {
-    @IBOutlet weak var textLabel: UILabel!
+extension CodeInputAccessoryCollectionViewCell {
+    enum Appearance {
+        static let textColor = UIColor.stepikSystemPrimaryText
 
-    var text: String?
-    var size: CodeInputAccessorySize?
+        static let cornerRadius: CGFloat = 4
+        static let borderWidth: CGFloat = 1
+        static let borderColor = UIColor.clear.cgColor
 
-    static func width(for text: String, size: CodeInputAccessorySize) -> CGFloat {
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: CGFloat.greatestFiniteMagnitude, height: CGFloat.greatestFiniteMagnitude))
-        label.numberOfLines = 1
-        label.text = text
-        label.font = UIFont(name: "Courier", size: size.realSizes.textSize)!
-        label.textAlignment = .center
-        label.sizeToFit()
+        static let backgroundColor = UIColor.stepikTertiaryBackground
 
-        return max(size.realSizes.minAccessoryWidth, label.bounds.width + 10)
+        static let calculateWidthBoundsWidthPadding: CGFloat = 10
+    }
+}
+
+final class CodeInputAccessoryCollectionViewCell: UICollectionViewCell, Reusable {
+    private lazy var textLabel = Self.makeTextLabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        self.setupView()
+        self.addSubviews()
+        self.makeConstraints()
     }
 
-    override func layoutSubviews() {
-        super.layoutSubviews()
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(text: String, size: CodeInputAccessorySize) {
+        self.textLabel.text = text
+        self.textLabel.font = Self.makeFont(for: size)
+    }
+
+    static func calculateWidth(for text: String, size: CodeInputAccessorySize) -> CGFloat {
+        let label = self.makeTextLabel(
+            frame: CGRect(x: 0, y: 0, width: CGFloat.greatestFiniteMagnitude, height: CGFloat.greatestFiniteMagnitude),
+            size: size
+        )
+        label.text = text
+        label.sizeToFit()
+        return max(size.realSizes.minAccessoryWidth, label.bounds.width + Appearance.calculateWidthBoundsWidthPadding)
+    }
+
+    private static func makeTextLabel(frame: CGRect = .zero, size: CodeInputAccessorySize? = nil) -> UILabel {
+        let label = UILabel(frame: frame)
+        label.textColor = Appearance.textColor
+        label.numberOfLines = 1
+        label.textAlignment = .center
+
+        if let size = size {
+            label.font = self.makeFont(for: size)
+        }
+
+        return label
+    }
+
+    private static func makeFont(for size: CodeInputAccessorySize) -> UIFont {
+        UIFont(name: "Courier", size: size.realSizes.textSize) ?? .systemFont(ofSize: size.realSizes.textSize)
+    }
+}
+
+extension CodeInputAccessoryCollectionViewCell: ProgrammaticallyInitializableViewProtocol {
+    func setupView() {
+        self.layer.cornerRadius = Appearance.cornerRadius
+        self.layer.borderWidth = Appearance.borderWidth
+        self.layer.borderColor = Appearance.borderColor
+        self.layer.masksToBounds = true
 
         self.backgroundColor = .stepikTertiaryBackground
-        self.textLabel.textColor = .stepikSystemPrimaryText
     }
 
-    func initialize(text: String, size: CodeInputAccessorySize) {
-        self.text = text
-        self.size = size
-        textLabel.text = text
-        let regularCourier = UIFont(name: "Courier", size: size.realSizes.textSize)!
-        textLabel.font = regularCourier
-        setRoundedStyle()
+    func addSubviews() {
+        self.contentView.addSubview(self.textLabel)
     }
 
-    private func setRoundedStyle() {
-        self.layer.cornerRadius = 4.0
-        self.layer.borderWidth = 1.0
-        self.layer.borderColor = UIColor.clear.cgColor
-        self.layer.masksToBounds = true
+    func makeConstraints() {
+        self.textLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.textLabel.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
 }
